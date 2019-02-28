96193dfa5734846731427e5d533bd3a1ed88fb40
Tcibisov Dmitriy <dcibisof@nic.ru>
2019-03-01 01:10:42 +0300

    HW1: Правильная скобочная последовательность

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,11 +3,40 @@
 
 
 def is_bracket_correct(input_string):
-    '''
+    """
     Метод проверяющий является ли поданная скобочная
      последовательность правильной (скобки открываются и закрываются)
      не пересекаются
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
-    '''
-    raise NotImplementedError
+    """
+    counters = {'(': 0, '[': 0, '{': 0}
+    open_brackets = list(counters.keys())
+    close_brackets = [')', ']', '}']
+    brackets_cl_op = {')': '(', ']': '[', '}': '{'}
+    brackets_list = open_brackets + close_brackets
+    brackets_mapping = {
+        '(': open_brackets + [')'],
+        '[': open_brackets + [']'],
+        '{': open_brackets + ['}'],
+    }
+
+    brackets = []
+    for bracket in input_string:
+        if bracket in brackets_list:  # скобка допустима
+            if bracket in open_brackets:  # скобка открывающая
+                brackets.append(bracket)
+                counters[bracket] += 1
+            else:  # скобка закрывающая
+                if not brackets:
+                    # если первой идёт закрывающаяся скобка
+                    return False
+                if bracket not in brackets_mapping[brackets[-1]]:
+                    # скобка не может следовать после последней открывающейся
+                    return False
+                brackets.pop(-1)
+                counters[brackets_cl_op[bracket]] -= 1
+        else:  # не входит в список допустимых скобок
+            return False
+
+    return not bool(sum(counters.values()))
