1f4f94192fcafc8e2b42b34ddf2a1abf9d40cf69
Borzenkov Andrey <boss.borzenkov@list.ru>
2019-03-01 01:17:18 +0300

    calculator.py commit - my first commit

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -3,11 +3,33 @@
 
 
 def calculator(x, y, operator):
-    '''
+    """
     Простенький калькулятор в прямом смысле. Работает c числами
     :param x: первый агрумент
     :param y: второй аргумент
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
-    '''
+    """
+    if operator == 'plus':
+        try:
+            return x + y
+        except TypeError:
+            return None
+    elif operator == 'minus':
+        try:
+            return x - y
+        except TypeError:
+            return None
+    elif operator == 'mult':
+        try:
+            return x * y
+        except TypeError:
+            return None
+    elif operator == 'divide':
+        if x == 0:
+            return None
+        try:
+            return x / y
+        except TypeError:
+            return None
     raise NotImplementedError
