39ecf16f7d2f20477864bf18db081203016a23b2
Tcibisov Dmitriy <dcibisof@nic.ru>
2019-03-01 20:57:49 +0300

    HW1: Инвертируйте int + доработка по 'Найти 2 числа в списке, дающих в сумме заданное число'

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -9,6 +9,61 @@ def find_indices(input_list, n):
     n. В случае, если таких элементов в массиве нет,
     то возвращается None
     Ограничение по времени O(n)
+    (find_indices([2, 2], 4) == tuple([0, 1]))
+    :param input_list: список произвольной длины целых чисел
+    :param n: целевая сумма
+    :return: tuple из двух индексов или None
+    """
+    # перенос значений списка в словарь значение - индексы
+    input_dict = dict()  # {}
+    for i in range(len(input_list)):
+        if not input_dict.get(input_list[i]):
+            input_dict[input_list[i]] = []
+        input_dict[input_list[i]].append(i)
+
+    # если разница n и текущего элемента ссылается
+    # на существующий элемент, то получается искомое
+    for k in input_list:
+        i = input_dict.get(k)  # список индексов
+        j = input_dict.get(n - k)  # list
+
+        if j:
+            if i != j:
+                # разные списки индексов
+                return tuple([i[0], j[0]])
+            elif len(i) > 1:
+                # список индексов с более чем одним указателем
+                return tuple([i[0], j[1]])
+
+    return None
+
+
+def find_indices_unique_list(input_list, n):
+    """
+    Не учитывает ситуацию с несколькими одинаковыми значениями
+    (которые в рамках словаря затрут друг друга)
+    (find_indices_unique_list([2, 2], 4) == None)
+    Ограничение по времени O(n)
+    :param input_list: список произвольной длины целых чисел
+    :param n: целевая сумма
+    :return: tuple из двух индексов или None
+    """
+    input_dict = {input_list[i]: i for i in range(len(input_list))}
+
+    # если разница n и текущего элемента ссылается
+    # на существующий элемент, то получается искомое
+    for k in input_list:
+        i = input_dict.get(k)
+        j = input_dict.get(n - k)
+        if j and i != j:
+            return tuple([i, j])
+
+    return None
+
+
+def find_indices_close_to_square_complexity(input_list, n):
+    """
+    Сложность квадратичная O(n^2)
     :param input_list: список произвольной длины целых чисел
     :param n: целевая сумма
     :return: tuple из двух индексов или None
--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,10 +3,11 @@
 
 
 def reverse(number):
-    '''
+    """
     Метод, принимающий на вход int и
     возвращающий инвертированный int
     :param number: исходное число
     :return: инвертированное число
-    '''
-    raise NotImplementedError
+    """
+    number = str(number)
+    return -int(number[:0:-1]) if number[0] == '-' else int(number[::-1])
