dbf398f9f7eec8a38e8ad6b4f96893ff0c477215
Danr0 <denp00@yandex.ru>
2019-03-02 23:24:10 +0300

    Update hw1_invertdict.py

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -3,10 +3,31 @@
 
 
 def invert_dict(source_dict):
-    '''
+    """
     Функция которая разворачивает словарь, т.е.
     каждому значению ставит в соответствие ключ.
     :param source_dict: dict
     :return: new_dict: dict
-    '''
-    raise NotImplementedError
+    """
+    back = {}
+
+    def pars(a):
+        out = []
+        for i in a:
+            if type(i) is list or type(i) is set or type(i) is tuple:
+                out += list(pars(i))
+            else:
+                out.append(i)
+        return out
+
+    for key in source_dict:
+        a = source_dict[key]
+        if type(a) is list or type(a) is set or type(a) is tuple:
+            s = list(a)
+            back[key] = pars(s)
+        else:
+            back[key] = a
+    return back
+
+
+# print(invert_dict({1 : ["sa",[2, [4] ]], 2:("sa"),3:[22]}))
