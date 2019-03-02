9830c02b605f2b6bf0291fa789f90bac9e714dd2
Danr0 <denp00@yandex.ru>
2019-03-02 20:42:42 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,34 +2,32 @@
 # coding: utf-8
 
 
-def calculate_determinant(list_of_lists):
+def calculate_determinant(a):
     """
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
     None
     Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
+    :param a: список списков - исходная матрица
     :return: значение определителя или None
     """
-    for i in range(len(list_of_lists)):
-        if len(list_of_lists[i]) != len(list_of_lists):
+    for i in range(len(a)):
+        if len(a[i]) != len(a):
             return None
     res = 1
-    n = len(list_of_lists)
+    n = len(a)
     for i in range(n):
         # выбираем опорный элемент
-        j = max(range(i, n), key=lambda k: abs(list_of_lists[k][i]))
+        j = max(range(i, n), key=lambda k: abs(a[k][i]))
         if i != j:
-            list_of_lists[i], list_of_lists[j] = list_of_lists[j], list_of_lists[i]
+            a[i], a[j] = a[j], a[i]
             res *= -1
         # убеждаемся, что матрица не вырожденная
-        if list_of_lists[i][i] == 0:
+        if a[i][i] == 0:
             return 0
-        res *= list_of_lists[i][i]
+        res *= a[i][i]
         # "обнуляем" элементы в текущем столбце
         for j in range(i + 1, n):
-            b = list_of_lists[j][i] / list_of_lists[i][i]
-            list_of_lists[j] = [
-                list_of_lists[j][k] - b * list_of_lists[i][k] for k in range(n)
-            ]
+            b = a[j][i] / a[i][i]
+            a[j] = [a[j][k] - b * a[i][k] for k in range(n)]
     return res
