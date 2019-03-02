fa1f3f399fb98befb37aade5c960452153038ed5
Danr0 <denp00@yandex.ru>
2019-03-02 15:10:49 +0300

    send

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,21 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
+    if operator == "plus":
+        return x + y
+    if operator == "minus":
+        return x - y
+    if operator == "divide":
+        if y != 0:
+            return x / y
+        else:
+            return None
+    if operator == "mult":
+        return x * y 
+    if operator == "power":
+        return x ** y
+    #raise ValueError("Bad operator call")
+
     raise NotImplementedError
+
+#print(calculator(140.28,140.28,'power'))
