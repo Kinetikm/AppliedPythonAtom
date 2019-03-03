ee6cfca8657930761a3310a75a6be3aeb5a9a78f
Евгений Вертиков <wertygeny@gmail.com>
2019-03-03 15:09:26 +0300

    Вертиков Евгений

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -2,12 +2,97 @@
 # coding: utf-8
 
 
+def spot_priority(symbol):
+    if symbol == '+' or symbol == '-':
+        return 0
+    elif symbol == '*' or symbol == '/':
+        return 1
+    elif symbol == '^': 
+        return 2
+    else:
+        return -1
+
 def advanced_calculator(input_string):
-    '''
-    Калькулятор на основе обратной польской записи.
-    Разрешенные операции: открытая скобка, закрытая скобка,
-     плюс, минус, умножить, делить
-    :param input_string: строка, содержащая выражение
-    :return: результат выполнение операции, если строка валидная - иначе None
-    '''
+    i = 0
+    minus = 0
+    stack = [] 
+    out = [] 
+    while i < len(input_string):
+        if input_string[i].isdigit():
+            Help = input_string[i]
+            i += 1
+            if i != len(input_string):
+                while input_string[i].isdigit() or input_string[i] == '.':
+                    Help += input_string[i]
+                    i += 1
+                    if i == len(input_string):
+                        break
+            if minus:    
+                out.append('-'+Help)
+                minus = 0
+            else:
+                out.append(Help)
+
+        elif input_string[i] == '(':
+            stack.append(input_string[i])
+            i += 1       
+
+        elif input_string[i] == ')':
+            Help = stack.pop()
+            while Help != '(':
+                out.append(Help)
+                Help = stack.pop()
+            i += 1
+
+        else:
+            j = spot_priority(input_string[i])
+            if input_string[i] == '-' and (i == 0 or input_string[i - 1] == '('): 
+                if input_string[i+1] == '(':
+                    out.append('0')
+                    stack.append('-')
+                else:
+                    minus = 1
+            else:
+                if stack:
+                    Help = stack.pop()
+                    while  j <= spot_priority(Help):
+                        out.append(Help)
+                        if stack:
+                            Help = stack.pop()
+                        else:
+                            break
+                    if j > spot_priority(Help):
+                        stack.append(Help)
+                stack.append(input_string[i])
+            i += 1
+
+    stack.reverse() 
+    out.extend(stack) 
+
+    i = 0
+    while i < len(out):
+        if out[i] == '+':
+            out.pop(i)
+            out.insert(i-2, float(out.pop(i-1))+float(out.pop(i-2)))
+            i -= 1
+        elif out[i] == '-':
+            out.pop(i)
+            out.insert(i-2, float(out.pop(i-2))-float(out.pop(i-2)))
+            i -= 1
+        elif out[i] == '*':
+            out.pop(i)
+            out.insert(i-2, float(out.pop(i-1))*float(out.pop(i-2)))
+            i -= 1
+        elif out[i] == '/':
+            out.pop(i)
+            out.insert(i-2, float(out.pop(i-2))/float(out.pop(i-2)))
+            i -= 1
+        elif out[i] == '^':
+            out.pop(i)
+            out.insert(i-2, float(out.pop(i-2))**float(out.pop(i-2)))
+            i -= 1
+        else:
+            i+=1
+
+    return float(out[0])            
     raise NotImplementedError
