1383b9001aeaa28d009e04518cffcabc370e03af
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-04 00:13:11 +0300

    int9

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,29 +3,20 @@
 
 
 def reverse(number):
-    """
-    Метод, принимающий
-    на вход
-    int и
-    возвращающий
-    инвертированный
-    int
-    :param number:
-     исходное число
-    :return:
-    инвертированное число
-    """
-
     if number < 0:
         k = -1
     else:
         k = 1
     number = abs(number)
-    a = 0
+    n = 0
     while abs(number) > 0:
-        l1 = number % 10
+        ost = number % 10
         number = number // 10
-        a = a * 10 + l1
-    return k * a
-    raise NotImplementedError
-
+        n = n * 10 + ost
+    return k * n
+    '''
+    Метод, принимающий на вход int и
+    возвращающий инвертированный int
+    :param number: исходное число
+    :return: инвертированное число
+    '''
