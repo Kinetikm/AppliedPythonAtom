a2850c244ab31b6aeb04ee5e28d81f3c42322545
Анастасия <asyadyoma@yandex.ru>
2019-03-03 13:24:46 +0300

    change palindrom

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+input_string1 = input()
+
 
 def check_palindrom(input_string):
-    '''
-    Метод проверяющий строку на то, является ли
-    она палиндромом.
-    :param input_string: строка
-    :return: True, если строка являестя палиндромом
-    False иначе
-    '''
-    raise NotImplementedError
+    if input_string[::-1] == input_string:
+        return True
+    else:
+        return False
+
+
+print(check_palindrom(input_string1))
+
+#    '''
+#    Метод проверяющий строку на то, является ли
+#    она палиндромом.
+#    :param input_string: строка
+#    :return: True, если строка являестя палиндромом
+#    False иначе
+#    '''
