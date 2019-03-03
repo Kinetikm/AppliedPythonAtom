1080a53499d85fd021b2b3befe73f3abaca00f5d
MaximAbramenko <gotosleepmcht@gmail.com>
2019-03-03 20:01:21 +0300

    skobki

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -10,4 +10,27 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
-    raise NotImplementedError
+    if len(input_string) % 2 != 0:
+        return False
+    i = 0
+    while i < len(input_string)-1:
+        if input_string[i] == "(":
+            if input_string[i+1] == ")":
+                i = i + 2
+                continue
+            else:
+                return False
+        if input_string[i] == "[":
+            if input_string[i+1] == "]":
+                i = i + 2
+                continue
+            else:
+                return False
+        if input_string[i] == "{":
+            if input_string[i+1] == "}":
+                i = i + 2
+                continue
+            else:
+                return False
+        return False
+    return True
