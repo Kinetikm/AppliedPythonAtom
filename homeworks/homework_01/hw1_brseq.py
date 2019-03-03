70528c0415188bc1f9f42903290406de52e922e5
IgorKreknin <Ikreknin@bk.ru>
2019-03-03 23:36:36 +0300

    Homework changes

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -14,21 +14,21 @@ def is_bracket_correct(input_string):
     sum2 = 0
     sum3 = 0
     for char in input_string:
-    	if char == "{":
-    		sum1 += 1
-    	if char == "}":
-    		sum1 -= 1
-    	if char == "[":
-    		sum2 += 1
-    	if char == "]":
-    		sum2 -= 1
-    	if char == "(":
-    		sum3 += 1
-    	if char == ")":
-    		sum3 -= 1
+        if char == "{":
+            sum1 += 1
+        if char == "}":
+            sum1 -= 1
+        if char == "[":
+            sum2 += 1
+        if char == "]":
+            sum2 -= 1
+        if char == "(":
+            sum3 += 1
+        if char == ")":
+            sum3 -= 1
     if sum1 == 0 and sum2 == 0 and sum3 == 0:
-    	return True
+        return True
     else:
-    	return False
+        return False
 
     raise NotImplementedError
