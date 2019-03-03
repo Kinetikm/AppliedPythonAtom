c34bd81bb35dc2d40aca374a29b014445b93a711
IgorKreknin <Ikreknin@bk.ru>
2019-03-04 01:42:42 +0300

    Homework change

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,10 +9,17 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
+    ind = False
     myNumber = 0
+    if number < 0:
+        number *= -1
+        ind = True
     while number > 0:
         myNumber *= 10
         myNumber += (number % 10)
         number //= 10
-    return myNumber
+    if ind:
+        return -1*myNumber
+    else:
+        return myNumber
     raise NotImplementedError
