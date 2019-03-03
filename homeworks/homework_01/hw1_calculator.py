87b83225df7a5d3283ece64fe256c8c9b13565b0
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-03 14:42:46 +0300

    Homework changes 2

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-def tryfloat(value):
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
-
 def calculator(x, y, operator):
     '''
     Простенький калькулятор в прямом смысле. Работает c числами
@@ -16,16 +9,15 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
-    if tryfloat(x) and tryfloat(y):
-        if operator == "plus":
-            return x + y
-        if operator == "minus":
-            return x - y
-        if operator == "mult":
-            return x * y
-        if operator == "divide":
-            return x / y if y != 0 else None
+    if not (type(x) == float and type(y) == float):
         return None
-
+    if operator == "plus":
+        return x + y
+    if operator == "minus":
+        return x - y
+    if operator == "mult":
+        return x * y
+    if operator == "divide":
+        return x / y if y != 0 else None
     return None
     raise NotImplementedError
