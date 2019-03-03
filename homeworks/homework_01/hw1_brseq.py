4d0c2ca7e8094932d69266d2b02e579066ef2f08
IgorKreknin <Ikreknin@bk.ru>
2019-03-03 23:00:03 +0300

    Homework changes

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -10,4 +10,27 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+
+    for char in input_string:
+    	if char == '{':
+    		sum1 += 1
+    	if char == '}':
+    		sum1 -= 1
+    	if char == '[':
+    		sum2 += 1
+    	if char == ']':
+    		sum2 -= 1
+    	if char == '(':
+    		sum3 += 1
+    	if char == ')':
+    		sum3 -= 1
+
+    if sum1 == 0 and sum2 == 0 and sum3 == 0:
+    	return True
+    else:
+    	return False
+    	
     raise NotImplementedError
--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,20 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
+    try:
+    	float(x)
+    	float(y)
+    	if operator == "plus":
+    		return x + y
+    	if operator == "minus":
+    		return x - y
+    	if operator == "mult":
+    		return x * y
+    	if operator == "divide":
+    		if y == 0:
+    			return None
+    		else:
+    			return x / y
+    except ValueError:
+    	return None
     raise NotImplementedError
