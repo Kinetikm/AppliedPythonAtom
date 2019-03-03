745adf2a32ca88c1d7ece18b9597f02099bfb416
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-03 16:52:49 +0300

    polindrom

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,7 +10,7 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    input_string = input_string.strip()
+    
     if input_string[::-1] == input_string:
         return True
     else:
