2587ed98f860cdd81dd630273fb14ff936565dcd
Анастасия <asyadyoma@yandex.ru>
2019-03-03 14:00:55 +0300

    change calculator

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#x1 = input()
-#y1 = input()
-#operator1 = input()
+# x1 = input()
+# y1 = input()
+# operator1 = input()
 
 
 def calculator(x, y, operator):
-#        if operator == 'plus':
-#            return float(x) + float(y)
-#        if operator == 'minus':
-#            return float(x) - float(y)
-#        if operator == 'mult':
-#            return float(x) * float(y)
-#        if operator == 'divide':
-#            return float(x) / float(y)
-#        else:
-#            return None
+    #        if operator == 'plus':
+    #            return float(x) + float(y)
+    #        if operator == 'minus':
+    #            return float(x) - float(y)
+    #        if operator == 'mult':
+    #            return float(x) * float(y)
+    #        if operator == 'divide':
+    #            return float(x) / float(y)
+    #        else:
+    #            return None
     raise NotImplementedError
 
-
-#print(calculator(x1, y1, operator1))
+# print(calculator(x1, y1, operator1))
 
 #    '''
 #    Простенький калькулятор в прямом смысле. Работает c числами
