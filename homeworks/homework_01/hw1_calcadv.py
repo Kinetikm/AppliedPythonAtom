3f207dc6d60abbc39bacc162966737534bdce7ca
Евгений Вертиков <wertygeny@gmail.com>
2019-03-03 15:30:02 +0300

    Вертиков Евгений

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -7,16 +7,14 @@ def spot_priority(symbol):
         return 0
     elif symbol == '*' or symbol == '/':
         return 1
-    elif symbol == '^': 
-        return 2
     else:
         return -1
 
 def advanced_calculator(input_string):
     i = 0
     minus = 0
-    stack = [] 
-    out = [] 
+    stack = []
+    out = []
     while i < len(input_string):
         if input_string[i].isdigit():
             Help = input_string[i]
@@ -27,7 +25,7 @@ def advanced_calculator(input_string):
                     i += 1
                     if i == len(input_string):
                         break
-            if minus:    
+            if minus:
                 out.append('-'+Help)
                 minus = 0
             else:
@@ -35,7 +33,7 @@ def advanced_calculator(input_string):
 
         elif input_string[i] == '(':
             stack.append(input_string[i])
-            i += 1       
+            i += 1
 
         elif input_string[i] == ')':
             Help = stack.pop()
@@ -46,28 +44,31 @@ def advanced_calculator(input_string):
 
         else:
             j = spot_priority(input_string[i])
-            if input_string[i] == '-' and (i == 0 or input_string[i - 1] == '('): 
-                if input_string[i+1] == '(':
-                    out.append('0')
-                    stack.append('-')
+            if j != -1:
+                if input_string[i] == '-' and (i == 0 or input_string[i - 1] == '('): 
+                    if input_string[i+1] == '(':
+                        out.append('0')
+                        stack.append('-')
+                    else:
+                        minus = 1
                 else:
-                    minus = 1
+                    if stack:
+                        Help = stack.pop()
+                        while  j <= spot_priority(Help):
+                            out.append(Help)
+                            if stack:
+                                Help = stack.pop()
+                            else:
+                                break
+                        if j > spot_priority(Help):
+                            stack.append(Help)
+                    stack.append(input_string[i])
             else:
-                if stack:
-                    Help = stack.pop()
-                    while  j <= spot_priority(Help):
-                        out.append(Help)
-                        if stack:
-                            Help = stack.pop()
-                        else:
-                            break
-                    if j > spot_priority(Help):
-                        stack.append(Help)
-                stack.append(input_string[i])
+                return None
             i += 1
 
-    stack.reverse() 
-    out.extend(stack) 
+    stack.reverse()
+    out.extend(stack)
 
     i = 0
     while i < len(out):
@@ -92,7 +93,7 @@ def advanced_calculator(input_string):
             out.insert(i-2, float(out.pop(i-2))**float(out.pop(i-2)))
             i -= 1
         else:
-            i+=1
+            i += 1
 
-    return float(out[0])            
+    return float(out[0])     
     raise NotImplementedError
