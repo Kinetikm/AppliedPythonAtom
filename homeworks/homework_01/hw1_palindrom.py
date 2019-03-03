fbd944c1cac9bde336d5ab23b4999c2a9dbb545f
electron97 <ars2d2@yandex.ru>
2019-03-04 02:27:06 +0300

    changed_2 palingrom

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,7 +10,7 @@
 
 
 def check_palindrom(input_string):
-    if input_string == input_string[::-1]:
+    if input_string == input_string[::-1]:  
         return True
     else:
         return False
