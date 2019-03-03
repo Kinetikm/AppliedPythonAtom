ab5d5303df7398e64ed8cec8af4010dc57f793f9
duckyduckk <kolya1996rus@mail.ru>
2019-03-03 21:46:37 +0300

    asd

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -3,11 +3,16 @@
 
 
 def calculator(x, y, operator):
-    '''
-    Простенький калькулятор в прямом смысле. Работает c числами
-    :param x: первый агрумент
-    :param y: второй аргумент
-    :param operator: 4 оператора: plus, minus, mult, divide
-    :return: результат операции или None, если операция не выполнима
-    '''
-    raise NotImplementedError
+    try:
+        if (operator== 'plus'):
+            return (x + y)
+        if operator == "minus":
+            return x - y
+        if operator == "mult":
+            return x * y
+        if operator == 'divide':
+            return (x / y)
+        else:
+            print('None')
+    except:
+        print('None')
