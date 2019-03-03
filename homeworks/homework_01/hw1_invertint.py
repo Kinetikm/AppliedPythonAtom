6bcf17821d6e0a3227f96bff6bbbd3720481d156
MaximAbramenko <gotosleepmcht@gmail.com>
2019-03-03 18:45:54 +0300

    инвертируем целое

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,10 +3,20 @@
 
 
 def reverse(number):
+    if number < 0:
+        k = -1
+    else:
+        k = 1
+    number = abs(number)
+    nout = 0
+    while abs(number) > 0:
+        l = number % 10
+        number = number // 10
+        nout = nout * 10 + l
+    return k * nout
     '''
     Метод, принимающий на вход int и
     возвращающий инвертированный int
     :param number: исходное число
     :return: инвертированное число
     '''
-    raise NotImplementedError
