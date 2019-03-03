94bafa555461bfd6d6f6689b9356ccd2398e62fe
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-03 16:58:48 +0300

    polindrom1

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -4,16 +4,17 @@
 
 def check_palindrom(input_string):
     '''
-    Метод проверяющий строку на то, является ли
+    Метод проверяющий строку
+    на то, является ли
     она палиндромом.
-    :param input_string: строка
-    :return: True, если строка являестя палиндромом
+    :param input_string:
+     строка
+    :return: True, если
+    строка являестя
+    палиндромом
     False иначе
     '''
-    
     if input_string[::-1] == input_string:
         return True
-    else:
-        return False
-
+    return False
     raise NotImplementedError
