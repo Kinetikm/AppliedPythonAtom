f002bd272968f7ba2542bf732488f3814b16f760
Анастасия <asyadyoma@yandex.ru>
2019-03-04 13:01:59 +0300

    change invertint, brseq

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,11 +3,19 @@
 
 
 def is_bracket_correct(input_string):
-    '''
-    Метод проверяющий является ли поданная скобочная
-     последовательность правильной (скобки открываются и закрываются)
-     не пересекаются
-    :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
-    :return: True or False
-    '''
-    raise NotImplementedError
+    for i in range(len(input_string)):
+        if (input_string[i] == '(' and input_string[- (i + 1)] == ')') \
+                or (input_string[i] == '[' and input_string[- (i + 1)] == ']') \
+                or (input_string[i] == '{' and input_string[- (i + 1)] == '}'):
+            return True
+        else:
+            return False
+
+#    '''
+#    Метод проверяющий является ли поданная скобочная
+#     последовательность правильной (скобки открываются и закрываются)
+#     не пересекаются
+#    :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
+#    :return: True or False
+#     '''
+#    raise NotImplementedError
