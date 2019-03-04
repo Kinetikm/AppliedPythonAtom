55efc3bf322d6cb4b0042c5b72d83b2d3f0b9fbc
NSKuznetsov <helloqwtheslayer@mail.ru>
2019-03-04 23:23:24 +0300

    Калькулятор

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -3,11 +3,17 @@
 
 
 def calculator(x, y, operator):
-    '''
-    Простенький калькулятор в прямом смысле. Работает c числами
-    :param x: первый агрумент
-    :param y: второй аргумент
-    :param operator: 4 оператора: plus, minus, mult, divide
-    :return: результат операции или None, если операция не выполнима
-    '''
+    if isinstance(x, (int, float)) * isinstance(y, (int, float)) == 1:
+        if operator == 'plus':
+            return x + y
+        elif operator == 'minus':
+            return x - y
+        elif operator == 'mult':
+            return x * y
+        elif operator == 'divide' and y != float(0):
+            return x / y
+        else:
+            return None
+    else:
+        return None
     raise NotImplementedError
