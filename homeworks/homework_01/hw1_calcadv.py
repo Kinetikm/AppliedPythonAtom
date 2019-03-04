e436337439efb196f212abb6af8492485b797e28
electron97 <ars2d2@yandex.ru>
2019-03-04 23:17:51 +0300

    invertdict, invertint, palindrom, calcadv changed

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-def advanced_calculator(input_string):
-    '''
+'''
     Калькулятор на основе обратной польской записи.
     Разрешенные операции: открытая скобка, закрытая скобка,
      плюс, минус, умножить, делить
     :param input_string: строка, содержащая выражение
     :return: результат выполнение операции, если строка валидная - иначе None
-    '''
+'''
+
+
+def advanced_calculator(input_string):
     raise NotImplementedError
+    # stack = []
+    # i = 0
+    # obj = input_string.spltit(' ')
+    # for sign in obj:
+    #    if sign == '+':
+    #        stack[i - 1] = stack[i - 2] + stac = / *------------ **
+    #        stack[i - 1] = stack[i - 2] - stack[i - 1]
+    #    elif sign == '*':
+    #        stack[i - 1] = stack[i - 2] * stack[i - 1]
+    #    elif sign == '/':
+    #        stack[i - 1] = stack[i - 2] / stack[i - 1]
--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import copy
 
 
 def invert_dict(source_dict):
@@ -9,4 +10,28 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict
     '''
-    raise NotImplementedError
+
+    def add_key(dictionary, val, key):
+        if val in dictionary:
+            if type(dictionary[val]) is list:
+                dictionary[val].append(key)
+            else:
+                dictionary[val] = [dictionary[val]] + [key]
+        else:
+            dictionary[val] = key
+        return dictionary
+
+    def rekurs(res, element, k):
+        if type(element) is list or type(element) is tuple or type(element) is set:
+            for elem in element:
+                res = rekurs(res, copy.deepcopy(elem), k)
+        else:
+            res = add_key(res, copy.deepcopy(element), source_key)
+        return res
+
+    result = dict()
+    for source_key in (source_dict):
+        source_val = source_dict[source_key]
+        result = rekurs(result, copy.deepcopy(source_val), source_key)
+
+    return result
