d0d75af0297547ce340e5fb06f02930304a153aa
Maxim Khomyak <0xc00000f@gmail.com>
2019-03-05 02:46:44 +0300

    solved version of det

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import copy
 
 
 def calculate_determinant(list_of_lists):
@@ -11,4 +12,32 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+
+    if not is_square(list_of_lists):
+        return None
+
+    return solve(list_of_lists, 1)
+
+
+def solve(matrix, mul):
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        total = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            sign *= -1
+            total += mul * solve(m, sign * matrix[0][i])
+        return total
+
+
+def is_square(m): return all(len(row) == len(m) for row in m)
+
