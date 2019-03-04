95432d4ae1c49e3323c96d4fbb8dfebbe1c0eaf9
Maxim Khomyak <0xc00000f@gmail.com>
2019-03-04 03:52:45 +0300

    solved hw1_arrsearch.py

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -13,4 +13,26 @@ def find_indices(input_list, n):
     :param n: целевая сумма
     :return: tuple из двух индексов или None
     '''
-    raise NotImplementedError
+
+    input_list.sort()
+
+    i = 0
+    i2 = 0
+    j2 = len(input_list) - 1
+    j = len(input_list) - 1
+
+    while i <= j:
+        if input_list[i] + input_list[j] < n:
+            if i != j2:
+                i += 1
+            else:
+                return None
+        if input_list[i] + input_list[j] > n:
+            if j != i2:
+                j -= 1
+            else:
+                return None
+        if (input_list[i] + input_list[j]) == n:
+            return (i, j)
+
+    return None
