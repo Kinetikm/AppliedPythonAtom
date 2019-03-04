ec1b5741ace024cc5e02de0cc592de8b71274fa9
Maxim Khomyak <0xc00000f@gmail.com>
2019-03-04 14:33:31 +0300

    release simplecalc

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 
+import numbers
+
+
 def calculator(x, y, operator):
     '''
     Простенький калькулятор в прямом смысле. Работает c числами
@@ -11,25 +14,27 @@ def calculator(x, y, operator):
     :return: результат операции или None, если операция не выполнима
     '''
 
-#     operations = {
-#         'plus': lambda x, y: x + y,
-#         'minus': lambda x, y: x - y,
-#         'mult': lambda x, y: x * y,
-#         'divide': divide(x, y),
-#     }
-#
-#     if operator in operations:
-#         return operations[operator](x, y)
-#     else:
-#         return None
-#
-# def divide(x, y):
-#     try:
-#         res = x / y
-#     except ZeroDivisionError:
-#         return None
-#     except ValueError:
-#         return None
-#     return res
+    operations = {
+        'plus': lambda x, y: x + y,
+        'minus': lambda x, y: x - y,
+        'mult': lambda x, y: x * y,
+        'divide': lambda x, y: divide(x, y),
+    }
+    try:
+        if isinstance(x, numbers.Number) & isinstance(y, numbers.Number):
+            if operator in operations.keys():
+                return operations[operator](x, y)
+            else:
+                return None
+    except TypeError:
+        return None
 
-    raise NotImplementedError
+
+def divide(x, y):
+    try:
+        res = x / y
+    except ZeroDivisionError:
+        return None
+    except ValueError:
+        return None
+    return res
