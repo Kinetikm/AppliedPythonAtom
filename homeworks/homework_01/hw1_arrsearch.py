f6b4900973002e6fb1d1b26fd3352eac04222373
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-05 02:31:44 +0300

    Homework changes

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,12 +3,13 @@
 
 
 def find_indices(input_list, n):
-    MyDict = {}
-    for i in range(len(input_list)):
-        k = input_list[i]
-        if k not in MyDict:
-            MyDict.update({k : i})
-    for j in range(len(input_list)):
-        if MyDict.get(n-input_list[j]):
-            return [MyDict.get(n-input_list[j]), j]
-    return None
+    #MyDict = {}
+    #for i in range(len(input_list)):
+    #    k = input_list[i]
+    #    if k not in MyDict:
+    #        MyDict.update({k : i})
+    #for j in range(len(input_list)):
+    #    if MyDict.get(n-input_list[j]):
+    #        return [MyDict.get(n-input_list[j]), j]
+    #return None
+	raise NotImplementedError
--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 def calculator(x, y, operator):
-    '''
-    Простенький калькулятор в прямом смысле. Работает c числами
-    :param x: первый агрумент
-    :param y: второй аргумент
-    :param operator: 4 оператора: plus, minus, mult, divide
-    :return: результат операции или None, если операция не выполнима
-    '''
-    raise NotImplementedError
+    if (type(x) == float and type(y) == float):
+        if operator == "plus":
+            return x+y
+        elif operator == "minus":
+            return x-y
+        elif operator == "mult":
+            return x*y
+        elif operator == "divide" and y != 0:
+            return x/y
+    return None
+
+print(calculator(3, 23, "mult"))
