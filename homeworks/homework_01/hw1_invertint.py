efe5cab37c023a346f3e9e58fe6181267bb9a0b9
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-05 01:01:53 +0300

    Homework changes

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -6,12 +6,12 @@ def reverse(number):
     buf = 0
     K = number
     if number < 0:
-        number *= -1;
+        number *= -1
     while number:
         buf = buf * 10 + number % 10
         number = number // 10
     if K < 0:
-        return  buf * (-1)
+        return buf * (-1)
     else:
         return buf
 
