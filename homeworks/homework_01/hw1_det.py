a62ef0aa98321a01b5d527685dd1a68019d8048e
Eireenn <arinagovorova@gmail.com>
2019-03-04 23:25:40 +0300

    ---

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,12 +3,20 @@
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    length=len(list_of_lists)
+    for i in list_of_lists:
+        if len(i)!=length:
+            return(None)
+    return recurs(list_of_lists,[i for i in range(length)],[j for j in range(length)])
+
+def recurs(Ll,a,b):
+    res=0
+    if (len(a) == 1):
+        return Ll[a[0]][b[0]]
+    b1=b[1:]
+    for i in range (len(a)):
+        a1=a[::]
+        a1.remove(a[i])
+        res=res+(-1)**(i%2)*Ll[a[i]][b[0]]*recurs(Ll,a1,b1)
+    return (res)
+
