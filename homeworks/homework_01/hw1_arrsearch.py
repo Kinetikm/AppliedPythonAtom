7e80df32219e56159e2c4f024c0b3ad4fa66c9f2
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-05 02:35:17 +0300

    Homework changes

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,13 +3,4 @@
 
 
 def find_indices(input_list, n):
-    #MyDict = {}
-    #for i in range(len(input_list)):
-    #    k = input_list[i]
-    #    if k not in MyDict:
-    #        MyDict.update({k : i})
-    #for j in range(len(input_list)):
-    #    if MyDict.get(n-input_list[j]):
-    #        return [MyDict.get(n-input_list[j]), j]
-    #return None
 	raise NotImplementedError
