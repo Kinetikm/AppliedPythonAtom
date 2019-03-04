a3c532a70afd3d22cf014ec57e1ee6252d2b9e40
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-05 01:11:10 +0300

    Homework changes

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,14 +3,12 @@
 
 
 def find_indices(input_list, n):
-    '''
-    Метод возвращает индексы двух различных
-    элементов listа, таких, что сумма этих элементов равна
-    n. В случае, если таких элементов в массиве нет,
-    то возвращается None
-    Ограничение по времени O(n)
-    :param input_list: список произвольной длины целых чисел
-    :param n: целевая сумма
-    :return: tuple из двух индексов или None
-    '''
-    raise NotImplementedError
+  MyDict = {}
+  for i in range(len(input_list)) :
+      k = input_list[i]
+      if not  k in MyDict:
+          MyDict.update({k:i})
+  for j in range(len(input_list)):
+      if MyDict.get(n-input_list[j]):
+          return [MyDict.get(n-input_list[j]), j]
+  return None
