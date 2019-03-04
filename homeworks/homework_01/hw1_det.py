a69427c5ca91cb274136338ef120cfea5274b390
IgorKreknin <Ikreknin@bk.ru>
2019-03-04 12:13:36 +0300

    Homework changes

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,20 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
+    n = len(list_of_lists)
+    for myList in list_of_lists:
+        if len(myList) != n:
+            return None
+    if n == 2:
+        a = list_of_lists[0][0] * list_of_lists[1][1]
+        b = list_of_lists[0][1] * list_of_lists[1][0]
+        return a - b
+    det = 0
+    for i in range(n):
+        myList = list_of_lists[:]
+        for j in range(1, n):
+            myList[j].pop(i)
+        myList.pop(0)
+        det += (-1) ** (i + 2) * calculate_determinant(myList)
+    return det
     raise NotImplementedError
