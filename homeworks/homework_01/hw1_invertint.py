4884db99df44a889f4f91f80ba1f66a09fe21604
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-04 20:36:33 +0300

    Homework changes

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 
 
-def reverse (number):
+def reverse(number):
     buf = 0
-    while a:
-        buf=buf*10+number%10
-        number/=10
+    while number:
+        buf=buf*10 + number % 10
+        number /= 10
     return buf
