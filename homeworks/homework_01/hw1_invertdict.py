7c6444e40eb94c1128855207f20077a4548d231d
Евгений Вертиков <wertygeny@gmail.com>
2019-03-04 14:24:29 +0300

    Вертиков Евгений

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -14,20 +14,23 @@ def help_func(key, value, new_dict):
             if j == value:
                 help_list = new_dict[j]
                 help_list.append(key)
-                new_dict.update({value : help_list})
+                new_dict.update({value: help_list})
                 h = 1
                 break
         if not h:
             help_list = []
             help_list.append(key)
-            new_dict.update({value : help_list})
+            new_dict.update({value: help_list})
+
 
 def invert_dict(source_dict):
     new_dict = {}
+    if len(source_dict) == 0:
+        return source_dict
     for i in source_dict.keys():
         help_func(i, source_dict[i], new_dict)
     for i in new_dict.keys():
         if len(new_dict[i]) == 1:
-            new_dict.update({i : new_dict[i][0]})
+            new_dict.update({i: new_dict[i][0]})
     return new_dict
     raise NotImplementedError
