fc76afc6f8efb551d297a1e1b72327010f9cd780
Maxim Khomyak <0xc00000f@gmail.com>
2019-03-04 03:23:24 +0300

    solved hw1_brseq.py

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
 
 def is_bracket_correct(input_string):
     '''
@@ -10,4 +29,33 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
-    raise NotImplementedError
+
+    map1 = {
+        '{': '{}',
+        '[': '[]',
+        '(': '()',
+        '}': '{}',
+        ']': '[]',
+        ')': '()'
+    }
+
+    s = Stack()
+    list_push = ['{', '[', '(']
+    list_pop = ['}', ']', ')']
+
+    for br in input_string:
+        if br in map1.keys():
+            if br in list_push:
+                s.push(map1[br])
+            if br in list_pop:
+                try:
+                    tmp = s.pop()
+                except IndexError:
+                    return False
+                if map1[br] != tmp:
+                    return False
+
+    if s.isEmpty():
+        return True
+
+    return False
