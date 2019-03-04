808ff8252c5ee887cb314a90431099e552c1e978
Анастасия <asyadyoma@yandex.ru>
2019-03-04 13:10:08 +0300

    chage brseq

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -4,9 +4,9 @@
 
 def is_bracket_correct(input_string):
     for i in range(len(input_string)):
-        if (input_string[i] == '(' and input_string[- (i + 1)] == ')') \
-                or (input_string[i] == '[' and input_string[- (i + 1)] == ']') \
-                or (input_string[i] == '{' and input_string[- (i + 1)] == '}'):
+        if (input_string[i] == '(' and input_string[- (i + 1)] == ')') or (
+                input_string[i] == '[' and input_string[- (i + 1)] == ']') or (
+                input_string[i] == '{' and input_string[- (i + 1)] == '}'):
             return True
         else:
             return False
