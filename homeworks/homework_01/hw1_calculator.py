0b7b863e8bec998af5ab8d9c140170f9ce301e06
Maxim Khomyak <0xc00000f@gmail.com>
2019-03-04 03:53:52 +0300

    first versions of simplecalc and invertdict, dont work

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,26 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
+
+#     operations = {
+#         'plus': lambda x, y: x + y,
+#         'minus': lambda x, y: x - y,
+#         'mult': lambda x, y: x * y,
+#         'divide': divide(x, y),
+#     }
+#
+#     if operator in operations:
+#         return operations[operator](x, y)
+#     else:
+#         return None
+#
+# def divide(x, y):
+#     try:
+#         res = x / y
+#     except ZeroDivisionError:
+#         return None
+#     except ValueError:
+#         return None
+#     return res
+
     raise NotImplementedError
--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# CHET NE VZLETELO
 
 def invert_dict(source_dict):
     '''
@@ -9,4 +10,44 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict
     '''
+#     new_dict = {}
+#
+#     if not source_dict:
+#         return source_dict
+#     for k, v in source_dict.items():
+#         if isinstance(v, (list, tuple, set)):
+#             for v2 in get1List(v):
+#                 helper(k, v2, new_dict)
+#         else:
+#             helper(k, v, new_dict)
+#
+#     return new_dict
+#
+#
+# def helper(k, v, dict):
+#     if v in dict.keys():
+#         if isinstance(dict[v], list):
+#             dict[v].append(k)
+#         else:
+#             tmp = dict[v]
+#             dict[v] = [tmp, k]
+#     else:
+#         dict[v] = k
+#
+# def get1List(lst):
+#     tmp = []
+#     tmp.append(findList(lst))
+#     return tmp
+#
+# def findList(lst):
+#     tmp = []
+#     if not lst:
+#         return
+#     if isinstance(lst, (list, tuple, set)):
+#         for i in lst:
+#             findList(i)
+#     else:
+#         tmp.append(lst)
+#     return tmp
+
     raise NotImplementedError
