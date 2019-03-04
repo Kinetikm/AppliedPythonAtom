2b512387a3ffbe59ad5cc884d463bb89b824f592
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-04 19:48:40 +0300

    “Determinant”

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,6 +2,17 @@
 # coding: utf-8
 
 
+import copy
+
+
+def calculate_minor(a, i, j):
+    minor = copy.deepcopy(a)
+    del minor[i]
+    for i in range(len(a[0]) - 1):
+        del minor[i][j]
+    return minor
+
+
 def calculate_determinant(list_of_lists):
     '''
     Метод, считающий детерминант входной матрицы,
@@ -11,4 +22,16 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    n = len(list_of_lists[0])
+    m = len(list_of_lists)
+    if m != n:
+        return None
+    if n == 1:
+        return list_of_lists[0][0]
+    determinant = 0
+    k = 1
+    for i in range(n):
+        determinant += list_of_lists[0][i] * calculate_determinant(calculate_minor(list_of_lists, 0, i)) * k
+        k *= -1
+    return determinant
+raise NotImplementedError
