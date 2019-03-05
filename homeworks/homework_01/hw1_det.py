b8763b4ac63f68a7b4288693deb6f4b84d495868
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-06 02:07:38 +0300

    Homework changes

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,20 +3,23 @@
 
 
 import copy
+
+
 def RemoveS(matr, i, j):
     del matr[i]
     for count in range(len(matr)):
         del matr[count][j]
 
+
 def calculate_determinant(list_of_lists):
-    k=0
+    k = 0
     if list_of_lists == [[]]:
         return None
     if len(list_of_lists) > 2:
         for count in range(len(list_of_lists)):
-            matr2_0=copy.deepcopy(list_of_lists)
+            matr2_0 = copy.deepcopy(list_of_lists)
             RemoveS(matr2_0, count, 0)
-            k=k+((-1)**(count+2)) * list_of_lists[count][0] * \
+            k = k+((-1)**(count+2)) * list_of_lists[count][0] * \
               calculate_determinant(matr2_0)
         return k
     elif len(list_of_lists) == 2:
@@ -26,3 +29,5 @@ def calculate_determinant(list_of_lists):
         return list_of_lists[0][0]
     else:
         return None
+
+print(calculate_determinant([[202.048], [-36.161]]))
