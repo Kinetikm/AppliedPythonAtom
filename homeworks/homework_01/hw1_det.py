e476aa89148ca8cfb499d40c2a2b38a5d9acc50e
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-06 01:51:00 +0300

    Homework changes

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -12,7 +12,6 @@ def calculate_determinant(list_of_lists):
     k=0
     if list_of_lists == [[]]:
         return None
-    prinf("----------------------------------------->", len(list_of_lists))
     if len(list_of_lists) > 2:
         for count in range(len(list_of_lists)):
             matr2_0=copy.deepcopy(list_of_lists)
@@ -27,20 +26,3 @@ def calculate_determinant(list_of_lists):
         return list_of_lists[0][0]
     else:
         return None
-
-# print(calculate_determinant([[]]))
-b=[[2, 4, 1, 244],[3, 7, 8, 234], [1, 2, 5, 127], [4, 1, 87, 95]]
-# print(calculate_determinant(b))
-print(calculate_determinant([[]]))
-# def Determinant(matr):
-#     k=0
-#     if len(matr) > 2:
-#         for count in range(len(matr)):
-#             matr2_0=copy.deepcopy(matr)
-#             RemoveS(matr2_0, count, 0)
-#             k=k+((-1)**(count+2))*matr[count][0] * \
-#               Determinant(matr2_0)
-#         return k
-#     else:
-#         return matr[0][0] * matr[1][1]-\
-#                matr[0][1] * matr[1][0]
