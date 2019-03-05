507304afefb60f498526559983e80155b2ce5e7d
Hedgehog7 <antonovalv98@mail.ru>
2019-03-05 22:51:05 +0300

    BrSeq done

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,6 +3,39 @@
 
 
 def is_bracket_correct(input_string):
+    br_list = list()
+    for i in input_string:
+        if i == '(' or i == '[' or i == '{':
+            br_list.append(i)
+        if i == ')':
+            if len(br_list) != 0:
+                if br_list[len(br_list) - 1] == '(':
+                    br_list.pop()
+                else:
+                    return False
+            else:
+                return False
+        if i == ']':
+            if len(br_list) != 0:
+                if br_list[len(br_list) - 1] == '[':
+                    br_list.pop()
+                else:
+                    return False
+            else:
+                return False
+        if i == '}':
+            if len(br_list) != 0:
+                if br_list[len(br_list) - 1] == '{':
+                    br_list.pop()
+                else:
+                    return False
+            else:
+                return False
+    if len(br_list) == 0:
+        return True
+    else:
+        return False
+
     '''
     Метод проверяющий является ли поданная скобочная
      последовательность правильной (скобки открываются и закрываются)
@@ -10,4 +43,3 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
-    raise NotImplementedError
