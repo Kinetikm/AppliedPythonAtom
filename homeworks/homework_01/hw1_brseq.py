3c4c90f5ca88d2a2abeac838e918ea14f8433562
MaximAbramenko <gotosleepmcht@gmail.com>
2019-03-05 12:23:17 +0300

    skobki

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,34 +3,26 @@
 
 
 def is_bracket_correct(input_string):
+    list_bracket = []
+    for i in input_string:
+        if i == "(" or i == "[" or i == "{":
+            list_bracket.append(i)
+        elif i == ")":
+            if list_bracket.pop() != "(":
+                return False
+        elif i == "]":
+            if list_bracket.pop() != "[":
+                return False
+        elif i == "}":
+            if list_bracket.pop() != "{":
+                return False
+        else:
+            return False
+    return True
     '''
     Метод проверяющий является ли поданная скобочная
      последовательность правильной (скобки открываются и закрываются)
      не пересекаются
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
-    '''
-    if len(input_string) % 2 != 0:
-        return False
-    i = 0
-    while i < len(input_string)-1:
-        if input_string[i] == "(":
-            if input_string[i+1] == ")":
-                i = i + 2
-                continue
-            else:
-                return False
-        if input_string[i] == "[":
-            if input_string[i+1] == "]":
-                i = i + 2
-                continue
-            else:
-                return False
-        if input_string[i] == "{":
-            if input_string[i+1] == "}":
-                i = i + 2
-                continue
-            else:
-                return False
-        return False
-    return True
+    '''   
