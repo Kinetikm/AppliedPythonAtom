e2716bd13e273c90bf7d69c8902db412606e2972
electron97 <ars2d2@yandex.ru>
2019-03-05 03:29:05 +0300

    brseq changed again..

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,19 +3,88 @@
 
 
 def is_bracket_correct(input_string):
+    if input_string == '':
+        return True
+    if input_string[0] == ')' or input_string == ']' or input_string == '}' or len(input_string) % 2 == 1:
+        return False
+
+    def one(str):
+        kr = 1
+        kv = 0
+        fi = 0
+        for symb in str:
+            if symb == '[':
+                kv += 1
+            elif symb == '{':
+                fi += 1
+            elif symb == '(':
+                kr += 1
+            elif symb == ')':
+                kr -= 1
+            elif symb == ']':
+                kv -= 1
+            elif symb == '}':
+                fi -= 1
+            if kv < 0 or fi < 0:
+                return False
+            if kr == 0:
+                return kv == 0 and fi == 0
+        return kv == 0 and fi == 0 and kr == 0
+
+    def two(str):
+        kr = 0
+        kv = 1
+        fi = 0
+        for symb in str:
+            if symb == '[':
+                kv += 1
+            elif symb == '{':
+                fi += 1
+            elif symb == '(':
+                kr += 1
+            elif symb == ']':
+                kv -= 1
+            elif symb == ')':
+                kr -= 1
+            elif symb == '}':
+                fi -= 1
+            if kr < 0 or fi < 0:
+                return False
+            if kv == 0:
+                return kr == 0 and fi == 0
+        return kv == 0 and fi == 0 and kr == 0
+
+    def three(str):
+        kr = 0
+        kv = 0
+        fi = 1
+        for symb in str:
+            if symb == '[':
+                kv += 1
+            elif symb == '{':
+                fi += 1
+            elif symb == '(':
+                kr += 1
+            elif symb == ')':
+                kr -= 1
+            elif symb == ']':
+                kv -= 1
+            elif symb == '}':
+                fi -= 1
+            if kv < 0 or kr < 0:
+                return False
+            if fi == 0:
+                return kr == 0 and fi == 0
+        return kv == 0 and fi == 0 and kr == 0
+
     result = True
-    i = 1
-    for symbol in input_string[:len(input_string) // 2]:
-        if symbol == '(':
-            if ')' != input_string[len(input_string) - i]:
-                result = False
-        elif symbol == "[":
-            if ']' != input_string[len(input_string) - i]:
-                result = False
-        elif symbol == '{':
-            if "}" != input_string[len(input_string) - i]:
-                result = False
-        else:
-            return False
-        i += 1
+    for i in range(len(input_string)):
+        if input_string[i] == '(':
+            result = one(input_string[i + 1:])
+        elif input_string[i] == '[':
+            result = two(input_string[i + 1:])
+        elif input_string[i] == '{':
+            result = three(input_string[i + 1:])
+        if result == False:
+            return result
     return result
