98c4eaf104d08951444e170bbe3fc77bdd3a2eb5
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-05 22:26:25 +0300

    Homework changes

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -3,7 +3,7 @@
 
 
 def check_palindrom(input_string):
-    if type(input_string)==str:
+    if type(input_string) == str:
         strBuf = input_string[::-1]
         if strBuf == input_string:
             return True
