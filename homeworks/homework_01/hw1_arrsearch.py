af81f607c0e6c5fb9585c77d48d60a7e1ae51889
Hedgehog7 <antonovalv98@mail.ru>
2019-03-05 22:56:59 +0300

    ArrSearch done

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,6 +3,19 @@
 
 
 def find_indices(input_list, n):
+    di = {}
+    if len(input_list) == 0:
+        return None
+    else:
+        for i in range(len(input_list)):
+            di[int(input_list[i])] = i
+
+        for j in range(len(input_list) - 1):
+            a = int(input_list[j])
+            if (n - a) in di.keys() and di[n - a] != j:
+                return j, di[n - a]
+        return None
+
     '''
     Метод возвращает индексы двух различных
     элементов listа, таких, что сумма этих элементов равна
@@ -13,4 +26,5 @@ def find_indices(input_list, n):
     :param n: целевая сумма
     :return: tuple из двух индексов или None
     '''
-    raise NotImplementedError
+
+# raise NotImplementedError
