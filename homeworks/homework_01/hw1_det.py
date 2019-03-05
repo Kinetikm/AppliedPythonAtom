79764595f0ea242103087ec6e16c08eaa59fc0dd
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-06 01:07:36 +0300

    Homework changes

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,13 +2,21 @@
 # coding: utf-8
 
 
+import copy
+def RemoveS(matr, i, j):
+    del matr[i]
+    for count in range(len(matr)):
+        del matr[count][j]
+
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    k=0
+    if len(list_of_lists) > 2:
+        for count in range(len(list_of_lists)):
+            matr2_0=copy.deepcopy(list_of_lists)
+            RemoveS(matr2_0, count, 0)
+            k=k+((-1)**(count+2)) * list_of_lists[count][0] * \
+              Determinant(matr2_0)
+        return k
+    else:
+        return matr[0][0] * list_of_lists[1][1]-\
+               list_of_lists[0][1] * list_of_lists[1][0]
