013c450214b728227ba55a825a318eb56944228a
NSKuznetsov <helloqwtheslayer@mail.ru>
2019-03-05 17:32:58 +0300

    hw1_brseq

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -10,4 +10,16 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
+    if len(input_string) % 2:
+        return False
+    else:
+        s = input_string
+        for i in range(int(len(input_string) / 2)):
+            a = s.replace('{}', '')
+            b = a.replace('()', '')
+            s = b.replace('[]', '')
+        if s == '':
+            return True
+        else:
+            return False
     raise NotImplementedError
