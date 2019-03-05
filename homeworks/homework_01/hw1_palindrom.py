77f9c53326fad768dd662648283cb5d03ed233ad
Hedgehog7 <antonovalv98@mail.ru>
2019-03-05 22:55:39 +0300

    Palindrom done

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -3,6 +3,13 @@
 
 
 def check_palindrom(input_string):
+    length = len(input_string)
+    flag = True
+    for i in range(0, length // 2):
+        if input_string[i] != input_string[length - i - 1]:
+            flag = False
+            break
+    return flag
     '''
     Метод проверяющий строку на то, является ли
     она палиндромом.
@@ -10,4 +17,4 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    raise NotImplementedError
+    # raise NotImplementedError
