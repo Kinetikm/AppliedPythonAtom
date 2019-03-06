44f0034d0ff23428db13b0720bb577406223d4e9
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-06 22:39:40 +0300

    надеюсь последняя правка

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -2,16 +2,16 @@
 
 def calculator(x, y, operator):
     if format(type(x)) == format(type(1))\
-        or format(type(x)) == format(type(1.0)):
-            if format(type(y)) == format(type(1)) or\
+            or format(type(x)) == format(type(1.0)):
+        if format(type(y)) == format(type(1)) or\
                 format(type(y)) == format(type(1.0)):
-                    if operator == "plus":
-                        return x + y
-                    if operator == "minus":
-                        return x - y
-                    if operator == "mult":
-                        return x * y
-                    if operator == "divide":
-                        if y != 0:
-                            return x / y
+            if operator == "plus":
+                return x + y
+            if operator == "minus":
+                return x - y
+            if operator == "mult":
+                return x * y
+            if operator == "divide":
+                if y != 0:
+                    return x / y
     return None
