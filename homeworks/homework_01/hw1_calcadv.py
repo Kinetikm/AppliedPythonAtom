903bd7f3386e6f893d3ff76a5b379d8de3ce73b1
Astapov <38396330+iastapov17@users.noreply.github.com>
2019-03-06 14:31:59 +0200

    Update hw1_calcadv.py

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 def advanced_calculator(input_string):
-	if input_string.isalpha() or len (input_string) == 0:
-		return None
-	bad = ['**','//','%','<<','>>','&','|','<','>','<=','>=','==','not','and','or']
-	for i in bad:
-		if (input_string.find(i) != -1):
-			return None
-	try :
-		x = eval(input_string)
-	except:
-		return None
-	return x if (type(x) is int or type(x) is float) else None
+    if input_string.isalpha() or len (input_string) == 0:
+        return None
+    bad = ['**','//','%','<<','>>','&','|','<','>','<=','>=','==','not','and','or']
+    for i in bad:
+        if (input_string.find(i) != -1):
+            return None
+    try :
+        x = eval(input_string)
+    except:
+        return None
+    return x if (type(x) is int or type(x) is float) else None
