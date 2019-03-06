460da4908100e9e65f8493fde273c054d17faf41
duckyduckk <kolya1996rus@mail.ru>
2019-03-06 20:14:01 +0300

    lst

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,13 +2,21 @@
 # coding: utf-8
 
 
-def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+def calculate_determinant(mat):
+    from copy import deepcopy as dcopy
+    if mat == [[]]:
+        return None
+    for i in mat:
+        if len(i) != len(mat):
+            return None
+    if len(mat) * len(mat[0]) == 1:
+        det = mat[0][0]
+        return det
+    det = 0
+    for i in range(len(mat)):
+        minor = dcopy(mat[1:])
+        for j in minor:
+            # print(minor)
+            j.remove(j[i])
+        det = det + (-1) ** i * (mat[0][i] * calculate_determinant(minor))
+    return det
