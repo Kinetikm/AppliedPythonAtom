96002dcb29278ab360dd1b421df785f288734637
Astapov <38396330+iastapov17@users.noreply.github.com>
2019-03-06 14:31:32 +0200

    Update hw1_calculator.py

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 def calculator(x, y, operator):
-	try:
-		x = float(x)
-		y = float(y)
-	except:
-		return None
-	if ( operator == 'plus' ):
-		return x + y
-	elif ( operator == 'minus' ):
-		return x - y
-	elif ( operator == 'mult' ):
-		return x * y
-	elif ( operator == 'divide' ):
-		if ( y != 0):
-			return x / y
-	return None
+    try:
+        x = float(x)
+        y = float(y)
+    except:
+        return None
+    if ( operator == 'plus' ):
+        return x + y
+    elif ( operator == 'minus' ):
+        return x - y
+    elif ( operator == 'mult' ):
+        return x * y
+    elif ( operator == 'divide' ):
+        if ( y != 0):
+            return x / y
+    return None
