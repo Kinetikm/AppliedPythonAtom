e039ab05453ee477e454f239570e37837ce16aa3
Astapov <38396330+iastapov17@users.noreply.github.com>
2019-03-06 14:44:40 +0200

    Update hw1_invertint.py

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -4,13 +4,13 @@
 def reverse(number):
     res = 0
     flag = False
-    if ( number < 0 ):
+    if (number < 0):
         flag = True
         number = abs(number)
-    while ( number > 0):
+    while (number > 0):
         temp = number % 10
         res = (res * 10) + temp
-        number = number // 10     
+        number = number // 10
     if flag:
         return -res
     else:
