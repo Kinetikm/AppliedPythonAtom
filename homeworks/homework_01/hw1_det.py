2a858acfda1f77abaec3eb27e30ac254fed7225f
electron97 <ars2d2@yandex.ru>
2019-03-06 18:10:15 +0300

    det changed

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,33 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+
+    def rezat(arr, j):
+        def func(a):
+            return a[:j] + a[j + 1:]
+
+        ar = list(map(func, arr[1:]))
+        return ar
+
+    def det(arr):
+        d = 0
+        if len(arr) != len(arr[0]):
+            return None
+        if len(arr) == 1:
+            return arr[0][0]
+        elif len(arr) == 2:
+            d += (arr[0][0] * arr[1][1] - arr[0][1] * arr[1][0])
+        else:
+            one_plus_minus = -1
+            i = 0
+            for elem in arr[0]:
+                one_plus_minus *= -1
+                d += one_plus_minus * elem * det(rezat(arr, i))
+                i += 1
+
+        return d
+
+    return det(list_of_lists)
+
+
+print(calculate_determinant([[58.472, 222.501], [-233.389, -47.711]]))
