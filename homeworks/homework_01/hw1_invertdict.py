12be51bf6cd8965fb755d47fc50a413aa82f6e99
Astapov <38396330+iastapov17@users.noreply.github.com>
2019-03-06 14:30:38 +0200

    Update hw1_invertdict.py

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 def getvalues(v):
     res = []
     if type(v) in [list, tuple, set]:
@@ -13,21 +14,21 @@ def getvalues(v):
     else:
         res = [v]
     return res
-	
+    
 def invert_dict(source_dict):
-	res = {}
-	if ( type(source_dict) is not dict ):
-		return res
-	for k, v in source_dict.items():
-		values = getvalues(v)
-		for v in values :
-			if v in res:
-				if ( type (res[v]) is not list ):
-					x = res[v]
-					res[v] = [x]
-					res[v].append(k)
-				else:
-					res[v].append(k)
-			else:
-				res[v] = k
-	return res
+    res = {}
+    if ( type(source_dict) is not dict ):
+        return res
+    for k, v in source_dict.items():
+        values = getvalues(v)
+        for v in values :
+            if v in res:
+                if ( type (res[v]) is not list ):
+                    x = res[v]
+                    res[v] = [x]
+                    res[v].append(k)
+                else:
+                    res[v].append(k)
+            else:
+                res[v] = k
+    return res
