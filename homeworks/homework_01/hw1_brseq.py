cc15f32fc54497546489319394e56a3db1971cc3
avoklovaetak <48108442+avoklovaetak@users.noreply.github.com>
2019-03-06 23:50:18 +0300

    Update hw1_brseq.py

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,11 +3,15 @@
 
 
 def is_bracket_correct(input_string):
-    '''
-    Метод проверяющий является ли поданная скобочная
-     последовательность правильной (скобки открываются и закрываются)
-     не пересекаются
-    :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
-    :return: True or False
-    '''
-    raise NotImplementedError
+    br1 = '([{'
+    br2 = ')]}'
+    pairs = []
+    for symbol in input_string:
+        if symbol in br1:
+            pairs.append(br1.index(symbol))
+        elif symbol in br2:
+            if pairs and pairs[-1] == br2.index(symbol):
+                pairs.pop()
+            else:
+                return False
+    return not pairs
