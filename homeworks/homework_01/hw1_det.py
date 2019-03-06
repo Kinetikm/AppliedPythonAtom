333cd3ee79e9b1591c5365f1fc61b9741ad46465
Astapov <38396330+iastapov17@users.noreply.github.com>
2019-03-06 14:31:02 +0200

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,19 +2,20 @@
 # coding: utf-8
 import copy
 
+
 def calculate_determinant(list_of_lists):
-	res = 0
-	m = len ( list_of_lists )
-	for i in list_of_lists:
-		n = len( i )
-		if m != n:
-			return None
-	if n == 1:
-	    return list_of_lists[0][0]
-	for i in range(n):
-		x = copy.deepcopy(list_of_lists)
-		del x[0]
-		for j in range(m - 1):
-			del x[j][i]
-		res += ((-1)**(i)) * list_of_lists[0][i] * calculate_determinant( x )
-	return res  
+    res = 0
+    m = len ( list_of_lists )
+    for i in list_of_lists:
+        n = len( i )
+        if m != n:
+            return None
+    if n == 1:
+        return list_of_lists[0][0]
+    for i in range(n):
+        x = copy.deepcopy(list_of_lists)
+        del x[0]
+        for j in range(m - 1):
+            del x[j][i]
+        res += ((-1)**(i)) * list_of_lists[0][i] * calculate_determinant( x )
+    return res  
