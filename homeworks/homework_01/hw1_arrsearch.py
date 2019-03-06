2e510f94293fdb69c8156b82b01c771bbebdeb8e
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-06 15:01:50 +0300

    Homework changes

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -6,8 +6,8 @@ def find_indices(input_list, n):
     MyDict = {}
     for j in range(len(input_list)):
         k = input_list[j]
-        if k not in MyDict:
-            MyDict.update({k: j})
         if MyDict.get(n-input_list[j]):
             return [MyDict.get(n-input_list[j]), j]
+        if k not in MyDict:
+            MyDict.update({k: j})
     return None
