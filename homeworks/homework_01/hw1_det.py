168a5534eeb11449a885d2d5969fe4b4e60f370c
LubimDobrynchuk <Bim2k10@mail.ru>
2019-03-07 02:14:15 +0300

    calc, skobki, invrt int, palindrom, determ

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
+import copy
+# list_of_lists = [[1, 4, 3], [4, 5, 6], [7, 8, 9]]
+
+
+def minor(list_of_list, i, j):
+    # print("1: ", list_of_list, i, j)
+    mtrx = copy.deepcopy(list_of_list)
+    del mtrx[i]
+    for i in range(len(list_of_list) - 1):
+        del mtrx[i][j]
+    return mtrx
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    # print(list_of_lists)
+    column = len(list_of_lists[0])
+    row = len(list_of_lists)
+    if column != row:
+        return None
+    if column == 1:
+        return list_of_lists[0][0]
+    det = 0
+    for j in range(column):
+        sigma = (-1) ** (2 + j)
+        det += list_of_lists[0][j] * sigma *\
+            calculate_determinant(minor(list_of_lists, 0, j))
+    return det
+# print(calculate_determinant(list_of_lists))
+# raise NotImplementedError
