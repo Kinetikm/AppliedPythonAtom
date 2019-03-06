f37f4f040db558e1d4b9c1615519d090c092fb64
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-06 21:09:24 +0300

    arrsearch

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -13,4 +13,11 @@ def find_indices(input_list, n):
     :param n: целевая сумма
     :return: tuple из двух индексов или None
     '''
+    hash_1 = {}
+    for i, val in enumerate(input_list):
+        hash_1[val] = i
+    for i, val in enumerate(input_list):
+        if (n - val) in hash_1 and hash_1[n - val] != i:
+            return tuple([i, hash_1.get(n - val)])
+    return None
     raise NotImplementedError
