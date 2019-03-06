62a38601ef74f73e79bb6d7d04da07beb709144f
Astapov <38396330+iastapov17@users.noreply.github.com>
2019-03-06 14:32:42 +0200

    Update hw1_arrsearch.py

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 def find_indices(input_list, n):
-	if ( input_list == None ) or (len ( input_list ) == 0 )  :
-		return None
-	res = []
-	i = 0
-	j = len( input_list ) - 1
-	input_list.sort()
-	while (i != j):
-		sum = input_list[i] + input_list[j]
-		if (sum > n):
-			j -= 1
-		elif (sum < n):
-			i += 1
-		else:
-			res.append(i)
-			res.append(j)
-			return tuple(res)
-	return None
+    if ( input_list == None ) or (len ( input_list ) == 0 )  :
+        return None
+    res = []
+    i = 0
+    j = len( input_list ) - 1
+    input_list.sort()
+    while (i != j):
+        sum = input_list[i] + input_list[j]
+        if (sum > n):
+            j -= 1
+        elif (sum < n):
+            i += 1
+        else:
+            res.append(i)
+            res.append(j)
+            return tuple(res)
+    return None
