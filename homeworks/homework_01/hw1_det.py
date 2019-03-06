5486ac6b8b1e7a432b7367d8de0f05b92d583e71
Antonova Lidia <antonovalv98@mail.ru>
2019-03-06 23:25:53 +0300

    DetMatrix Done

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import copy
+
 
 def calculate_determinant(list_of_lists):
-    '''
+    m_len = len(list_of_lists)
+    n_len = len(list_of_lists[0])
+    if m_len != n_len:
+        return None
+    if n_len == 1:
+        return list_of_lists[0][0]
+    sign = 1
+    det = 0
+    for j in range(n_len):
+        det += list_of_lists[0][j] * sign * calculate_determinant(
+            minor(list_of_lists, 0, j))
+        sign *= -1
+    return det
+
+
+def minor(list_of_lists, i, j):
+    minor_matrix = copy.deepcopy(list_of_lists)
+    del minor_matrix[i]
+    for i in range(len(list_of_lists[0]) - 1):
+        del minor_matrix[i][j]
+    return minor_matrix
+
+
+'''
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
     None
@@ -11,4 +36,3 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
