10a2a7855831b0dcc5f6901848251ea68c458d99
IgorKurylev <kurilevigor@mail.ru>
2019-03-06 20:50:11 +0300

    all except calc_adv

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -20,22 +20,4 @@ def find_indices(input_list, n):
                 continue
         else:
             continue
-
-    input_list.sort()
-    i, j = 0, len(input_list) - 1
-    print(i, j)
-    while i != j:
-        try:
-            sum = input_list[i] + input_list[j]
-            print(sum)
-        except IndexError:
-            return None
-        if sum < n:
-            i += 1
-        if sum > n:
-            j -= 1
-        if sum == n:
-            if i != j:
-                return i, j
-            return None
     return None
