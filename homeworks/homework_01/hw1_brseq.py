e1df78cbba4c57b1c368265cf22f63e940aff8a4
Анастасия <asyadyoma@yandex.ru>
2019-03-07 01:34:05 +0300

    “brseq”

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,4 +3,8 @@
 
 
 def is_bracket_correct(input_string):
-    raise NotImplementedError
+    while '()' in input_string or '[]' in input_string or '{}' in input_string:
+        input_string = input_string.replace('()', '')
+        input_string = input_string.replace('[]', '')
+        input_string = input_string.replace('{}', '')
+    return not input_string
