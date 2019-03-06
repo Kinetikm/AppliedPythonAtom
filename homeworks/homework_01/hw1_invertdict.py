eb74346e79ef60d20df04c83915d10977ddcaa5e
MaximAbramenko <gotosleepmcht@gmail.com>
2019-03-06 23:37:01 +0300

    dict

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -2,6 +2,16 @@
 # coding: utf-8
 
 
+def value_to_list(value):
+    result_list = []
+    if isinstance(value, list) or isinstance(value, set):
+        for i in value:
+            result_list += value_to_list(i)
+    else:
+        result_list.append(value)
+    return result_list
+
+
 def invert_dict(source_dict):
     '''
     Функция которая разворачивает словарь, т.е.
@@ -9,4 +19,18 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict
     '''
-    raise NotImplementedError
+    if not isinstance(source_dict, dict):
+        return None
+    new_dict = {}
+    for key, value in source_dict.items():
+        for i in value_to_list(value):
+            if i in new_dict:
+                if isinstance(new_dict[i], list):
+                    new_dict[i].append(key)
+                else:
+                    item = new_dict.get(i)
+                    new_dict[i] = [item, key]
+            else:
+                new_dict[i] = key
+    return new_dict
+    
