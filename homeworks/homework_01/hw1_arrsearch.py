02d84f184bd226901a9998ade0848c422a015d47
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-06 23:57:06 +0300

    добавил перенос строк

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -4,11 +4,13 @@
 
 def find_indices(input_list, n):
     for a in input_list:
-        if (n-a) in input_list and input_list.index(a) != input_list.index(n-a):
+        if (n-a) in input_list and \
+                input_list.index(a) != input_list.index(n-a):
             return input_list.index(a), input_list.index(n-a)
-        if (n-a) in input_list and input_list.index(a) == input_list.index(n-a):
+        if (n-a) in input_list and \
+                input_list.index(a) == input_list.index(n-a):
             t = input_list.index(a)
-            input_list[t]=[]
+            input_list[t] = []
             if (n-a) in input_list:
                 return t, input_list.index(n-a)
     return None
