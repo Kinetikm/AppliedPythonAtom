f2f12e0e9ca95ce9a0c81e4d9ec40364caa77f26
duckyduckk <kolya1996rus@mail.ru>
2019-03-06 16:29:28 +0300

    try555

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -3,14 +3,14 @@
 
 
 def calculator(x, y, operator):
-    if (type(x) == (int or float)) and (type(y) == (int or float)):
-        if (operator == 'plus'):
+    if (type(x) in [float, int]) and (type(y) in [float, int]):
+        if (operator in ['plus', "+"]):
             return (x + y)
-        elif operator == "minus":
+        elif operator in ["minus", "-"]:
             return x - y
-        elif operator == "mult":
+        elif operator in ["mult", "*"]:
             return x * y
-        elif operator == 'divide':
+        elif operator in ['divide', "/"]:
             if y != 0:
                 return (x / y)
             else:
