485d78a07fb7e142badca1408cd4c352c67fc1a3
IgorKreknin <Ikreknin@bk.ru>
2019-03-06 10:46:14 +0300

    Homework changes

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -2,6 +2,16 @@
 # coding: utf-8
 
 
+def open_value(value):
+    myList = []
+    if type(value) is set or type(value) is tuple or type(value) is list:
+        for item in value:
+            myList += open_value(item)
+    else:
+        return [value]
+    return myList
+
+
 def invert_dict(source_dict):
     '''
     Функция которая разворачивает словарь, т.е.
@@ -10,7 +20,16 @@ def invert_dict(source_dict):
     :return: new_dict: dict
     '''
     myDict = {}
+
     for key in source_dict:
-    	myDict[source_dict[key]] = key
+        openedValue = open_value(source_dict[key])
+        for item in openedValue:
+            try:
+                myDict[item].append(key)
+            except KeyError:
+                myDict[item] = key
+            except AttributeError:
+                myDict[item] = [myDict[item]]
+                myDict[item].append(key)
     return myDict
     raise NotImplementedError
