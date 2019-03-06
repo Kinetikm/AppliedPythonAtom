973714f5542b045389da6ec48e7e33b8c10cd9f5
= <danila.gamkov@mail.ru>
2019-03-06 23:41:38 +0300

    calculator

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,20 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
-    raise NotImplementedError
+    num1 = type(x)
+    num2 = type(y)
+    if num1 == int or num1 == float or num1 == complex:
+        if num2 == int or num2 == float or num2 == complex:
+            if operator == "plus":
+                return x + y
+            if operator == "minus":
+                return x - y
+            if operator == "mult":
+                return x * y
+            if operator == "divide":
+                if y != 0:
+                    return x / y
+                else:
+                    return None
+    else:
+        return None
