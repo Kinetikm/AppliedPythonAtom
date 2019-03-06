abba30886effb308e1b48d620dfbe9d6a5ed5402
defeizer <artem.kamanov@gmail.com>
2019-03-07 02:22:09 +0300

    Каманов Артем

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -16,11 +16,3 @@ def is_bracket_correct(input_string):
         return True
     else:
         return False
-    '''
-    Метод проверяющий является ли поданная скобочная
-     последовательность правильной (скобки открываются и закрываются)
-     не пересекаются
-    :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
-    :return: True or False
-    '''
-    raise NotImplementedError
