449345f8e0df8386fbddc77ad9d2da68dd998c0a
avoklovaetak <48108442+avoklovaetak@users.noreply.github.com>
2019-03-06 21:00:36 +0300

    Update hw1_arrsearch.py

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -5,7 +5,8 @@
 def find_indices(input_list, n):
         for i in input_list:
                 k = n - i
-                if k in input_list and input_list.index(i) != input_list.index(k):
+                if k in input_list and\
+                        input_list.index(i) != input_list.index(k):
                         a = (input_list.index(i), input_list.index(k))
-                return a
+                        return a
         return None
