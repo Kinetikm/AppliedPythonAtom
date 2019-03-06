8dd7e6b4d2c39ae5cfc7c6c2cda9e628bd6bdeb0
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-06 14:40:02 +0300

    Homework changes

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,11 +3,14 @@
 
 
 def find_indices(input_list, n):
-    '''
-    Метод проверяющий является ли поданная скобочная
-     последовательность правильной (скобки открываются и закрываются)
-     не пересекаются
-    :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
-    :return: True or False
-    '''
-    raise NotImplementedError
+  MyDict = {}
+  for j in range(len(input_list)):
+      k = input_list[j]
+      if k not in MyDict:
+          MyDict.update({k: j})
+      if MyDict.get(n-input_list[j]):
+          return [MyDict.get(n-input_list[j]), j]
+  return None
+
+a = [2, 1, 4, 5, 7, 5, 1, 5, 8, 354, 23, 4356, 3421, 6, 321, 4]
+print(find_indices(a, 4362))
