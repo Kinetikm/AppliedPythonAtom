59cb969e9662b8a647caade164ee5c7abe867db5
aHDPik <andreyrud@inbox.lv>
2019-03-06 22:38:00 +0300

    Modified adv calc

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 ops = set("*/+-")
+low_priority = set("+-")
+high_priority = set("*/")
+brackets = set("()")
 
 def advanced_calculator(input_string):
     '''
@@ -11,14 +14,15 @@ def advanced_calculator(input_string):
     :param input_string: строка, содержащая выражение
     :return: результат выполнение операции, если строка валидная - иначе None
     '''
+    input_string = to_polish(input_string)
     parts = input_string.split(' ')
     stack = list()
     for part in parts:
-        if not part in ops:
+        if not (part in ops):
             stack.append(float(part))
         elif len(stack)>1:
-            a = stack.pop(-1)
             b = stack.pop(-1)
+            a = stack.pop(-1)
             if part=='+':
                 stack.append(a+b)
             elif part=='-':
@@ -33,3 +37,32 @@ def advanced_calculator(input_string):
         return stack[0]
     else:
         return None
+
+def to_polish(infix_string):
+    parts = infix_string.split(' ')
+    stack = list()
+    res=''
+    opstack = list()
+    for part in parts:
+        if not part in ops and not part in brackets:
+            res+=part+' '
+        elif part == '(':
+            stack.append(part)
+        elif (part==')'):
+            lastPart=stack.pop()
+            while lastPart!='(' and len(stack)>0:
+                res+=lastPart+' '
+                lastPart=stack.pop()
+            if lastPart!='(':
+                return None
+        elif part in ops:
+            while (part in low_priority) and len(stack)>0 and (stack[-1] in high_priority):
+                res+=stack.pop()+" "
+            stack.append(part)
+    while len(stack)>0:
+        lastPart=stack.pop()
+        if lastPart!='(':
+            res+=lastPart+" "
+        else:
+            return None
+    return res.strip()
