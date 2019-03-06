1a802897301bd49f738dafec995ade0d9846cdfe
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-06 23:53:31 +0300

    правка кода

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -4,8 +4,11 @@
 
 def find_indices(input_list, n):
     for a in input_list:
-        if (n-a) in input_list:
-            return input_list.index(a),input_list.index(n-a)
-        else:
-            return None
+        if (n-a) in input_list and input_list.index(a) != input_list.index(n-a):
+            return input_list.index(a), input_list.index(n-a)
+        if (n-a) in input_list and input_list.index(a) == input_list.index(n-a):
+            t = input_list.index(a)
+            input_list[t]=[]
+            if (n-a) in input_list:
+                return t, input_list.index(n-a)
     return None
