93279d69dbe6217519541efc3d73c81a614f7f70
bsnsky <busyansky@bk.ru>
2019-03-06 22:55:33 +0300

    andanotherone

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -7,7 +7,8 @@ def find_indices(input_list, n):
     for i in range(0, len(input_list)):
         temp = n - input_list[i]
         if temp >= 0 and temp in s:
-            first_index = input_list.index(input_list[i])
-            second_index = input_list.index(temp)
-            return second_index, first_index
+            first_index = input_list.index(temp)
+            second_index = input_list.index(input_list[i])
+            return first_index, second_index
         s.add(input_list[i])
+    return None
