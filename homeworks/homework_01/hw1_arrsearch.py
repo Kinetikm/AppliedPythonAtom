d95048fda2a81a4cca3ef889b40e34ecb23e448b
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-06 22:06:47 +0300

    очередная правка find_indices

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -4,6 +4,6 @@ def find_indices(input_list, n):
     for a in input_list:
         if (n-a) in input_list:
             return input_list.index(a),\
-        input_list.index(n-a)
+                    input_list.index(n-a)
         else:
             return None
