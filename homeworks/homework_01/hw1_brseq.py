4143c0f0c4438650821c68473accbf327df9cbed
Анастасия <asyadyoma@yandex.ru>
2019-03-06 22:49:38 +0300

    “ups2.0”

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,20 +3,4 @@
 
 
 def is_bracket_correct(input_string):
-    # i = 0
-    #    n = 0
-    #    if (len(input_string) % 2) == 1:
-    #        return False
-    #    else:
-    #        while i < (len(input_string) / 2):
-    #            if (input_string[i] == '(' and input_string[- (i + 1)] == ')') or (
-    #                    input_string[i] == '[' and input_string[- (i + 1)] == ']') or (
-    #                    input_string[i] == '{' and input_string[- (i + 1)] == '}'):
-    #                n += 1
-    #            i += 1
-    #        if n == len(input_string) / 2:
-    #            return True, n
-    #        else:
-    #            return False, n
-
     raise NotImplementedError
