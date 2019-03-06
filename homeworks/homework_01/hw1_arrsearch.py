ea443581667347134e5393b70ecb9d2dbf4baf6f
avoklovaetak <48108442+avoklovaetak@users.noreply.github.com>
2019-03-06 19:27:19 +0300

    Update hw1_arrsearch.py

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,17 +3,7 @@
 
 
 def find_indices(input_list, n):
-    '''
-    Метод возвращает индексы двух различных
-    элементов listа, таких, что сумма этих элементов равна
-    n. В случае, если таких элементов в массиве нет,
-    то возвращается None
-    Ограничение по времени O(n)
-    :param input_list: список произвольной длины целых чисел
-    :param n: целевая сумма
-    :return: tuple из двух индексов или None
-    '''
-    for i in input_list:
+        for i in input_list:
         k = n - i
         if k in input_list and input_list.index(i) != input_list.index(k)  :
             a = (input_list.index(i), input_list.index(k))
