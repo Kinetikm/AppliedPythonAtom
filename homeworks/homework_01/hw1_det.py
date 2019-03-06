f1c2c1bc238477f00199885bcf8084176f52baf5
Егор Сенчук <senchuk_egor@mail.ru>
2019-03-07 00:40:37 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    res = 0
+    m = len(list_of_lists)
+    for i in list_of_lists:
+        n = len(i)
+        if m != n:
+           return None
+    if n == 1:
+       return list_of_lists[0][0]
+    i = 0
+    while i < len(list_of_lists):
+        sup_matr = list_of_lists[::]
+        sup_matr.pop(0)
+        j = 0
+        while j < len(sup_matr):
+            sup_matr[j].pop(i)
+            j += 1
+        res += list_of_lists[0][i] * (1 ** i) * calculate_determinant(sup_matr)
+        i += 1
+    return res
