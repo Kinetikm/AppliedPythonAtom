5002386583b5695c57ade8244b313612dadee970
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-07 03:27:53 +0300

    Update hw1_invertdict.py

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -10,8 +10,9 @@ def invert_dict(source_dict):
         else:
             arr.append(value)
         return arr
-
     new_dict = {}
+    if source_dict == '':
+        return new_dict
     for key, val in source_dict.items():
         z = []
         for i in internal(val, z):
