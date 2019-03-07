2e284bbff61b8c7b764229fa78d0dc3d1879c1e5
Iuliia Letunova <letunova.yuliya@mail.ru>
2019-03-07 06:39:25 +0300

    start

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -9,24 +9,4 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict;
     '''
-    source_dict = dict(source_dict)
-    new_dict = {}
-    for key in source_dict.keys():
-        new_key = source_dict[key]
-        new_value = key
-        if ((type(new_key) == str) or 
-            (type(new_key) == float) or 
-            (type(new_key) == int)):
-            new_key = [new_key]
-        else:
-            new_key = list(new_key)
-        for new_new_key in new_key:
-            if new_new_key in new_dict:
-                new_dict[new_new_key].append(new_value)
-            else:
-                new_dict[new_new_key] = [new_value]
-    for key in new_dict.keys():
-        if len(new_dict[key]) == 1:
-            val = new_dict[key]
-            new_dict[key] = val[0]
-    return new_dict
+    raise NotImplementedError
