76f5cb30f6c4addaa288a8ca3d7b3acb6bb24b3a
HamsterMephi <Anti-demon@mail.ru>
2019-03-07 08:30:39 +0300

    Invertdict new verwtf

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -2,11 +2,30 @@
 # coding: utf-8
 
 
+def expand(a, b):
+    if (type(a) == tuple) or (type(a) == set) or (type(a) == list):
+        for i in a:
+            expand(i, b)
+    else:
+        b.append(a)
+
+
 def invert_dict(source_dict):
     if not isinstance(source_dict, dict):
         return None
-    intended_dict = {}
-    for k, v in source_dict.items():
-        for i in v:
-            intended_dict.setdefault(i, []).append(k)
-    return intended_dict
+    else:
+        new_dict = {}
+        for k, a in source_dict.items():
+            b = []
+            expand(a, b)
+            for i in range(len(b)):
+                if b[i] in new_dict.keys():
+                    if isinstance(new_dict[b[i]], list):
+                        new_dict[b[i]].append(k)
+                    else:
+                        new_dict[b[i]] = [new_dict[b[i]]]
+                        new_dict[b[i]].append(k)
+                else:
+                    new_dict[b[i]] = k
+        return new_dict
+    raise NotImplementedError
