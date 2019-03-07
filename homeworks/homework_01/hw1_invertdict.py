77b7965f348a4958a365d874392c82cedb7cb508
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-07 03:14:10 +0300

    добавление

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 def invert_dict(source_dict):
-    '''
-    Функция которая разворачивает словарь, т.е.
-    каждому значению ставит в соответствие ключ.
-    :param source_dict: dict
-    :return: new_dict: dict
-    '''
-    raise NotImplementedError
+    def internal(value):
+        internal_arr = []
+        if isinstance(value, (list, tuple, set)):
+            for v in value:
+                internal_arr.append(internal(v))
+        else:
+            internal_arr.append(value)
+        return internal_arr
+
+    new_dict = {}
+    for key, val in source_dict.items():
+        for i in internal(val):
+            if i in new_dict:
+                if isinstance(new_dict[i], list):
+                    new_dict[i].append(key)
+                else:
+                    new_dict[i] = [new_dict[i]]
+                    new_dict[i].append(key)
+            else:
+                new_dict[i] = key
+    return new_dict
