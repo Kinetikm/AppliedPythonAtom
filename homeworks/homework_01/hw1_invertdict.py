2ae8140d2e17086035499700011a4add935cdad4
HamsterMephi <Anti-demon@mail.ru>
2019-03-07 08:16:02 +0300

    Invertdict new ver5

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -2,13 +2,11 @@
 # coding: utf-8
 
 
-def invert_dict(dict1):
-    intended_dict ={}
-    try:
-        for k, v in dict1.items():
-            for i in v:
-                intended_dict.setdefault(i, []).append(k)
-    except (AttributeError,TypeError):
-        return dict1
-    else:
-        return intended_dict
+def invert_dict(source_dict):
+    if not isinstance(source_dict, dict):
+        return None
+	intended_dict = {}
+    for k, v in dict1.items():
+        for i in v:
+            intended_dict.setdefault(i, []).append(k)
+	return intended_dict
