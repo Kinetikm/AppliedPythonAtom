6a412c4673fa121bbe088019ac3ae99b46fd2c05
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-07 03:19:59 +0300

    правка кода

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 def invert_dict(source_dict):
-    def internal(value):
-        internal_arr = []
+    def internal(value, arr):
         if isinstance(value, (list, tuple, set)):
             for v in value:
-                internal_arr.append(internal(v))
+                arr = (internal(v, arr))
         else:
-            internal_arr.append(value)
-        return internal_arr
+            arr.append(value)
+        return arr
 
     new_dict = {}
     for key, val in source_dict.items():
-        for i in internal(val):
+        z = []
+        for i in internal(val, z):
             if i in new_dict:
                 if isinstance(new_dict[i], list):
                     new_dict[i].append(key)
