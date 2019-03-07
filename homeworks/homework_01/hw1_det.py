6805d1f6fbf42ebfd2385055c5cc41d0e30d8a7b
ArtySad <48071922+ArtySad@users.noreply.github.com>
2019-03-07 03:49:47 +0300

    Поправлен формат исходника

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -8,18 +8,20 @@ def calculate_determinant(list_of_lists):
     if size > 1:
         res = 1
         for i in range(size):
-            if len(list_of_lists[i])!=size:
+            if len(list_of_lists[i]) != size:
                 return None
             j = max(range(i, size), key=lambda k: abs(list_of_lists[k][i]))
             if i != j:
-                list_of_lists[i], list_of_lists[j] = list_of_lists[j], list_of_lists[i]
+                list_of_lists[i], list_of_lists[j]
+                = list_of_lists[j], list_of_lists[i]
                 res *= -1
             if list_of_lists[i][i] == 0:
                 return 0
             res *= list_of_lists[i][i]
             for j in range(i + 1, size):
                 b = list_of_lists[j][i] / list_of_lists[i][i]
-                list_of_lists[j] = [list_of_lists[j][k] - b * list_of_lists[i][k] for k in range(size)]
+                list_of_lists[j] = [list_of_lists[j][k]
+                - b * list_of_lists[i][k] for k in range(size)]
         return res
     else:
         try:
