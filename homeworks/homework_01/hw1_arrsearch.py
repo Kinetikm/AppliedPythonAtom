51a4471f1b5476cc7edb005bc10848e77cca4612
LubimDobrynchuk <Bim2k10@mail.ru>
2019-03-07 04:03:13 +0300

    calc, skobki, invrt int, palindrom, determ, index

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,4 +3,21 @@
 
 
 def find_indices(input_list, n):
-    raise NotImplementedError
+    mas = input_list
+    sum = n
+    mas.sort()
+    levyi = 0
+    pravyi = len(mas) - 1
+    if len(mas) == 0 or len(mas) == 1:
+        return None
+    while levyi != pravyi:
+        g = mas[levyi] + mas[pravyi]
+        if g < sum:
+            levyi += 1
+        elif g > sum:
+            pravyi -= 1
+        else:
+            return levyi, pravyi
+            break
+    if levyi == pravyi and g != mas[levyi] + mas[pravyi]:
+        return None
