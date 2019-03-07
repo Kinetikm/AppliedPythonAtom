1721b77d51e7bfecd169b858f54b8f5844f8a78c
ArtySad <48071922+ArtySad@users.noreply.github.com>
2019-03-07 04:08:29 +0300

    Переносы строк

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,8 +3,7 @@
 
 
 def calculate_determinant(list_of_lists):
-    print(list_of_lists)
-    size = len(list_of_lists)
+      size = len(list_of_lists)
     if size > 1:
         res = 1
         for i in range(size):
@@ -12,20 +11,20 @@ def calculate_determinant(list_of_lists):
                 return None
             j = max(range(i, size), key=lambda k: abs(list_of_lists[k][i]))
             if i != j:
-                list_of_lists[i], list_of_lists[j] \
-                = list_of_lists[j], list_of_lists[i]
+                list_of_lists[i], list_of_lists[j]\
+                    = list_of_lists[j], list_of_lists[i]
                 res *= -1
             if list_of_lists[i][i] == 0:
                 return 0
             res *= list_of_lists[i][i]
             for j in range(i + 1, size):
                 b = list_of_lists[j][i] / list_of_lists[i][i]
-                list_of_lists[j] = [list_of_lists[j][k] \
-                - b * list_of_lists[i][k] for k in range(size)]
+                list_of_lists[j]\
+                    = [list_of_lists[j][k] - b * list_of_lists[i][k] for k in range(size)]
         return res
     else:
         try:
-            if (size == len(list_of_lists[0])):
+            if size == len(list_of_lists[0]):
                 return list_of_lists[0][0]
         except TypeError:
             return None
