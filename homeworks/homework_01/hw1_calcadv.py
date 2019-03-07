17e0923810c8b368237583738f5d8a4b13bc3a7e
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-07 20:49:26 +0300

    Update hw1_calcadv.py

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -9,10 +9,13 @@ def advanced_calculator(input_string):
     s = s.replace(" ", "")
     i = 0
     while i < len(s):
-        if s[i] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", ")", "(", "-", "+", "*", "/"]:
+        if s[i] not in ["0", "1", "2", "3", "4", "5",\
+                "6", "7", "8", "9", ".", ",", ")",\
+                        "(", "-", "+", "*", "/"]:
             return None
         i += 1
-    for x in ['**', '*/', '/*', '//', '*+', '+*', '/+', '+/', '*-', '-*', '/-', '-/']:
+    for x in ['**', '*/', '/*', '//', '*+', '+*',\
+              '/+', '+/', '*-', '-*', '/-', '-/']:
         if x in s:
             return None
     s = s.replace("--", "+")
@@ -24,6 +27,6 @@ def advanced_calculator(input_string):
     if s[0] == '+':
         s = s[1::]
     try:
-        return eval(s)
+        return float(eval(s))
     except:
         return None
