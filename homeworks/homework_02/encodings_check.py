fe68371dff69e917045a7a183060514859d1f549
IgorKurylev <kurilevigor@mail.ru>
2019-03-10 02:01:01 +0300

    working with files

---
+++ homeworks/homework_02/encodings_check.py
@@ -0,0 +1,36 @@
+def is_utf8(f_name):
+    try:
+        with open(f_name, encoding='utf8') as f:
+            f.read(1)
+    except UnicodeError:
+        return False
+    return True
+
+
+def is_cp1251(f_name):
+    try:
+        with open(f_name, encoding='cp1251') as f:
+            f.read(1)
+    except UnicodeError:
+        return False
+    return True
+
+
+def is_utf16(f_name):
+    try:
+        with open(f_name, encoding='utf16') as f:
+            f.read(1)
+    except UnicodeError:
+        return False
+    return True
+
+
+def define_encoding(f_name):
+    if is_utf8(f_name):
+        return 'utf8'
+    elif is_utf16(f_name):
+        return 'utf16'
+    elif is_cp1251(f_name):
+        return 'cp1251'
+    else:
+        return None
--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,21 @@
 import sys
 
-# Ваши импорты
+
+from encodings_check import define_encoding
+from file_worker import check_is_exists, print_info
+from format_check import define_format
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        raise SystemExit("Argument does not exist")
 
-    # Ваш код
+    is_exist = check_is_exists(filename)
+    if not is_exist:
+        raise SystemExit("Unable to open file")
+    encode = define_encoding(filename)
+    if encode is None:
+        raise SystemError("Unknown encoding")
+    format = define_format(filename, encode)
+    print_info(filename, format, encode)
