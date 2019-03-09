5d1a624b079bb4bbe2fdf2c4de7beaf25476772d
IgorKurylev <kurilevigor@mail.ru>
2019-03-09 17:14:31 +0300

    vk_poster

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -9,55 +9,56 @@ from homeworks.homework_02.fastmerger import FastSortedListMerger
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.posts = {}
+        self.subs = {}
 
     def user_posted_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        выложил пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        if post_id not in self.posts:
+            self.posts[post_id] = [user_id, []]
 
     def user_read_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        прочитал пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        if post_id in self.posts.keys():
+            if user_id not in self.posts[post_id][1]:
+                self.posts[post_id][1].append(user_id)
+        else:
+            self.posts[post_id] = [[], [user_id]]
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-        '''
-        Метод который вызывается когда пользователь follower_user_id
-        подписался на пользователя followee_user_id.
-        :param follower_user_id: id пользователя. Число.
-        :param followee_user_id: id пользователя. Число.
-        :return: ничего
-        '''
-        pass
+        if follower_user_id not in self.subs.keys():
+            self.subs[follower_user_id] = [followee_user_id]
+        else:
+            if followee_user_id not in self.subs.get(follower_user_id):
+                self.subs[follower_user_id].append(followee_user_id)
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
-        '''
-        Метод который вызывается когда пользователь user_id
-        запрашивает k свежих постов людей на которых он подписан.
-        :param user_id: id пользователя. Число.
-        :param k: Сколько самых свежих постов необходимо вывести. Число.
-        :return: Список из post_id размером К из свежих постов в
-        ленте пользователя. list
-        '''
-        pass
+        res_list = []
+        if user_id in self.subs.keys():
+            for post in sorted(list(self.posts.keys()), reverse=True):
+                if self.posts[post][0] in self.subs[user_id]:
+                    if user_id not in self.posts[post][1]:
+                        res_list.append(post)
+                    if len(res_list) == k:
+                        return res_list
+        return res_list
 
     def get_most_popular_posts(self, k: int) -> list:
-        '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
-        :param k: Сколько самых свежих популярных постов
-        необходимо вывести. Число.
-        :return: Список из post_id размером К из популярных постов. list
-        '''
-        pass
+        popular = [(k, len(self.posts[k][1])) for k in
+                   sorted(self.posts, key=lambda i:
+                   len(self.posts.get(i)[1]), reverse=True)]
+        res_list = []
+        group_ind = popular[0][1]
+        while len(res_list) != k:
+            found = []
+            for elem in popular:
+                if elem[1] == group_ind:
+                    found.append(elem[0])
+            if len(found) == 0:
+                group_ind -= 1
+                continue
+            found = sorted(found, reverse=True)
+            for elem in found:
+                res_list.append(elem)
+                if len(res_list) == k:
+                    break
+            group_ind -= 1
+        return res_list
