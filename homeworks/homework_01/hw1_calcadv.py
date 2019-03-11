41099a877507f6ef24127c8296f5a5313fb9a99f
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-11 11:37:29 +0300

    another varient of calcadv

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -1,32 +1,98 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 def advanced_calculator(input_string):
-    s = input_string.strip()
-    if s == '':
-        return None
-    s = s.replace(" ", "")
-    i = 0
-    while i < len(s):
-        if s[i] not in ["0", "1", "2", "3", "4", "5",
-                        "6", "7", "8", "9", ".", ",", 
-                        ")", "(", "-", "+", "*", "/"]:
+    for i in range(10):
+        input_string = input_string.replace(str(i) + " ", str(i) + "|")
+    for x in [" ", "\t", "--", "++", "+-", "(-", "/-", "*-"]:
+        while x in input_string:
+            input_string = input_string.replace("\t", "").\
+                replace(" ", "")
+            input_string = input_string.replace("--", "+").\
+                replace("++", "+")
+            input_string = input_string.replace("+-", "-").\
+                replace("-+", "-")
+            input_string = input_string.replace("(-", "(0-")
+            input_string = input_string.replace("/-", "*(0-1)/").\
+                replace("*-", "*(0-1)*")
+    if len(input_string) > 0 and input_string[0] in ["-", "+"]:
+        input_string = "0" + input_string
+    item = ""
+    output = []
+    l = []
+    while len(input_string) > 0:
+        if input_string[0] in ["+", "-", "/", "*"]:
+            if len(item) > 0:
+                try:
+                    output.append(float(item))
+                    item = ""
+                except (TypeError, ValueError):
+                    return None
+            while len(l) > 0 and l[len(l) - 1] is not "(":
+                if l[len(l) - 1] in ['*', '/'] or\
+                        input_string[0] in ['+', '-']:
+                    output.append(l.pop())
+                else:
+                    break
+            l.append(input_string[0])
+            input_string = input_string[1::]
+        elif input_string[0] is "(":
+            if len(item) > 0:
+                return None
+            l.append("(")
+            input_string = input_string[1::]
+        elif input_string[0] is ")":
+            if len(item) == 0:
+                return None
+            try:
+                output.append(float(item))
+                item = ""
+            except (TypeError, ValueError):
+                return None
+            input_string = input_string[1::]
+            try:
+                item = l.pop()
+                while item is not "(":
+                    output.append(item)
+                    item = l.pop()
+                item = ""
+            except IndexError:
+                    return None
+        elif input_string[0].isdigit() or input_string[0] is ".":
+            item += input_string[0]
+            input_string = input_string[1::]
+        elif input_string[0] is "|":
+            output.append(float(item))
+            item = ""
+            input_string = input_string[1::]
+        else:
             return None
-        i += 1
-    for x in ['**', '*/', '/*', '//', '*+', '+*',
-              '/+', '+/', '*-', '-*', '/-', '-/']:
-        if x in s:
+    if len(item) > 0:
+        try:
+            output.append(float(item))
+            item = ""
+        except (ValueError, TypeError):
             return None
-    s = s.replace("--", "+")
-    s = s.replace("++", "+")
-    s = s.replace("+-", "-")
-    s = s.replace("-+", "-")
-    if s[0] == '-':
-        s = "0" + s
-    if s[0] == '+':
-        s = s[1::]
+    while len(l) > 0:
+        output.append(l.pop())
     try:
-        return eval(s)
-    except:
+        while len(output) > 0:
+            item = output.pop(0)
+            if isinstance(item, float):
+                l.append(item)
+            else:
+                a = l.pop()
+                b = l.pop()
+                if item is "+":
+                    l.append(b + a)
+                elif item is "-":
+                    l.append(b - a)
+                elif item is "/":
+                    try:
+                        l.append(b / a)
+                    except ZeroDivisionError:
+                        return None
+                elif item is "*":
+                    l.append(b * a)
+    except IndexError:
         return None
+    if len(l) != 1:
+        return None
+    return l[0]
