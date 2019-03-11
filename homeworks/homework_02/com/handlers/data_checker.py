c6570ace52956a8b811cc660791417aae0a2a8da
AsAsgard <dbrylkin96@gmail.com>
2019-03-11 08:37:24 +0300

    Check for empty table added

--- homeworks/homework_02/com/handlers/data_checker.py
+++ homeworks/homework_02/com/handlers/data_checker.py
@@ -4,6 +4,8 @@
 
 def checkData(data):
     validKeys = sorted(["Название", "Ссылка", "Теги", "Оценка"])
+    if len(data) == 0:
+        raise SyntaxError
     for element in data:
         if sorted(element.keys()) != validKeys:
             raise SyntaxError
