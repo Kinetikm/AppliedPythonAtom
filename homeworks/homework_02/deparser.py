6b053b08e98efc3f59d43c12082481f33f92ca4e
IgorKreknin <Ikreknin@bk.ru>
2019-03-12 22:03:26 +0300

    Homework changes

--- homeworks/homework_02/deparser.py
+++ homeworks/homework_02/deparser.py
@@ -4,15 +4,15 @@ import reader
 
 
 def depars(file):
-	try:
-		input_sting = reader.read(file)
-		try:
-			return (json.loads(input_sting), "JSON")
-		except ValueError:
-			input_data = input_sting.split("\n")
-			deparsedData = []
-			for string in input_data:
-				deparsedData.append(string.split("\t"))
-			return (deparsedData, "TSV")
-	except ValueError:
-		raise ValueError
+    try:
+        input_sting = reader.read(file)
+        try:
+            return (json.loads(input_sting), "JSON")
+        except ValueError:
+            input_data = input_sting.split("\n")
+            deparsedData = []
+            for string in input_data:
+                deparsedData.append(string.split("\t"))
+            return (deparsedData, "TSV")
+    except ValueError:
+        raise ValueError
--- homeworks/homework_02/reader.py
+++ homeworks/homework_02/reader.py
@@ -1,16 +1,14 @@
-
-
 def read(file):
-	heandle = open(file, "r", encoding = "utf-16")
-	try:
-		return heandle.read()
-	except UnicodeError:
-		heandle = open(file, "r", encoding = "utf-8")
-		try:
-			return heandle.read()
-		except UnicodeError:
-			heandle = open(file, "r", encoding = "cp1251")
-			try:
-				return heandle.read()
-			except:
-				raise ValueError
+    heandle = open(file, "r", encoding="utf-16")
+    try:
+        return heandle.read()
+    except UnicodeError:
+        heandle = open(file, "r", encoding="utf-8")
+        try:
+            return heandle.read()
+        except UnicodeError:
+            heandle = open(file, "r", encoding="cp1251")
+            try:
+                return heandle.read()
+            except:
+                raise ValueError
