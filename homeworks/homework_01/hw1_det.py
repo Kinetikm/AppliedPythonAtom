5265bc6348894afaf14e6c45d3a91fc334f98629
Olegiwe <Starwars700@yandex.ru>
2019-03-12 23:35:32 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import copy
+
 
 def calculate_determinant(list_of_lists):
+    print(list_of_lists)
+
     '''
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
@@ -11,4 +15,26 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    mx = list_of_lists
+    if len(mx) == 0:
+        return None
+    size = len(mx)
+    for list in mx:
+        if len(list) != size:
+            return None
+
+    def recur(arg):
+        res = 0
+        mtx = copy.deepcopy(arg)
+        if len(mtx) == 1:
+            return mtx[0][0]
+        if len(mtx) == 2:
+            return (mtx[0][0]*mtx[1][1] - mtx[1][0] * mtx[0][1])
+        item = mtx.pop(0)
+        for i in range(0, len(arg)):
+            cpmtx = copy.deepcopy(mtx)
+            for k in range(0, len(mtx)):
+                cpmtx[k].pop(i)
+            res += (-1)**(i) * item[i] * recur(cpmtx)
+        return res
+    return recur(mx)
