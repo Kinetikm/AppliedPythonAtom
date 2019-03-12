46eed0915378c3a5f6c50aaae5e2c82a1acd2933
petroglif <32108436+petroglif@users.noreply.github.com>
2019-03-12 17:56:54 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,7 +3,8 @@
 import math
 
 
-def calculate_determinant(dim, list_of_lists):
+def calculate_determinant(list_of_lists):
+    raise NotImplementedError
     '''
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
@@ -11,47 +12,4 @@ def calculate_determinant(dim, list_of_lists):
     Гарантируется, что в матрице float
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
-    '''
-
-    data = list_of_lists
-    print(data)
-
-    if dim == 0:
-        det = 0
-    if dim == 1:
-        det = data[0]
-    if dim == 2:
-        det = data[0] * data[3] - data[2] * data[1]
-    if dim == 3:
-        det = data[0] * data[4] * data[8] + data[1] * data[5] * data[6] + \
-              data[2] * data[3] * data[7] - data[6] * data[4] * data[2] - \
-              data[7] * data[5] * data[0] - data[8] * data[3] * data[1]
-    if dim > 3:
-        det = 0
-        i = 0
-        for j in range(dim):
-            det += data[i * dim + j] * alg_dopolnenie(dim, data, i, j)
-
-    return det
-
-
-def alg_dopolnenie(dim, data, i, j):
-    return math.pow(-1, i + j) * dop_minor(dim, data, i, j)
-
-
-def dop_minor(dim, data, i, j):
-    new_matrix = []
-    for k in range(dim * dim):
-        if (math.floor(k / dim) != i and k % dim != j):
-            new_matrix.append(data[k])
-    return calculate_determinant(dim - 1, new_matrix)
-
-
-
-data = []
-for i in range(len(listoflist112)):
-    for j in range(len(listoflist112)):
-        data = data + [listoflist112[i][j]]
-dim = len(listoflist112)
-
-print(calculate_determinant(dim, data))
+    
