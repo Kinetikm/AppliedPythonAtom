f9d75ece334940d045b37cbb7ddb5e8cb3f11444
NSKuznetsov <helloqwtheslayer@mail.ru>
2019-03-12 03:14:37 +0300

    VKposter changed 1

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -97,39 +97,40 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        lst = []
+        Lst = []
         for post in self.ReadPosts.keys():
-            lst.append([post, len(set(self.ReadPosts[post]))])
-        N = []
-        bank = set()
-        for i in range(len(lst)):
+            Lst.append([post, len(set(self.ReadPosts[post]))])
+        DoubleList = []
+        Bank = set()
+        for i in range(len(Lst)):
             index = 0
-            ll = []
-            for j in range(len(lst) - 1, i - 1, -1):
-                if lst[i][1] == lst[j][1] and i != j:
-                    ll.append(lst[j][0])
-                    bank.add(lst[j][1])
+            AuxList = []
+            for j in range(len(Lst) - 1, i - 1, -1):
+                if Lst[i][1] == Lst[j][1] and i != j:
+                    AuxList.append(Lst[j][0])
+                    Bank.add(lst[j][1])
                     index += 1
                 elif i == j and index > 0:
-                    ll.append(lst[j][0])
-                    N.append([ll, lst[i][1]])
-                elif i == j and index == 0 and lst[j][1] not in bank:
-                    N.append([lst[i][0], lst[i][1]])
-        P = {}
-        for i in range(len(N)):
-            P.setdefault(N[i][1], sorted(N[i][0], reverse=True))
+                    AuxList.append(Lst[j][0])
+                    DoubleList.append([AuxList, Lst[i][1]])
+                elif i == j and index == 0 and Lst[j][1] not in Bank:
+                    DoubleList.append([Lst[i][0], Lst[i][1]])
+        InvReadPosts = {}
+        for i in range(len(DoubleList)):
+            InvReadPosts.setdefault(DoubleList[i][1],
+                                    sorted(DoubleList[i][0], reverse=True))
         Output = []
-        for key in sorted(P.keys()):
+        for key in sorted(InvReadPosts.keys()):
             try:
-                if k - len(P[key]) >= 0:
-                    Output += P[key]
-                    k -= len(P[key])
+                if k - len(InvReadPosts[key]) >= 0:
+                    Output += InvReadPosts[key]
+                    k -= len(InvReadPosts[key])
                 else:
-                    Output += P[key][:k]
+                    Output += InvReadPosts[key][:k]
                     break
             except TypeError:
                 if k > 0:
-                    Output.append(P[key])
+                    Output.append(InvReadPosts[key])
                     k -= 1
                 else:
                     break
