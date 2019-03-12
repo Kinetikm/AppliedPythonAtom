6067e159d54fa29d5760380284f4793a21bc136d
EvgeniyMist <wertygeny@gmail.com>
2019-03-12 14:23:13 +0300

    Вертиков Евгений

--- homeworks/homework_02/heap.py
+++ homeworks/homework_02/heap.py
@@ -5,24 +5,54 @@
 class Heap():
 
     def __init__(self, array):
-        pass
+        self.input_inf = array[:]
+        self.main = []
+        self.build_heap()
 
     def add(self, elem_with_priority):
-        pass
+        self.main.append(elem_with_priority)
+        i = len(self.main) - 1
+        while i > 0:
+            parent_i = (i - 1)//2
+            if comparator_d(self.main[i], self.main[parent_i]):
+                h = self.main[i]
+                self.main[i] = self.main[parent_i]
+                self.main[parent_i] = h
+                i = parent_i
+            else:
+                break
 
     def build_heap(self):
-        pass
-
+        i = 1
+        self.main.append(self.input_inf[0])
+        while i < len(self.input_inf):
+            self.add(self.input_inf[i])
+            i += 1
 
 class MaxHeap(Heap):
 
     def __init__(self, array):
-        raise NotImplementedError
+        Heap.__init__(self, array)
 
     def extract_maximum(self):
-        pass
-
-
+        result = self.main[0]
+        i = len(self.main) - 1
+        self.main[0] = self.main[i] # перемещаем последний элемент в корень
+        self.main.pop()
+        i = 0
+        while 2*i+1 < len(self.main) - 1: # фактически метод shift down
+            child_l_i = 2*i+1
+            child_r_i = 2*i+2
+            if comparator_d(self.main[child_l_i], self.main[child_r_i]):
+                change_i = child_l_i
+            else:
+                change_i = child_r_i
+            h = self.main[i]
+            self.main[i] = self.main[change_i]
+            self.main[change_i] = h
+            i = change_i
+        return result
+ 
 def comparator_d(x, y):
     if x[0] == y[0]:
         return x[1] >= y[1]
