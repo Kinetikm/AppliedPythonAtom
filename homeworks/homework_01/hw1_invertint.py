378f5bc50392f417b48f8ef0703a6bdf7ab9e8f2
petroglif <32108436+petroglif@users.noreply.github.com>
2019-03-13 12:15:11 +0300

    2

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,18 +3,16 @@
 
 
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
-    tmp2 = []
-    tmp1 = str(number)
-    tmp3 = 0
-    leng = len(tmp1)
-    for i in range(leng):
-        tmp2.append(tmp1[-(i + 1)])
-    for i in range(leng):
-        tmp3 += int(tmp1[i]) * (10 ** i)
-    return tmp3
+    res = 0
+    flag = False
+    if (number < 0):
+        flag = True
+        number = abs(number)
+    while (number > 0):
+        temp = number % 10
+        res = (res * 10) + temp
+        number = number // 10
+    if flag:
+        return -res
+    else:
+        return res
