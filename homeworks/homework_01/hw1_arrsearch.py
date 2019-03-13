b2bc65231e198639ff5bacc11ac935b9d8e4f241
ArtySad <artemoner@mail.ru>
2019-03-13 14:45:10 +0300

    очередной commit

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,13 +3,16 @@
 
 
 def find_indices(input_list, n):
+    print(input_list, "n=", n)
     i = 0
     j = len(input_list) - 1
-    while i != j:
+    while i < j:
         if input_list[i] + input_list[j] == n:
-            return i, j
+            a = (i, j)
+            return a
         elif input_list[i] + input_list[j] < n:
             i = i + 1
         elif input_list[i] + input_list[j] > n:
             j = j - 1
+    print("None")
     return
