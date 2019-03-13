0cc720d3d1f04bb40e3a782060c30317ef7e892f
Olegiwe <o.kuznetsov>
2019-03-14 00:43:21 +0300

    Poster and merger

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -9,7 +9,11 @@ from homeworks.homework_02.fastmerger import FastSortedListMerger
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.all_posts = {}
+        self.posts_of_each_user = {}
+        self.subscriptions = {}
+        self.read_by_user = {}
+    #    raise NotImplementedError
 
     def user_posted_post(self, user_id: int, post_id: int):
         '''
@@ -19,7 +23,12 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if post_id not in self.all_posts:
+            self.all_posts[post_id] = []
+        if user_id not in self.posts_of_each_user:
+            self.posts_of_each_user[user_id] = []
+        if post_id not in self.posts_of_each_user[user_id]:
+            self.posts_of_each_user[user_id].append(post_id)
 
     def user_read_post(self, user_id: int, post_id: int):
         '''
@@ -29,7 +38,10 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if post_id not in self.all_posts:
+            self.all_posts[post_id] = []
+        if user_id not in self.all_posts[post_id]:
+            (self.all_posts[post_id]).append(user_id)
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         '''
@@ -39,7 +51,14 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
-        pass
+        if followee_user_id not in self.subscriptions:
+            self.subscriptions[followee_user_id] = []
+        if follower_user_id not in self.read_by_user:
+            self.read_by_user[follower_user_id] = []
+        if follower_user_id not in self.subscriptions[followee_user_id]:
+            (self.subscriptions[followee_user_id]).append(follower_user_id)
+        if followee_user_id not in self.read_by_user[follower_user_id]:
+            (self.read_by_user[follower_user_id]).append(followee_user_id)
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
         '''
@@ -50,7 +69,16 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
-        pass
+        list_of_lists = []
+        output_list = []
+        if user_id not in self.read_by_user.keys():
+            return output_list
+        for followee in self.read_by_user[user_id]:
+            if followee not in self.posts_of_each_user:
+                continue
+            list_of_lists.append(self.posts_of_each_user[followee])
+        output_list = FastSortedListMerger.merge_first_k(list_of_lists, k)
+        return output_list
 
     def get_most_popular_posts(self, k: int) -> list:
         '''
@@ -60,4 +88,15 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        pass
+        post_tuple_list = []
+        output = []
+        for post in self.all_posts.keys():
+            post_tuple_list.append((len(self.all_posts[post]), post))
+        post_heap = MaxHeap(post_tuple_list)
+        for i in range(0, k):
+            try:
+                max_el = post_heap.extract_maximum()
+            except IndexError:
+                break
+            output.append(max_el[1])
+        return output
