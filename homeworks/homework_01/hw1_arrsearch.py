ad699b302471a84c06fe5c02aac657314b1a5cf7
Егор Сенчук <48069676+yagorkas@users.noreply.github.com>
2019-03-13 22:35:38 +0300

    arrsearch pep8 fixed

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -1,8 +1,8 @@
 def find_indices(input_list, n):
     sup_dict = {}
     for i in range(len(input_list)):
-         if input_list[i] in sup_dict:
-             return sup_dict.get(input_list[i]), i
-         else:
-             sup_dict[n - input_list[i]] = i
+        if input_list[i] in sup_dict:
+            return sup_dict.get(input_list[i]), i
+        else:
+            sup_dict[n - input_list[i]] = i
     return None
