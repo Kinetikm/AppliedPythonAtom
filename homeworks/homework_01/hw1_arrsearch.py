9a6153ae5057c380702a2b09e655be4c48b79495
DmitriiLatkin <latkinDU@gmail.com>
2019-03-13 13:11:32 +0300

    Сообщение

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,13 +3,9 @@
 
 
 def find_indices(input_list, n):
-    b = ()
+    input_dict = dict()
     for i in range(len(input_list)):
-        j = 0
-        while j < i:
-            if (input_list[j]+input_list[i]) == n:
-                b = (j, i)
-            j = j + 1
-    if b == ():
-        return None
-    return b
+        if (n - input_list[i]) in input_dict:
+            return input_dict[n - input_list[i]], i
+        input_dict[input_list[i]] = i
+    return None
