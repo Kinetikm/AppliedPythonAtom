55237a612901664f8870140b322d6170543933f3
petroglif <32108436+petroglif@users.noreply.github.com>
2019-03-13 12:14:14 +0300

    1

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 def calculator(x, y, operator):
-
-    if (y == 0):
+    try:
+        x = float(x)
+        y = float(y)
+    except:
         return None
     if (operator == 'plus'):
         return x + y
-    if (operator == 'minus'):
+    elif (operator == 'minus'):
         return x - y
-    if (operator == 'mult'):
+    elif (operator == 'mult'):
         return x * y
-    if (operator == 'divide'):
-        return x / y
-
-
-
+    elif (operator == 'divide'):
+        if (y != 0):
+            return x / y
+    return None
