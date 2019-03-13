f088edfc12ef8050ecd16e5f463ff88208159ae6
petroglif <32108436+petroglif@users.noreply.github.com>
2019-03-13 12:17:44 +0300

    4

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -3,13 +3,10 @@
 
 
 def invert_dict(source_dict):
-    newdict = {}
-    for k, v in source_dict.items():
-        newdict.setdefault(v, []).append(k)
-    return newdict
     '''
     Функция которая разворачивает словарь, т.е.
     каждому значению ставит в соответствие ключ.
     :param source_dict: dict
     :return: new_dict: dict
     '''
+    raise NotImplementedError
