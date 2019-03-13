f17d33dc498e767b02208cd95b3449525ee7e249
unknown <dr.interned@gmail.com>
2019-03-14 02:05:04 +0300

    4

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -7,8 +7,9 @@ from maketable import *
 def new_print_table(table):
     col_width = [max(len(x) for x in col) for col in zip(*table)]
     length = sum(col_width) + 21
+    flag = True
     for tline in table:
-        test = (
+        test1 = (
                 "|  " +
                 "  |  ".join(
                     "{:^{}}".format(
@@ -16,31 +17,40 @@ def new_print_table(table):
                         col_width[i])for i,
                     x in enumerate(tline)) +
                 "  |")
+        if len(test1) != length:
+            print('Формат не валиден')
+            flag = False
+            break
         test2 = (
                 "|  " + "  |  ".join(
                     "{:{}}".format(x, col_width[i])for i,
                     x in enumerate(tline[:len(tline) - 1])) +
                 "  |  " + '{:>{}}'.format(tline[-1], col_width[-1]) + "  |")
-    print('-' * length)
-    count = 1
-    for line in table:
-        if count == 1:
-            print(
-                "|  " +
-                "  |  ".join(
-                    "{:^{}}".format(
-                        x,
-                        col_width[i])for i,
-                    x in enumerate(line)) +
-                "  |")
-            count += 1
-        else:
-            print(
-                "|  " + "  |  ".join(
-                    "{:{}}".format(x, col_width[i])for i,
-                    x in enumerate(line[:len(line) - 1])) +
-                "  |  " + '{:>{}}'.format(line[-1], col_width[-1]) + "  |")
-    print('-' * length)
+        if len(test2) != length:
+            print('Формат не валиден')
+            flag = False
+            break
+    if flag is True:
+        print('-' * length)
+        count = 1
+        for line in table:
+            if count == 1:
+                print(
+                    "|  " +
+                    "  |  ".join(
+                        "{:^{}}".format(
+                            x,
+                            col_width[i])for i,
+                        x in enumerate(line)) +
+                    "  |")
+                count += 1
+            else:
+                print(
+                    "|  " + "  |  ".join(
+                        "{:{}}".format(x, col_width[i])for i,
+                        x in enumerate(line[:len(line) - 1])) +
+                    "  |  " + '{:>{}}'.format(line[-1], col_width[-1]) + "  |")
+        print('-' * length)
 
 
 def print_file(path):
@@ -58,10 +68,12 @@ def print_file(path):
         else:
             print('Формат не валиден')
 
-
+'''
 if __name__ == '__main__':
     try:
         filename = sys.argv[1]
         print_file(filename)
     except(IndexError, AssertionError):
         print('Формат не валиден')
+'''
+print_file('files/posts-utf8.json')
