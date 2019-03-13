73950f8e2b982e71ba42d3f1bc50bbe19c814f0e
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-13 19:25:28 +0300

    fastmerger выбор К нибольших элементов из N отсортирвоанных списков

--- homeworks/homework_02/fastmerger.py
+++ homeworks/homework_02/fastmerger.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from homeworks.homework_02.heap import MaxHeap
-import copy
+from .heap import MaxHeap
 
 
 class FastSortedListMerger:
@@ -13,4 +12,19 @@ class FastSortedListMerger:
         принимает на вход список отсортированных непоубыванию списков и число
         на выходе выдает один список длинной k, отсортированных по убыванию
         '''
-        raise NotImplementedError
+        output = []  # результат
+        array = []  # массив для построения кучи
+        for i, list_ in enumerate(list_of_lists):  # из каждого списка берем поседний(наибольший) элемент
+            tmp = (list_.pop(), i)  # записываем кортеж (значение элемента, из какого списка был взят)
+            array.append(tmp)
+        heap = MaxHeap(array)  # строим кучу
+        j = 0  # счетщик длинны исходного списка
+        while j != k and h.heap:
+            j += 1
+            maximum, number = heap.extract_maximum()
+            output.append(maximum)
+            if list_of_lists[number]:
+                tmp = (list_of_lists[number].pop(),
+                       number)  # достаем элемент из того списка,из которого был только что записанный максимум
+                heap.add(tmp)  # добавляем его в кучу
+        return output
