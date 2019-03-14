b187794a0fb95bbd8897ae9d7758703fffc85bd7
Rustam <rustamelutingol@gmail.com>
2019-03-14 03:44:07 +0300

    Update table.py

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,17 @@
 import sys
 
 # Ваши импорты
+from encode import check_encoding
+from check_format import check_file_format
+from construct_table import take_data
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-
-    # Ваш код
+    try:
+        f = open(filename)
+        f.close()
+    except FileNotFoundError:
+        raise SystemExit("Файл не валиден")
+    coding = str(check_encoding(filename))
+    ext = check_file_format(filename, coding)
+    take_data(ext, filename, coding)
