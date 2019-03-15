e9ab70cd14beabeed6edf29c51a6c3d165634dda
IgorKurylev <kurilevigor@mail.ru>
2019-03-16 00:13:26 +0300

    revert list + event stats

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from collections import deque, Counter
+
 
 class TEventStats:
     FIVE_MIN = 300
 
     def __init__(self):
-        # TODO: реализовать метод
-        raise NotImplementedError
+        self.events = deque()
 
     def register_event(self, user_id, time):
-        """
-        Этот метод регистрирует событие активности пользователя.
-        :param user_id: идентификатор пользователя
-        :param time: время (timestamp)
-        :return: None
-        """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        self.events.appendleft((user_id, time))
 
     def query(self, count, time):
-        """
-        Этот метод отвечает на запросы.
-        Возвращает количество пользователей, которые за последние 5 минут
-        (на полуинтервале времени (time - 5 min, time]), совершили ровно count действий
-        :param count: количество действий
-        :param time: время для рассчета интервала
-        :return: activity_count: int
-        """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        cnt_dict = Counter()
+        res_cnt = 0
+        for event in self.events:
+            if 0 <= time - event[1] < self.FIVE_MIN:
+                cnt_dict[event[0]] += 1
+        for item in cnt_dict.values():
+            if item == count:
+                res_cnt += 1
+        return res_cnt
