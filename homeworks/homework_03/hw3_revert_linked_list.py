ec55db8a0beb1e3600c149d2674c265043e2e644
EvgeniyMist <wertygeny@gmail.com>
2019-03-16 08:28:07 +0300

    revert_linked_list

--- homeworks/homework_03/hw3_revert_linked_list.py
+++ homeworks/homework_03/hw3_revert_linked_list.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import copy
+
 
 def revert_linked_list(head):
-    """
-    A -> B -> C should become: C -> B -> A
-    :param head: LLNode
-    :return: new_head: LLNode
-    """
-    # TODO: реализовать функцию
-    raise NotImplementedError
+    # проверяем, присуствует ли хотя бы два элемента в списке
+    if head.next_node is not None:
+        # далее работаем с копиями объектов
+        h1 = copy.copy(head)
+        h2 = copy.copy(head.next_node)
+        h1.next_node = None
+        while h2.next_node is not None:
+            h = copy.copy(h2.next_node)
+            h2.next_node = copy.copy(h1)
+            h1 = copy.copy(h2)
+            h2 = copy.copy(h)
+        h2.next_node = copy.copy(h1)
+        return h2
+    else:
+        return head
