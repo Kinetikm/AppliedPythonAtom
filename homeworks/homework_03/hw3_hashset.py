87d047d27f100452b505fff4021be7279a47aebf
EvgeniyMist <wertygeny@gmail.com>
2019-03-16 20:44:26 +0300

    hashset

--- homeworks/homework_03/hw3_hashset.py
+++ homeworks/homework_03/hw3_hashset.py
@@ -4,29 +4,44 @@
 from homeworks.homework_03.hw3_hashmap import HashMap
 
 
-class HashSet:
+class HashSet(HashMap):
 
-    def __init__(self):
-        # TODO Сделать правильно =)
-        raise NotImplementedError
+    # не увидел смысла переопределять __init__
+    # (разве что ради переобозначения атрибута map)
+    
+    def get(self, value):
+        if self.map[hash(value) % self.max_size].count(value):
+            return True
+        return False
 
-    def get(self, key, default_value=None):
-        # TODO достаточно переопределить данный метод
-        raise NotImplementedError
+    def put(self, value):
+        if not self.map[hash(value) % self.max_size].count(value):
+            self.map[hash(value) % self.max_size].append(value)
+        
+        # встает вопрос о необходимости расширения hashset
+        # в случае ее присутствия необходимо переопределить метод _resize
 
-    def put(self, key, value):
-        # TODO метод put, нужно переопределить данный метод
-        raise NotImplementedError
-
-    def __len__(self):
-        # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+    # метод __len__ будет работать исправно
 
     def values(self):
-        # TODO возвращать итератор значений
-        raise NotImplementedError
+        result = []
+        for i in self.map:
+            result.extend(i)
+        return result
 
     def intersect(self, another_hashset):
-        # TODO метод, возвращающий новый HashSet
-        #  элементы - пересечение текущего и другого
-        raise NotImplementedError
+        i = 0
+        # создаем новый hashset оптимального размера
+        res_hashset = HashSet(min(another_hashset.max_size, self.max_size))
+        h = set(self.values())
+        h.intersection_update(set(another_hashset.values()))
+        h = list(h)
+        for i in h:
+            res_hashset.put(i)
+        return res_hashset
+        
+    def __contains__(self, item):
+        for i in self.map:
+            if i.count(item):
+                return True
+        return False
