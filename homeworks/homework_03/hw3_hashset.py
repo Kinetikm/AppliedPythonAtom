12229240ed655ecda1cfd043976622969f1359ad
Tcibisov Dmitriy <dcibisof@nic.ru>
2019-03-16 16:50:37 +0300

    HW3: HashSet

--- homeworks/homework_03/hw3_hashset.py
+++ homeworks/homework_03/hw3_hashset.py
@@ -4,29 +4,40 @@
 from homeworks.homework_03.hw3_hashmap import HashMap
 
 
-class HashSet:
+class HashSet(HashMap):
+    DEF_LEN = 4
 
-    def __init__(self):
-        # TODO Сделать правильно =)
-        raise NotImplementedError
+    def get(self, value, default_value=None):
+        return True if super().get(value, default_value) is not None else False
 
-    def get(self, key, default_value=None):
-        # TODO достаточно переопределить данный метод
-        raise NotImplementedError
-
-    def put(self, key, value):
-        # TODO метод put, нужно переопределить данный метод
-        raise NotImplementedError
-
-    def __len__(self):
-        # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
-
-    def values(self):
-        # TODO возвращать итератор значений
-        raise NotImplementedError
+    def put(self, key, value=None):
+        super().put(key, key)
 
     def intersect(self, another_hashset):
-        # TODO метод, возвращающий новый HashSet
-        #  элементы - пересечение текущего и другого
-        raise NotImplementedError
+        """
+        Метод, возвращающий новый HashSet
+        содержащий в себе пересекающиеся элементы
+        пересечения текущего и другого HashSet's
+        """
+        new_hashset = HashSet()
+
+        for item in self.values():
+            if another_hashset.get(item, None):
+                new_hashset.put(item)
+
+        return new_hashset
+
+    def union(self, another_hashset):
+        """
+        Метод, возвращающий новый HashSet
+        содержащий в себе все элементы
+        из текущего и другого HashSet's
+        """
+        new_hashset = HashSet()
+
+        values = self.values() + another_hashset.values()
+
+        for item in values:
+            new_hashset.put(item)
+
+        return new_hashset
