b4bb8bcfb1d7e52a44961e5e96aa362e7c16253b
Danr0 <denp00@yandex.ru>
2019-03-17 18:13:39 +0300

    Погонин Денис

--- homeworks/homework_03/hw3_hashset.py
+++ homeworks/homework_03/hw3_hashset.py
@@ -4,29 +4,30 @@
 from homeworks.homework_03.hw3_hashmap import HashMap
 
 
-class HashSet:
-
+class HashSet(HashMap):
     def __init__(self):
         # TODO Сделать правильно =)
-        raise NotImplementedError
+        super().__init__()
 
-    def get(self, key, default_value=None):
-        # TODO достаточно переопределить данный метод
-        raise NotImplementedError
+    # def get(self, key, default_value=None):
+    # TODO достаточно переопределить данный метод
 
-    def put(self, key, value):
+    def put(self, key):
         # TODO метод put, нужно переопределить данный метод
-        raise NotImplementedError
+        super().put(key, 1)
 
-    def __len__(self):
-        # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+    # def __len__(self):
+    # TODO Возвращает количество Entry в массиве
 
     def values(self):
         # TODO возвращать итератор значений
-        raise NotImplementedError
+        return super().keys()
 
     def intersect(self, another_hashset):
         # TODO метод, возвращающий новый HashSet
         #  элементы - пересечение текущего и другого
-        raise NotImplementedError
+        out = HashSet()
+        for a in self.values():
+            if another_hashset.get(a) is not None:
+                out.put(a)
+        return out
