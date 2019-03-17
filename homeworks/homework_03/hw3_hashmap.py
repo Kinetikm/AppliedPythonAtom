2181bd5a9c6568415a19a0132dc41d153f5c1d51
Tcibisov Dmitriy <dcibisof@nic.ru>
2019-03-17 14:49:57 +0300

    HW3: HashMap iterators

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -43,6 +43,49 @@ class HashMap:
             """Метод выводит 'key: {}, value: {}'"""
             return "Entry [key: {}, value:{}]".format(self.__key, self.__value)
 
+    class EntryItemsIterator:
+        """Итератор по Entry"""
+        def __iter__(self):
+            return self
+
+        def __init__(self, buckets):
+            self.buckets = buckets
+            self.i_bucket = 0
+            self.item = 0
+
+        def __next__(self):
+            # ищем следующий элемент пока не найдем или чанки не закончатся
+            while True:
+                # если бакеты закончились выходим
+                if self.i_bucket >= len(self.buckets):
+                    raise StopIteration
+
+                # если текущий чанк закончился переход к следующему
+                if self.item >= len(self.buckets[self.i_bucket]):
+                    self.i_bucket += 1
+                    self.item = 0
+                else:
+                    # получаем следующее значение из итератора
+                    item = self.get_item_data(self.buckets[self.i_bucket][self.item])
+                    self.item += 1
+                    return item
+
+        @staticmethod
+        def get_item_data(item):
+            return item.get_key(), item.get_value()
+
+    class EntryValuesIterator(EntryItemsIterator):
+        """Итератор по значениям Entry"""
+        @staticmethod
+        def get_item_data(item):
+            return item.get_value()
+
+    class EntryKeysIterator(EntryItemsIterator):
+        """Итератор по ключам Entry"""
+        @staticmethod
+        def get_item_data(item):
+            return item.get_key()
+
     def __init__(self, bucket_num=64):
         """
         Реализуем метод цепочек
@@ -108,19 +151,15 @@ class HashMap:
 
     def values(self):
         """Возвращает итератор значений"""
-        return [item[1] for item in self.items()]
+        return self.EntryValuesIterator(self._buckets)
 
     def keys(self):
         """Возвращает итератор ключей"""
-        return [item[0] for item in self.items()]
+        return self.EntryKeysIterator(self._buckets)
 
     def items(self):
         """Возвращает итератор пар ключ и значение (tuples)"""
-        items = []
-        for bucket in self._buckets:
-            for item in bucket:
-                items.append((item.get_key(), item.get_value()))
-        return items
+        return self.EntryItemsIterator(self._buckets)
 
     def _resize(self):
         """Измененяет размер хэш таблицы"""
