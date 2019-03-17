1bc449f8dbd96a37968b2586cba849960d147928
Tcibisov Dmitriy <dcibisof@nic.ru>
2019-03-17 14:10:57 +0300

    HW3: LRUCacheDecorator

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -2,18 +2,52 @@
 # coding: utf-8
 
 
+from collections import OrderedDict
+from time import time
+
+
+def ms_cur_time():
+    return int(round(time() * 1000))
+
+
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
-        '''
+        """
+        Инициализация декоратора
+         https://www.geeksforgeeks.org/class-as-decorator-in-python/
+
         :param maxsize: максимальный размер кеша
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        """
+        self.size = 0
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache_data = OrderedDict()
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        """Вызов функции"""
+
+        def wrapper(*args, **kwargs):
+            # проверяем кэш на наличие такого запроса и то что он еще жив
+            cached = self.cache_data.get(args)
+            if cached and \
+                    (self.ttl is None or ms_cur_time() - cached[1] <= self.ttl):
+                # обновление в кэше
+                self.cache_data[args] = (cached[0], ms_cur_time())
+                return cached[0]
+
+            # записи в кэше нет или устарела, вычисляем
+            result = func(*args, **kwargs)
+
+            # вытесняем старую(LRU) запись если кэш заполнен
+            if self.size >= self.maxsize:
+                self.cache_data.popitem(last=True)
+            else:
+                self.size += 1
+            self.cache_data[args] = (result, ms_cur_time())
+
+            return result
+
+        return wrapper
