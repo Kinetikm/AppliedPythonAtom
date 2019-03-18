1c3512e3c93e25315bd600acf22c499f77661351
Егор Сенчук <senchuk_egor@mail.ru>
2019-03-18 14:08:26 +0300

    revert_list added

--- homeworks/homework_03/hw3_revert_linked_list.py
+++ homeworks/homework_03/hw3_revert_linked_list.py
@@ -3,10 +3,13 @@
 
 
 def revert_linked_list(head):
-    """
-    A -> B -> C should become: C -> B -> A
-    :param head: LLNode
-    :return: new_head: LLNode
-    """
-    # TODO: реализовать функцию
-    raise NotImplementedError
+    try:
+        prev = head.next_node
+    except AttributeError:
+        return head
+    new_head = head
+    new_head.next_node = None
+    while prev:
+        new_head, prev.next_node, prev =\
+            prev, new_head, prev.next_node
+    return new_head
