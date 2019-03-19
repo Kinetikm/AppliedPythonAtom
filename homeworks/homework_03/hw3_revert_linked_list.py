961afb08563f3d0f1adc76e602cf89d88a55aeb4
Aidar Valshin <Aidar_V2000@mail.ru>
2019-03-19 22:05:46 +0300

    “list1

---
+++ homeworks/homework_03/hw3_revert_linked_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def revert_linked_list(head):
+    """
+    A -> B -> C should become: C -> B -> A
+    :param head: LLNode
+    :return: new_head: LLNode
+    """
+    # TODO: реализовать функцию
+    if head is None:
+        return head
+    prev, next = None, None
+    curr = head
+    while curr:
+        next = curr.next_node
+        curr.next_node = prev
+        prev = curr
+        curr = next
+    return prev
