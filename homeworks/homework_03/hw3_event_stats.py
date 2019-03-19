367cededdcb81d02f4c4972bd1b4dba1a4313b56
Егор Сенчук <senchuk_egor@mail.ru>
2019-03-19 21:34:51 +0300

    event_stat added

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
+import collections
 
 
 class TEventStats:
     FIVE_MIN = 300
 
     def __init__(self):
-        # TODO: реализовать метод
-        raise NotImplementedError
+        self.users_activity = dict()
 
     def register_event(self, user_id, time):
-        """
-        Этот метод регистрирует событие активности пользователя.
-        :param user_id: идентификатор пользователя
-        :param time: время (timestamp)
-        :return: None
-        """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        if user_id in self.users_activity.keys():
+            self.users_activity[user_id].append(time)
+        else:
+            self.users_activity[user_id] = [time]
 
     def query(self, count, time):
-        """
-        Этот метод отвечает на запросы.
-        Возвращает количество пользователей, которые за последние 5 минут
-        (на полуинтервале времени (time - 5 min, time]), совершили ровно count действий
-        :param count: количество действий
-        :param time: время для рассчета интервала
-        :return: activity_count: int
-        """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        overall_activity = dict()
+        for id in self.users_activity.keys():
+            overall_activity[id] = 0
+            if self.users_activity[id][0] > time:
+                overall_activity[id] -= 1
+                continue
+            for act in self.users_activity[id]:
+                if (act > time - self.FIVE_MIN) and (act <= time):
+                    overall_activity[id] += 1
+        result = [number for number in overall_activity.values()]
+        return collections.deque(result).count(count)
