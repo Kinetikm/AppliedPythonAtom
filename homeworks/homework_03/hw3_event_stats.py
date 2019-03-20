c5a077e16b6789155df94e0443492d90d9a8f485
Rustam <rustamelutingol@gmail.com>
2019-03-21 00:53:27 +0300

    Update hw3_event_stats.py

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+from collections import Counter, deque
 
 class TEventStats:
     FIVE_MIN = 300
 
     def __init__(self):
-        # TODO: реализовать метод
-        raise NotImplementedError
+        self._actions = deque()
 
     def register_event(self, user_id, time):
         """
@@ -16,8 +15,7 @@ class TEventStats:
         :param time: время (timestamp)
         :return: None
         """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        self._actions.append_left({'user': user_id, 'time': time})
 
     def query(self, count, time):
         """
@@ -28,5 +26,9 @@ class TEventStats:
         :param time: время для рассчета интервала
         :return: activity_count: int
         """
-        # TODO: реализовать метод
-        raise NotImplementedError
+        event_count = Counter()
+        for event in self._actions:
+            if time - self.FIVE_MIN < event['time'] <= time:
+                event_count[event['user']] += 1
+
+        return list(event_count.values()).count(event_count)
