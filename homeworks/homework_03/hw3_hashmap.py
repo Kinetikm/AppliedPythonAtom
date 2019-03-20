0a89d4877458eb4c4ae6fbc34ba3bc01cc9ef387
IgorKreknin <Ikreknin@bk.ru>
2019-03-20 22:32:56 +0300

    Hash changes

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -32,6 +32,47 @@ class HashMap:
             else:
                 return False
 
+        def __str__(self):
+            return "[key: {}, value: {}]".format(self._key, self._value)
+
+        def __repr__(self):
+            return "[key: {}, value: {}]".format(self._key, self._value)
+
+    class MyIterator:
+        def __iter__(self):
+            return self
+
+        def __init__(self, table):
+            self.__table = table
+            self.__bucket_iter = iter(table)
+            self.__item_iter = iter(next(self.__bucket_iter))
+
+        def __next__(self):
+            try:
+                return self._format(next(self.__item_iter))
+            except StopIteration:
+                while True:
+                    self.__item_iter = iter(next(self.__bucket_iter))
+                    try:
+                        return self._format(next(self.__item_iter))
+                    except:
+                        pass
+
+        def _format(self, item):
+            return item
+
+    class ValuesIterator(MyIterator):
+        def _format(self, item):
+            return item.get_value()
+
+    class KeysIterator(MyIterator):
+        def _format(self, item):
+            return item.get_key()
+
+    class ItemsIterator(MyIterator):
+        def _format(self, item):
+            return (item.get_key(), item.get_value())
+
     def __init__(self, bucket_num=64):
         '''
         Реализуем метод цепочек
@@ -40,28 +81,33 @@ class HashMap:
         self.__reserved = bucket_num
         self.__table = [[] for i in range(bucket_num)]
         self.__size = 0
-        
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        index = self._get_index(self._get_hash(key))
+        try:
+            index = self._get_index(self._get_hash(key))
+        except:
+            return default_value
         bucket = self.__table[index]
         for entry in bucket:
             if entry.get_key() == key:
-                return entry.get_value
+                return entry.get_value()
         return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        index = self._get_index(self._get_hash(key))
+        try:
+            index = self._get_index(self._get_hash(key))
+        except:
+            return None
         bucket = self.__table[index]
         for entry in bucket:
             if entry.get_key() == key:
                 entry._value = value
                 return
-        bucket.append(Entry(key, value))
+        bucket.append(self.Entry(key, value))
         self.__size += 1
         if self.__size >= self.__reserved:
             self._resize()
@@ -77,33 +123,41 @@ class HashMap:
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        return hash_value % self.__size
+        return hash_value % self.__reserved
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return self.ValuesIterator(self.__table)
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return self.KeysIterator(self.__table)
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return self.ItemsIterator(self.__table)
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        entryes = []
+        for bucket in self.__table:
+            entryes = entryes + bucket
+        self.__size = 0
+        self.__reserved *= 2
+        self.__table = [[] for i in range(self.__reserved)]
+        for entry in entryes:
+            self.put(entry.get_key(), entry.get_value())
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        list_of_items = [item for item in self.items()]
+        return "buckets: {}, items: {}".format(self.__table, list_of_items)
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        index = self._get_index(self._get_hash(item.get_key()))
+        index = self._get_index(self._get_hash(item))
         bucket = self.__table[index]
         for entry in bucket:
-            if entry.get_key() == item.get_key():
+            if entry.get_key() == item:
                 return True
         return False
--- homeworks/homework_03/hw3_hashset.py
+++ homeworks/homework_03/hw3_hashset.py
@@ -4,29 +4,34 @@
 from homeworks.homework_03.hw3_hashmap import HashMap
 
 
-class HashSet:
+class HashSet(HashMap):
+    plug = 'value'
 
-    def __init__(self):
+    def __init__(self, bucket_num=64):
         # TODO Сделать правильно =)
-        raise NotImplementedError
+        super().__init__(bucket_num)
 
     def get(self, key, default_value=None):
         # TODO достаточно переопределить данный метод
-        raise NotImplementedError
+        return super().__contains__(key)
 
-    def put(self, key, value):
+    def put(self, key, value=None):
         # TODO метод put, нужно переопределить данный метод
-        raise NotImplementedError
+        super().put(key, self.plug)
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return super().__len__()
 
     def values(self):
         # TODO возвращать итератор значений
-        raise NotImplementedError
+        return super().keys()
 
     def intersect(self, another_hashset):
         # TODO метод, возвращающий новый HashSet
         #  элементы - пересечение текущего и другого
-        raise NotImplementedError
+        result_hashset = HashSet(10)
+        for value in self.values():
+            if another_hashset.get(value):
+                result_hashset.put(value)
+        return result_hashset
