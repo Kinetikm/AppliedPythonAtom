36f5d4551db577feefb8c23619e21645682c486a
NSKuznetsov <helloqwtheslayer@mail.ru>
2019-03-20 23:06:20 +0300

    event_stats.py

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -7,6 +7,7 @@ class TEventStats:
 
     def __init__(self):
         # TODO: реализовать метод
+        self.user_act = {}
         raise NotImplementedError
 
     def register_event(self, user_id, time):
@@ -17,6 +18,10 @@ class TEventStats:
         :return: None
         """
         # TODO: реализовать метод
+        if not self.user_act.get(user_id):
+            self.user_act[user_id] = [time]
+        else:
+            self.user_act[user_id].append(time)
         raise NotImplementedError
 
     def query(self, count, time):
@@ -29,4 +34,16 @@ class TEventStats:
         :return: activity_count: int
         """
         # TODO: реализовать метод
+        activity_count = 0
+        for user, timestamp in self.act.items():
+            index = 0
+            for t in timestamp:
+                if 0 <= time - t < self.FIVE_MIN:
+                    index += 1
+            if index == count:
+                activity_count += 1
+            if index != 0 and count != 0:
+                continue
+
+        return activity_count
         raise NotImplementedError
