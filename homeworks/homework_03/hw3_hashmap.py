3ee92d86d5ed6cfd22db5dd30dcf4dfb2afb79c2
IgorKreknin <Ikreknin@bk.ru>
2019-03-20 18:26:03 +0300

    HW changes

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -14,48 +14,70 @@ class HashMap:
             :param key: ключ
             :param value: значение
             '''
+            self._key = key
+            self._value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self._key
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self._value
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            if self._key == other.get_key():
+                return True
+            else:
+                return False
 
     def __init__(self, bucket_num=64):
         '''
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         '''
-        raise NotImplementedError
+        self.__reserved = bucket_num
+        self.__table = [[] for i in range(bucket_num)]
+        self.__size = 0
+        
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        index = self._get_index(self._get_hash(key))
+        bucket = self.__table[index]
+        for entry in bucket:
+            if entry.get_key() == key:
+                return entry.get_value
+        return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        index = self._get_index(self._get_hash(key))
+        bucket = self.__table[index]
+        for entry in bucket:
+            if entry.get_key() == key:
+                entry._value = value
+                return
+        bucket.append(Entry(key, value))
+        self.__size += 1
+        if self.__size >= self.__reserved:
+            self._resize()
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return self.__size
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.__size
 
     def values(self):
         # TODO Должен возвращать итератор значений
@@ -79,4 +101,9 @@ class HashMap:
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        index = self._get_index(self._get_hash(item.get_key()))
+        bucket = self.__table[index]
+        for entry in bucket:
+            if entry.get_key() == item.get_key():
+                return True
+        return False
