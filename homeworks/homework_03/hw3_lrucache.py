0db38ab700049d44f56724bd93fe3d6bb3cc2bcf
Rustam <rustamelutingol@gmail.com>
2019-03-21 01:55:34 +0300

    Update hw3_lrucache.py

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
+from collections import OrderedDict
 
 
 class LRUCacheDecorator:
@@ -10,10 +12,25 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.size = 0
+        self.cached = OrderedDict()
+    def __call__(self, func):
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+        def wrapper(*args, **kwargs):
+            cached = self.cached.get(args)
+            if cached and \
+                    (self.ttl is None or int(round(time() * 1000)) - cached[1] <= self.ttl):
+                self.cached[args] = (cached[0], int(round(time() * 1000)))
+                self.cached.move_to_end(args, last=False)
+                return cached[0]
+            result = func(*args, **kwargs)
+            if self.size >= self.maxsize:
+                self.cached.popitem(last=True)
+            else:
+                self.size += 1
+            self.cached[args] = (result, int(round(time() * 1000)))
+            return result
+
+        return wrapper
