2744c962767356b8470592278afc78ad421c6cb8
MaximAbramenko <gotosleepmcht@gmail.com>
2019-03-21 09:59:19 +0300

    lin

--- homeworks/homework_03/hw3_revert_linked_list.py
+++ homeworks/homework_03/hw3_revert_linked_list.py
@@ -8,5 +8,12 @@ def revert_linked_list(head):
     :param head: LLNode
     :return: new_head: LLNode
     """
-    # TODO: реализовать функцию
-    raise NotImplementedError
+    if head is None:
+        return head
+    f_node = head.s_node
+    new_head = head
+    new_head.s_node = None
+    while f_node is not None:
+        f_node.s_node, new_head, f_node = \
+           new_head, f_node,  f_node.s_node
+    return new_head
