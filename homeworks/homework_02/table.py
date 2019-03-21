2771c894357b612d28518a4953b1215fd0d6dbca
BCroco <ninok_orb56@mail.ru>
2019-03-21 03:32:03 +0300

    table_from_Hm_2_2

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -2,7 +2,24 @@ import sys
 
 # Ваши импорты
 
+from homeworks.homework_02.checking import tsv_format, json_format
+from homeworks.homework_02.print_table import print_table
+
 if __name__ == '__main__':
     filename = sys.argv[1]
 
     # Ваш код
+    try:
+        data = None
+        if json_format(filename)[0]:
+            data = json_format(filename)[1]
+        elif tsv_format(filename)[0]:
+            data = tsv_format(filename)[1]
+        else:
+            print('Формат не валиден')
+        print_table(data)
+
+    except FileNotFoundError:
+        print('Файл не валиден')
+    except (UnicodeError, ValueError):
+        print('Формат не валиден')
