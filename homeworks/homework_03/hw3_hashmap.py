4fb9a31d08a376c11a33d2460b5fdda863eb9673
Rustam <rustamelutingol@gmail.com>
2019-03-21 04:29:37 +0300

    Update hw3_hashmap.py

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -14,69 +14,112 @@ class HashMap:
             :param key: ключ
             :param value: значение
             '''
+            self._key = key
+            self._value = value
 
         def get_key(self):
-            # TODO возвращаем ключ
-            raise NotImplementedError
+            return self._key
 
         def get_value(self):
-            # TODO возвращаем значение
-            raise NotImplementedError
+            return self._value
 
         def __eq__(self, other):
-            # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self._key == other._key
+        
+        def __repr__(self):
+            if self._value == None:
+                return f'{self._key}'
+            return f'**key: {self._key}, value: {self._value}**\n'
 
     def __init__(self, bucket_num=64):
         '''
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         '''
-        raise NotImplementedError
+        self._size = 0
+        self._hash_bucket = [None] * bucket_num
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        key_hash = self._get_hash(key)
+        elem_index = self._get_index(key_hash)
+        if (not self._hash_bucket[elem_index]):
+            return default_value
+        else:
+            list_of_elem = self._hash_bucket[elem_index]
+            for elem in list_of_elem:
+                if (key == elem.get_key()):
+                    return elem.get_value()
+        return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        key_hash = self._get_hash(key)
+        elem_index = self._get_index(key_hash)
+        elem = self.Entry(key, value)
+        if (not self._hash_bucket[elem_index]):
+            self._hash_bucket[elem_index] = [elem]
+            self._size += 1
+        else:
+            list_of_elem = self._hash_bucket[elem_index]
+            if (elem not in list_of_elem):
+                list_of_elem.append(elem)
+                self._size += 1
+            else:
+                for stored_elem in list_of_elem:
+                    if elem == stored_elem:
+                        stored_elem._value = elem._value
+                        break
+        if (self._size / len(self._hash_bucket) > 0.75):
+            self._resize()
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return self._size
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % len(self._hash_bucket)
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return (item[1] for item in self.items())
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return (item[0] for item in self.items())
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return ((entry.get_key(), entry.get_value())
+                for bucket in self._hash_bucket if bucket is not None for entry in bucket)
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        items = self.items()
+        self._size = 0
+        self._hash_bucket = len(self._hash_bucket) * 2 * [None]
+        for item in items:
+            self.put(*item)
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return f"buckets: {self._hash_bucket}, items: {self.items()}"
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        pos = self._get_index(self._get_hash(item))
+
+         if self._hash_bucket[pos] is None:
+            return False
+        for entry in self._hash_bucket[pos]:
+            if entry._key == item:
+                return True
+        return False
