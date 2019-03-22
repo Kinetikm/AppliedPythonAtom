78156c087a1ea72427bcc16c94a2f296118ff4d4
Danr0 <denp00@yandex.ru>
2019-03-22 23:31:36 +0300

    Update hw4_wordcounter.py

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -2,11 +2,13 @@
 # coding: utf-8
 
 from multiprocessing import Process, Manager
-import os
+import glob, os
+from os import listdir
+from os.path import isfile, join
 
 
 def word_count_inference(path_to_dir):
-    '''
+    """
     Метод, считающий количество слов в каждом файле из директории
     и суммарное количество слов.
     Слово - все, что угодно через пробел, пустая строка "" словом не считается,
@@ -15,5 +17,30 @@ def word_count_inference(path_to_dir):
     :param path_to_dir: путь до директории с файлами
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    """
+
+    onlyfiles = list()
+    for file in os.listdir(path_to_dir):
+        onlyfiles.append(file)
+
+    def countword(filename, path, d):
+
+        n = 0
+        with open(str(path) + "/" + str(filename)) as f:
+            for line in f:
+                l = line.split(" ")
+                for w in l:
+                    if w != "" and w != " " and w != "\n":
+                        n += 1
+        d[filename] = n
+        d["total"] += n
+
+    with Manager() as manager:
+        d = manager.dict()
+        d["total"] = 0
+        for files in onlyfiles:
+            p = Process(target=countword, args=(files, path_to_dir, d))
+            p.start()
+            p.join()
+        s = d.copy()
+    return s
