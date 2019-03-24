3c05caa298abe8e7f297e60feaac48e3e9b1f428
Olegiwe <o.kuznetsov>
2019-03-25 01:11:55 +0300

    4 done

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -16,4 +16,25 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    list_of_files = list(os.listdir(path_to_dir))
+    wordmap = Manager().dict()
+    list_of_procs = []
+
+    def count_words_in_file(file_path, file_name, wordmap):
+        counter = 0
+        with open(file_path + '/' + file_name, 'r') as fh:
+            lines = fh.readlines()
+            not_empty = list(filter(lambda x: x != "\n", lines))
+            for i in not_empty:
+                counter += len(i.split(" "))
+        wordmap[file_name] = counter
+        wordmap['total'] += counter
+
+    wordmap['total'] = 0
+    for file in list_of_files:
+        process = Process(target=count_words_in_file, args=(path_to_dir, file, wordmap))
+        list_of_procs.append(process)
+        process.start()
+    for pr in list_of_procs:
+        pr.join()
+    return wordmap
