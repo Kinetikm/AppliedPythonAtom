c9568a26c790222ecad77d58b844eb5e0a3fe98a
AidarValshin <Aidar_V2000@mail.ru>
2019-03-24 23:19:26 +0300

    “thread

--- homeworks/homework_03/hw3_revert_linked_list.py
+++ homeworks/homework_03/hw3_revert_linked_list.py
@@ -7,6 +7,15 @@ def revert_linked_list(head):
     A -> B -> C should become: C -> B -> A
     :param head: LLNode
     :return: new_head: LLNode
+    
     """
-    # TODO: реализовать функцию
-    raise NotImplementedError
+    if head is None:
+        return head
+    prev, next = None, None
+    curr = head
+    while curr is not None:
+        next = curr.next_node
+        curr.next_node = prev
+        prev = curr
+        curr = next
+    return prev
--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Pool, Queue, cpu_count
+from collections import deque
 import os
 
 
@@ -16,4 +17,33 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    result_dict = {"total": 0}  # Результирующий словарь
+    total_count = 0  # Суммарное число слов во всех файлах
+    files_list = create_deque(path_to_dir)
+    p = Pool(cpu_count())
+    with p:
+        while files_list:
+            filename = files_list.pop()
+            count = p.apply(words_counter, (path_to_dir + "/" + filename,)).get()
+            total_count += count
+            result_dict.update({filename: count})
+    p.close()
+    p.join()
+    result_dict.update({"total": total_count})
+    return result_dict
+
+
+def words_counter(filename):
+    num_words = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            words = line.split()
+            num_words += len(words)
+    f.close()
+    return num_words
+
+
+def create_deque(path_to_dir):
+    files = deque()
+    files.append(os.listdir(path_to_dir))
+    return files
