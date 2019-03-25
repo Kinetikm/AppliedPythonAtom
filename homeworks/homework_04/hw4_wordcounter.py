48074598eea1287580b958a635e81e377162ccba
Anton L <mr.antlav@gmail.com>
2019-03-25 13:43:45 +0300

    hw4

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Manager, Pool
 import os
+
+
+def _word_count_task(path_to_dir, filename, counter):
+    res = 0
+    with open(os.path.join(path_to_dir, filename), 'r') as f:
+        for line in f:
+            res += len(line.split())
+    # Не большая проблема с шаред памятью.. но ладно
+    counter[filename] = res
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +25,9 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    counter = manager.dict()
+
+    with Pool(os.cpu_count()) as p:
+        p.starmap(_word_count_task, [(path_to_dir, filename, counter) for filename in os.listdir(path_to_dir)])
+    return {'total': sum(counter.values()), **counter}
