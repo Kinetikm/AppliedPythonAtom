187c68c055caef50230bc96dbad31fd87541ad72
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-03-26 00:25:00 +0300

    HW4

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 from multiprocessing import Process, Manager
-import os
+from os import listdir
+
+
+def ProcFunction(path_to_file, filename, OutDict):
+    counter = 0
+    with open(path_to_file + '/' + filename, "r",
+              encoding='utf8') as fd:
+        for line in fd:
+            counter += len(line.split())
+        fd.close()
+    OutDict[filename] = counter
+    OutDict["total"] += counter
 
 
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    OutDict = Manager().dict({"total": 0})
+    bufferForProc = list()
+    for filename in listdir(path_to_dir):
+        procces_1 = Process(target=ProcFunction(path_to_dir,
+                                                filename, OutDict))
+        bufferForProc.append(procces_1)
+        procces_1.start()
+
+    for proc in bufferForProc:
+        proc.join()
+    return OutDict
