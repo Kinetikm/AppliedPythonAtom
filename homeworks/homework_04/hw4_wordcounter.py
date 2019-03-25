9a5ec843f7e34aea5648dcfdf826cd84ee01c4ea
BCroco <ninok_orb56@mail.ru>
2019-03-26 00:38:49 +0300

    hw_4

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -5,6 +5,17 @@ from multiprocessing import Process, Manager
 import os
 
 
+def count_words_in_file(queue, main_dict):
+    while not queue.empty():
+        file_path, name = queue
+        try:
+            with open(file_path + '/' + name, "r", encoding='utf8') as file:
+                main_dict[name] = len(file.read().split())
+            queue.task_done()
+        except FileNotFoundError:
+            return
+
+
 def word_count_inference(path_to_dir):
     '''
     Метод, считающий количество слов в каждом файле из директории
@@ -16,4 +27,18 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
+    main_dict = Manager().dict()
+    queue = Manager().Queue()
+    procs = []
+    file_names = list(os.listdir(path_to_dir))
+    for name in file_names:
+        queue.put((path_to_dir, name))
+    for _ in range(4):
+        p = Process(target=count_words_in_file, args=(queue, main_dict))
+        procs.append(p)
+        p.start()
+    for p in procs:
+        p.join()
+
+    main_dict['total'] = sum(main_dict.values())
     raise NotImplementedError
