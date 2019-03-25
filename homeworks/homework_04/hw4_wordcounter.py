c8a23b78c325b6c9740185ce2f1d022c0b89e1af
Maxim Khomyak <0xc00000f@gmail.com>
2019-03-25 20:47:22 +0300

    released counter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Queue, Lock
 import os
 
 
@@ -16,4 +15,37 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+
+    q = Queue()
+
+    word_cnt = Manager().dict()
+    word_cnt['total'] = 0
+
+    for file in os.listdir(path_to_dir):
+        q.put((path_to_dir, file))
+
+    tasks = [Process(target=word_counter, args=(q, word_cnt)) for _ in range(2)]
+
+    for task in tasks:
+        task.start()
+
+    for task in tasks:
+        task.join()
+
+    return word_cnt
+
+
+def word_counter(q, cnt):
+
+    print(q, cnt)
+
+    while q.qsize() > 0:
+        path, file = q.get()
+        counter = 0
+        with open(os.path.join(path, file), 'r') as data_file:
+            data = data_file.readlines()
+            not_empty = list(filter(lambda x: x != "\n", data))
+            for i in not_empty:
+                counter += len(i.split(" "))
+        cnt[file] = counter
+        cnt['total'] += counter
