6a6481ba9a9b0fb9509f02da795f7cc5aa98e49b
NSKuznetsov <helloqwtheslayer@mail.ru>
2019-03-26 04:10:34 +0300

    hw4_wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+
+from multiprocessing import Process, Manager, Queue, Pool
 import os
+
+
+def amount_of_files_words(file):
+    amount_of_words = 0
+    with open(file, 'r', encoding='utf-8') as f:
+        for line in f:
+            amount_of_words += len(line.split())
+    return amount_of_words
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +25,17 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    total_words = 0
+    Dict = {}
+    q = Queue()
+    for file in os.listdir(path_to_dir):
+        q.put(file)
+    with Pool(4) as p:
+        while q.empty() == False:
+            file = q.get()
+            path_to_file = path_to_dir + '/' + file
+            amount_of_words = p.apply(amount_of_files_words, (path_to_file,))
+            Dict[file] = amount_of_words
+            total_words += amount_of_words
+    Dict['total'] = total_words
+    return Dict
