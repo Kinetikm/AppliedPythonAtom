b190818976c12ac2da94af487b01bae893f6c84b
petroglif <ofisse884@mail.ru>
2019-03-26 20:51:23 +0300

    HW4

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
+import functools
 import os
+
+dict = Manager().dict()
+
+
+def read_file(namefile, pth):
+    with open(pth + '/' + namefile) as f:
+        dict[namefile] = len(f.read().split())
 
 
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    pool = Pool()
+    list_of_files = os.listdir(path_to_dir)
+    pool.map(functools.partial(read_file, pth=path_to_dir), list_of_files)
+    pool.close()
+    pool.join()
+    dict['total'] = sum(dict.values())
+    return dict
