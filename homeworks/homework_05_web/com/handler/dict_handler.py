192e771ee6742ad736cd6d56d8c63dbb3a7b8bd9
AsAsgard <AsAsgard@localhost.localdomain>
2019-03-31 14:37:20 +0300

    More changes in HW5

--- homeworks/homework_05_web/com/handler/dict_handler.py
+++ homeworks/homework_05_web/com/handler/dict_handler.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import com.handler.treeset
 from collections import OrderedDict
+import operator
 
 
-def reverse_dict_to_ordered(inputDict):
-    pass
+def reverse_dict_to_ordered(inputDict: dict):
+    inversedDict = dict()
+    for pair in inputDict.items():
+        if pair[1] not in inversedDict:
+            inversedDict[pair[1]] = treeset()
+        inversedDict[pair[1]].insert(pair[0])
+    resultDict = OrderedDict(sorted(inversedDict.items(), key=operator.itemgetter(0)))
+    return resultDict
