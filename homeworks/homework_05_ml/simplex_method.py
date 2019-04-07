41dff70ed792fcf307ba3a13c4464e408b2ab693
Nati098 <chicken000@mail.ru>
2019-04-07 21:15:44 +0300

    Add hw5 (ml)

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,33 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n, m = a.shape
+
+    a_big = np.vstack((a, np.negative(c)))
+    a_big = np.hstack((a_big, np.eye(a_big.shape[0])))
+    a_big = np.hstack((a_big, np.append(b, [0]).reshape((n+1, 1))))
+
+    # буферный список для индексов res списка по строкам
+    row_res_elements = np.array([i for i in range(m, m+n)])
+    res = np.zeros(m)
+
+    while np.min(a_big[-1, :-1]) < 0:
+        pivot_column_index = np.argmin(a_big[-1, :-1])
+        pivot_row_index = np.argmin(((a_big[:, -1] / a_big[:, pivot_column_index])[:-1]))
+        pivot_element = a_big[pivot_row_index, pivot_column_index]
+
+        a_big[pivot_row_index] /= pivot_element
+
+        # индексы всех строк, кроме pivot
+        row_idxs = [i for i in range(a_big.shape[0]) if i != pivot_row_index]
+        for idx in row_idxs:
+            buf_element = a_big[idx][pivot_column_index]
+            a_big[idx] -= buf_element*a_big[pivot_row_index]
+
+        row_res_elements[pivot_row_index] = pivot_column_index
+
+    for idx1, idx2 in zip(row_res_elements, range(a_big.shape[0])):
+        if idx1 < m:
+            res[idx1] = a_big[:, -1][idx2]
+
+    return res
