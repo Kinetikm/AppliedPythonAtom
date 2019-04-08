5c7baf51f5d85c494deb16e91d548b2dbf051838
Dima1122 <dm-avdeevyandex.ru>
2019-04-08 22:38:25 +0300

    withoutCSR

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,24 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    answer = np.zeros(a.shape[1])
+    x = np.zeros(max(a.shape[1], a.shape[0]))
+    e = np.eye(len(a) + 1)
+    b_sim_matrix = np.hstack((b, 0)).reshape(len(b) + 1, 1)
+    matrix = np.hstack(((np.vstack((a, (-1 * c))), e)))
+    sim_matrix = np.hstack((matrix, b_sim_matrix))
+    while min(sim_matrix[-1, :]) < 0:
+        lead_col = sim_matrix[-1, :].argmin()
+        lead_row = (sim_matrix[:-1, -1] / sim_matrix[:-1,
+                                                     lead_col]).argmin()
+        key_element = sim_matrix[lead_row, lead_col]
+        sim_matrix[lead_row, :] = sim_matrix[lead_row, :] / key_element
+        for i in range(sim_matrix.shape[0]):
+            if i != lead_row:
+                sim_matrix[i, :] = sim_matrix[i, :] - \
+                    sim_matrix[lead_row, :] * sim_matrix[i, lead_col]
+        x[lead_row] = lead_col + 1
+    for i in range(len(x)):
+        if int(x[i]) != 0:
+            answer[int(x[i]) - 1] = sim_matrix[i, -1]
+    return answer
