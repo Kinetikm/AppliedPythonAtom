e403ac318ba7e3871ba272c1e6d15ef6608a6234
BCroco <ninok_orb56@mail.ru>
2019-04-08 16:19:07 +0300

    simplex_method_+_notebook

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,30 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n, m = np.array(a).shape
+    main_matrix = np.zeros((n + 1, n + m + 2))
+    main_matrix[:n, :m] = np.array(a)
+    main_matrix[-1, :m] = -np.array(c)
+    main_matrix[:-1, -1] = np.array(b).T
+    main_matrix[:, m:-1] = np.eye(n + 1, n + 1)
+    while min(main_matrix[-1, :]) < 0:
+        pivot_column = main_matrix[-1, :].argmin()
+        pivot_row = (main_matrix[:-1, -1] / main_matrix[:-1,
+                                            pivot_column]).argmin()
+        main_matrix[pivot_row, :] /= main_matrix[pivot_row, pivot_column]
+        for i in range(n + 1):
+            if i != pivot_row:
+                main_matrix[i, :] -= main_matrix[pivot_row, :] * main_matrix[
+                    i, pivot_column]
+    index = np.where(main_matrix[:-1, :m] == 1)
+    result_x = np.zeros(m, dtype='i4')
+    for i in range(len(index[0])):
+        result_x[index[1][i]] = main_matrix[index[0][i], -1]
+    return result_x
+
+
+a = np.array([[2, 3, 2], [1, 1, 2]])
+b = np.array([1000, 800])
+c = np.array([7, 8, 10])
+
+x = simplex_method(a, b, c)
