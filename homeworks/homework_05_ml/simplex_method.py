6da807a402880f3c94a6a858b2542d3914914d9e
bibliophagist <greedthisissad@gmail.com>
2019-04-08 15:22:57 +0300

    HW 5 of 6 tasks

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -7,7 +7,7 @@ import numpy as np
 def simplex_method(a, b, c):
     """
     Почитать про симплекс метод простым языком:
-    * https://  https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
+    * https:// https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
     Реализацию алгоритма взять тут:
     * https://youtu.be/gRgsT9BB5-8 (это ссылка на 1-ое из 5 видео).
 
@@ -20,4 +20,25 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    a = a.astype('float64')
+    b = b.astype('float64')
+    c = c.astype('float64')
+    c = -1 * c
+    while not (c >= 0).all():
+        column_with_min_in_c = c.argmin()
+        row_with_min_in_a = (b / a[:, column_with_min_in_c]).argmin()
+        b[row_with_min_in_a] = b[row_with_min_in_a] / a[
+            row_with_min_in_a, column_with_min_in_c]
+        a[row_with_min_in_a] = a[row_with_min_in_a, :] / a[
+            row_with_min_in_a, column_with_min_in_c]
+        for i in range(a.shape[0]):
+            if i != row_with_min_in_a:
+                b[i] = -b[row_with_min_in_a] * a[i, column_with_min_in_c] + b[
+                    i]
+                a[i] = -a[i, column_with_min_in_c] * a[row_with_min_in_a] + a[
+                    i]
+        c = -c[column_with_min_in_c] * a[
+            row_with_min_in_a] + c
+    a[:, np.where(c > 0)[0]] = 0
+    x = b.T @ a
+    return x
