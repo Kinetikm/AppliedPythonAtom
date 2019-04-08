355633b72695ed810088aac3c1778e1ca5f8d320
Antonova Lidia <antonovalv98@mail.ru>
2019-04-08 23:56:52 +0300

    async variant

--- homeworks/homework_05_web/get_top_user_comments.py
+++ homeworks/homework_05_web/get_top_user_comments.py
@@ -2,7 +2,8 @@ import sys
 from bs4 import BeautifulSoup
 import re
 import collections
-import requests
+import asyncio
+import aiohttp
 
 # Ваши импорты
 
@@ -12,29 +13,49 @@ headers = {
 }
 
 
-def parce_and_write(link):
-    response = BeautifulSoup(requests.get(link, headers=headers).text, 'html.parser')
+async def get_html_page(link):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link, headers=headers) as res:
+            page = await res.text()
+            return page
+
+
+async def main(links):
+    result = await asyncio.gather(*(get_html_page(link) for link in links))
+    return result
+
+
+def write_to_file(filename, links):
+    data_to_record = list(asyncio.run(main(links)))
+    result_dict = dict()
+    try:
+        with open(filename, 'w') as file:
+            file.write("link, username, count_comments \n")
+            for index in range(len(data_to_record)):
+                buf = parser(data_to_record[index])
+                for tmp in sorted(buf, key=buf.get, reverse=True):
+                    result_dict[tmp] = buf[tmp]
+                for user in result_dict:
+                    file.write(links[index] + "," + user + "," + str(result_dict[user]) + "\n")
+    except IOError:
+        print("An IOError has occurred")
+
+
+def parser(page):
+    page_to_work_with = BeautifulSoup(page, "html.parser")
     result = [re.sub('<.+?>', '', str(user))
-              for user in response.findAll('span', attrs={'class': 'user-info__nickname_comment'})]
+              for user in page_to_work_with.findAll('span', attrs={'class': 'user-info__nickname_comment'})]
     users = collections.Counter()
     for word in result:
         users[word] += 1
-    users = users.most_common()
-    print(users)
-    try:
-        with open(filename, 'a') as file:
-            file.write('link, username, count_of_comments \n')
-            for user in users:
-                file.write(link + ',' + user[0] + ',' + str(user[1]) + '\n')
-    except IOError:
-        print("An IOError has occurred")
+    users_result = collections.OrderedDict(users.most_common())
+    return users_result
 
 
 if __name__ == '__main__':
     filename = 'top_user_comments.csv'
     links = sys.argv[1:4]
     links.sort()
-    for link in links:
-        parce_and_write(link)
+    write_to_file(filename, links)
 
     # Ваш код
