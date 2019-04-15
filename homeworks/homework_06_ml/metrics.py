c7b8cd102d5c04ac6b3abb7a97ae10d887828393
IgorKurylev <kurilevigor@mail.ru>
2019-04-15 21:20:43 +0300

    metrics

--- homeworks/homework_06_ml/metrics.py
+++ homeworks/homework_06_ml/metrics.py
@@ -6,30 +6,22 @@ import numpy as np
 
 
 def mse(y_true, y_hat, derivative=False):
-    """
-    Mean squared error regression loss
-    :param y_true: vector of truth (correct) target values
-    :param y_hat: vector of estimated target values
-    :return: loss
-    """
-    pass
+    y_true = np.array(y_true)
+    y_hat = np.array(y_hat)
+    if derivative:
+        return np.mean(2*(y_true - y_hat))
+
+    return np.mean((y_true - y_hat)**2)
 
 
 def mae(y_true, y_hat):
-    """
-    Mean absolute error regression loss
-    :param y_true: vector of truth (correct) target values
-    :param y_hat: vector of estimated target values
-    :return: loss
-    """
-    pass
+    y_true = np.array(y_true)
+    y_hat = np.array(y_hat)
+    return np.mean(np.abs(y_true - y_hat))
 
 
 def r2_score(y_true, y_hat):
-    """
-    R^2 regression loss
-    :param y_true: vector of truth (correct) target values
-    :param y_hat: vector of estimated target values
-    :return: loss
-    """
-    pass
+    y_true = np.array(y_true)
+    y_hat = np.array(y_hat)
+    return 1 - (np.sum((y_true - y_hat) ** 2) /
+                np.sum((y_true - np.mean(y_true))) ** 2)
