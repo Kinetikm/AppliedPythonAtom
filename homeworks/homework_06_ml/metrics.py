47db0bc93a13f37234fded9d3f037d7148247bd1
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-04-15 12:00:49 +0300

    Metrics

--- homeworks/homework_06_ml/metrics.py
+++ homeworks/homework_06_ml/metrics.py
@@ -12,7 +12,10 @@ def mse(y_true, y_hat, derivative=False):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    y_true = np.array(y_true)
+    y_hat = np.array(y_hat)
+    if len(np.array(y_true)) == len(np.array(y_hat)):
+        return np.mean((y_true - y_hat)**2)
 
 
 def mae(y_true, y_hat):
@@ -22,7 +25,10 @@ def mae(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    y_true = np.array(y_true)
+    y_hat = np.array(y_hat)
+    if len(np.array(y_true)) == len(np.array(y_hat)):
+        return np.mean(np.abs(y_true - y_hat))
 
 
 def r2_score(y_true, y_hat):
@@ -32,4 +38,8 @@ def r2_score(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    y_true = np.array(y_true)
+    y_hat = np.array(y_hat)
+    if len(np.array(y_true)) == len(np.array(y_hat)):
+        return 1 - (np.sum((y_true - y_hat)**2) /
+                    np.sum((y_true - np.mean(y_true)))**2)
