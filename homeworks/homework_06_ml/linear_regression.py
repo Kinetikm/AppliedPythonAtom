77c0b1d90d9f5b36c947520431eb7e60125bcad2
AidarValshin <Aidar_V2000@mail.ru>
2019-04-16 00:18:46 +0300

    “linear

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,58 +1,52 @@
-#!/usr/bin/env python
-# coding: utf-8
 import numpy as np
 from metrics import mse
 
 
-class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
+class MLinearRegression:
+    def __init__(self, lambda_coef=0.1, regulatization=None, alpha=0.5):
         """
-        :param lambda_coef: constant coef for gradient descent step шаг спуска
+        :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        self.learning_rate = lambda_coef
+        self.lambda_coef = lambda_coef
         self.regularizarion = regulatization
         self.alpha = alpha
         self.train = False
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, iterations=1000, delta=0.1 ** 10):
         """
         Fit model using gradient descent method
         :param X_train: training data
         :param y_train: target values for training data
         :return: None
         """
-        iterations = 1000
-        delta = 0.1 ** 5
-        if (X_train.shape[0] == y_train.shape[0]):
-            # создааем матрицу Х ов ,где первый столбец единичный
-            x_vect = np.hstack(((np.ones((X_train.shape[0], 1))), X_train))
-            # колчисество стобцов в матрице X
-            cols_X = x_vect.shape[1]
-            # веса.первый стобец будет для w0
-            self.w = (np.random.randn(cols_X) / np.sqrt(cols_X)).reshape(1, -1)
-            history = np.zeros(iterations + 1)
-            for it in range(iterations):
-                # https://habr.com/ru/company/ods/blog/322076/ брал отсюда
-                if self.regularizarion == 'L1':
-                    r = self.alpha * np.absolute(self.w) / 2
-                    r[0] = 0
-                elif self.regularizarion == 'L2':
-                    r = self.alpha * np.square(self.w ** 2) / 2
-                    r[0] = 0
-                else:
-                    r = 0
-                prediction = self.predict(X_train)
-                self.w -= (-2/ x_vect.shape[0]) *( self.learning_rate * x_vect.T.dot(
-                        (prediction - y_train)) + r)
-                history[it + 1] = mse(y_train, prediction)
-                if (it > 0):
-                    if np.abs(history[it + 1] - history[it]) < delta:
-                        break
-            # coef = self.r[1:] intercept = self.r[0]
-            self.train = True
-            return self
+
+        if X_train.shape[0] != y_train.shape[0]:
+            raise KeyError
+        self.train = True
+        change = np.zeros(iterations)
+        x_train = np.hstack([np.ones((X_train.shape[0], 1)), X_train])
+        columns = x_train.shape[1]
+        self.w = np.zeros(columns)
+        for i in range(iterations):
+            # https://habr.com/ru/company/ods/blog/322076/ брал отсюда
+            if self.regularizarion == 'L1':
+                r = self.alpha * np.absolute(0.5 * self.w)
+                r[0] = 0
+            elif self.regularizarion == 'L2':
+                r = self.alpha * np.square(self.w * 0.25)
+                r[0] = 0
+            else:
+                r = 0
+            prediction = self.predict(x_train)
+            self.w -= (2 / x_train.shape[0]) * self.lambda_coef * (x_train.T.dot(
+                (prediction - y_train)) + r)
+            change[i] = mse(y_train, prediction)
+            if i > 0 and np.abs(change[i] - change[i - 1]) < delta:
+                # print(i)
+                break
+        return self
 
     def predict(self, X_test):
         """
@@ -60,15 +54,18 @@ class LinearRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        assert getattr(self, 'train') is False, 'Model is not fitted'
-        x_test = np.hstack([np.ones((X_test.shape[0], 1)), X_test])
-        return x_test.dot(self.w)
+        assert self.train, 'Model is not trained'
+        if all(X_test[:, 0] == 1):
+            return X_test.dot(self.w)
+        else:
+            ones = np.ones((X_test.shape[0], 1))
+            x_test = np.hstack([ones, X_test])
+            return x_test.dot(self.w)
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        assert getattr(self, 'train') is False, 'Model is not fitted'
-        # assert getattr(self, 'w', 0) != 0, 'Model is not fitted'
+        assert self.train, 'Model is not trained'
         return self.w
