3241b5b650486cea73497edb30e9b20643a4cbe3
HamsterMephi <Anti-demon@mail.ru>
2019-04-16 05:04:01 +0300

    Только simplex

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import numpy as np
+import pandas as pd
 
 
 def simplex_method(a, b, c):
@@ -20,4 +21,39 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n, m = len(a), len(a[0])
+    SimplexTable = np.zeros((n+1, m+n+2))
+    Optimized_values = np.zeros(m)
+    IdentityMatrix = np.eye(n + 1)
+
+    SimplexTable[:n, :m] = a
+    SimplexTable[:n+1, m:m+n+1] = IdentityMatrix
+    SimplexTable[:n, -1] = b
+    MaxB = b.max()
+    SimplexTable[-1, :m] = -c
+    Optimized_values_lines = np.full(n, -1)
+
+    while(SimplexTable[-1].min() < 0):
+        ResColumn = np.argmin(SimplexTable[-1])
+
+        b[:] = SimplexTable[:n, -1]
+        for i in range(n):
+            if SimplexTable[i, ResColumn] == 0:
+                b[i] = 1 + MaxB
+            else:
+                b[i] /= SimplexTable[i, ResColumn]
+        ResLine = np.argmin(b)
+
+        inter = SimplexTable[ResLine, ResColumn]
+        if inter != 1:
+            SimplexTable[ResLine, :] /= inter
+        for i in range(n+1):
+            if i != ResLine:
+                SimplexTable[i, :] += -1 * SimplexTable[i, ResColumn] * SimplexTable[ResLine, :]
+        Optimized_values_lines[ResLine] = ResColumn
+
+    for i in range(n):
+        if Optimized_values_lines[i] >= 0:
+            Optimized_values[Optimized_values_lines[i]] = SimplexTable[i, -1]
+
+    return Optimized_values
