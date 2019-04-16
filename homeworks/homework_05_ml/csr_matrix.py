8b4a59c373f067249c7e46bb1b34c94bd96f7559
Olegiwe <o.kuznetsov>
2019-04-16 04:05:52 +0300

    csr tested

--- homeworks/homework_05_ml/csr_matrix.py
+++ homeworks/homework_05_ml/csr_matrix.py
@@ -11,68 +11,27 @@ class CSRMatrix:
     Here you can read how CSR sparse matrix works: https://en.wikipedia.org/wiki/Sparse_matrix
     """
     def __init__(self, init_matrix_representation):
-        """
-        :param init_matrix_representation: can be usual dense matrix
-        or
-        (row_ind, col, data) tuple with np.arrays,
-            where data, row_ind and col_ind satisfy the relationship:
-            a[row_ind[k], col_ind[k]] = data[k]
-             imr = init_matrix_representation
+
+        imr = init_matrix_representation
         self.a = np.array([], dtype=int)
         self.ia = np.array([0], dtype=int)
         self.ja = np.array([], dtype=int)
         if isinstance(imr, tuple) and len(imr) == 3:
-            ind = np.lexsort((imr[1], imr[0]))
-            self.a = np.append(self.a, [imr[2][i] for i in ind if imr[2][i] != 0])
-            self.ja = np.append(self.ja, [imr[1][i] for i in ind if imr[2][i] != 0])
+            indx = np.lexsort((imr[1], imr[0]))
+            self.a = \
+                np.append(self.a, [imr[2][i] for i in indx if imr[2][i] != 0])
+            self.ja = \
+                np.append(self.ja, [imr[1][i] for i in indx if imr[2][i] != 0])
             self.ia = np.zeros(max(imr[0]) + 2, dtype=int)
-            for i in [imr[0][i] for i in ind if imr[2][i] != 0]:
+            for i in [imr[0][i] for i in indx if imr[2][i] != 0]:
                 self.ia[i + 1:] += 1
         elif isinstance(imr, np.ndarray):
             self.a = np.append(self.a, imr[np.nonzero(imr)])
             self.ja = np.append(self.ja, np.nonzero(imr)[1])
-            summa = 0
+            total_elems = 0
             for i in np.arange(imr.shape[0]):
-                summa += imr[i, :][np.nonzero(imr[i, :])].shape[0]
-                self.ia = np.append(self.ia, summa)
-        else:
-            raise ValueError
-        """
-        self.a = np.array([], dtype=float)
-        self.ia = np.array([0], dtype=int)
-        self.ja = np.array([], dtype=int)
-        imr = init_matrix_representation
-        if isinstance(init_matrix_representation, tuple) and len(init_matrix_representation) == 3:
-            last_row = 0
-            temp_row = 0
-            for k in range(0, len(imr[2])):
-                if imr[2][k] == 0:
-                    continue
-                self.a = np.append(self.a, imr[2][k])
-                self.ja = np.append(self.ja, imr[1][k])
-                if imr[0][k] == last_row:
-                    temp_row += 1
-                else:
-                    self.ia = np.append(self.ia, self.ia[-1] + temp_row)
-                    last_row = imr[0][k]
-                    temp_row = 1
-            self.ia = np.append(self.ia, self.ia[-1] + temp_row)
-        elif isinstance(init_matrix_representation, np.ndarray):
-            last_row = 0
-            temp_row = 0
-            iter = np.nditer(imr, flags=['multi_index'])
-            while not iter.finished:
-                if iter[0] == 0:
-                     continue
-                self.a = np.append(self.a, iter[0])
-                self.ja = np.append(self.ja, iter.multi_index[1])
-                if iter.multi_index[0] == last_row:
-                    temp_row += 1
-                else:
-                    self.ia = np.append(self.ia, self.ia[-1] + temp_row)
-                    temp_row = 0
-                iter.iternext()
-            self.ia = np.append(self.ia, self.ia[-1] + temp_row)
+                total_elems += imr[i, :][np.nonzero(imr[i, :])].shape[0]
+                self.ia = np.append(self.ia, total_elems)
         else:
             raise ValueError
 
@@ -81,8 +40,9 @@ class CSRMatrix:
         Return value in i-th row and j-th column.
         Be careful, i and j may have invalid values (-1 / bigger that matrix size / etc.).
         """
-        if not ((isinstance(i, (int, float)))
-                and (isinstance(j, (int, float)))):
+        if not isinstance(i, (int, float)):
+            return None
+        if not isinstance(j, (int, float)):
             return None
         if i < 0 or j < 0:
             return None
@@ -100,15 +60,15 @@ class CSRMatrix:
         Set the value to i-th row and j-th column.
         Be careful, i and j may have invalid values (-1 / bigger that matrix size / etc.).
         """
-        if not ((isinstance(i, (int, float)))
-                and (isinstance(j, (int, float)))):
+        if not isinstance(i, (int, float)):
+            return None
+        if not isinstance(j, (int, float)):
             return None
         if i < 0 or j < 0:
             return None
         is_zero = True
         for k in np.arange(self.ia[i], self.ia[i+1]):
             if self.ja[int(k)] == j:
-                #self.a[int(k)] = value
                 if value != 0:
                     self.a[int(k)] = value
                 else:
@@ -117,21 +77,21 @@ class CSRMatrix:
                     self.ia[i + 1:] -= 1
                 is_zero = False
                 break
-        if is_zero == False:
+        if not is_zero:
             return
-        if self.ia[i + 1] == self.ia[i]:
-            self.ja = np.insert(self.ja, self.ia[i], j)
-            self.a = np.insert(self.a, self.ia[i], value)
+        LB = self.ia[i]
+        RB = self.ia[i + 1]
+        if RB == LB:
+            self.ja = np.insert(self.ja, LB, j)
+            self.a = np.insert(self.a, LB, value)
             self.ia[i + 1:] += 1
         else:
-            for k in range(self.ia[i], self.ia[i + 1]):
-                if (j < self.ja[k]
-                        or k == np.arange(self.ia[i], self.ia[i + 1])[-1]):
+            for k in range(LB, RB):
+                if (j < self.ja[k] or k == np.arange(LB, RB)[-1]):
                     self.ja = np.insert(self.ja, k + 1, j)
                     self.a = np.insert(self.a, k + 1, value)
                     self.ia[i + 1:] += 1
-
-
+        return
 
     def to_dense(self):
         """
