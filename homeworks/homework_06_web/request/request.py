c52ab87c64895260ed40775505ce2064870e3816
SafonovaD <safonovadashav@gmail.com>
2019-04-18 00:08:29 +0300

    HW_6

--- homeworks/homework_06_web/request/request.py
+++ homeworks/homework_06_web/request/request.py
@@ -1,10 +1,7 @@
 import uuid
 
-from homeworks.homework_06_web.request.request_type import RequestType
-
 
 class Request:
-    request_type: RequestType
 
     def __init__(self, request_type, authors=None, title=None, key_words=None, abstract=None):
         self.request_type = request_type
