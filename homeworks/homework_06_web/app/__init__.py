f4a34dd5baa860ca9025d21bdfa9a5814274d795
AsAsgard <dbrylkin96@gmail.com>
2019-04-18 08:08:25 +0300

    More changes in hw

--- homeworks/homework_06_web/app/__init__.py
+++ homeworks/homework_06_web/app/__init__.py
@@ -2,20 +2,42 @@
 # coding: utf-8
 
 from flask import Flask
+import logging
 from .database import db
 import appconfig
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(appconfig.currentConfig)
+def create_app(Log=None):
 
+    do_logging = False
+    if isinstance(Log, logging.Logger):
+        do_logging = True
+
+    def writeLog(mes):
+        if do_logging:
+            Log.info(mes)
+
+    writeLog('Creating app...')
+    app = Flask(__name__)
+    writeLog('App created')
+    writeLog('Configurating config...')
+    app.config.from_object(appconfig.currentConfig)
+    writeLog('Config configured')
+
+    writeLog('Initialize database...')
     db.init_app(app)
+    writeLog('Database initialized')
     with app.test_request_context():
+        writeLog('Creating tables...')
         db.create_all()
+        writeLog('Tables created...')
 
     import app.queries.data_handler as data_handler
 
     app.register_blueprint(data_handler.data_handler)
 
     return app
+
+
+def create_logger():
+    return logging.getLogger(appconfig.currentConfig.LOGGER_NAME)
--- homeworks/homework_06_web/manage.py
+++ homeworks/homework_06_web/manage.py
@@ -4,10 +4,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app.database import db
+from app.logger import Logger
 from app import create_app
 
-
-app = create_app()
+app = create_app(Logger)
 migrate = Migrate(app, db)
 manager = Manager(app)
 
@@ -15,4 +15,5 @@ manager.add_command('db', MigrateCommand)
 
 
 if __name__ == "__main__":
+    Logger.info('Starting server')
     manager.run()
