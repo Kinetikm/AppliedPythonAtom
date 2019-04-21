938477ab910646769e296e9cf03043c9dca78ec5
Dima1122 <dm-avdeevyandex.ru>
2019-04-21 23:21:06 +0300

    metrics_LogReg

--- homeworks/homework_07_ml/logreg/logistic_regression.py
+++ homeworks/homework_07_ml/logreg/logistic_regression.py
@@ -6,14 +6,28 @@ import numpy as np
 
 
 class LogisticRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
+    def __init__(
+            self,
+            n_iter=100,
+            lambda_coef=1.0,
+            regulatization=None,
+            alpha=0.5):
         """
         LogReg for Binary case
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        pass
+        self.lam = lambda_coef
+        assert regulatization is None or regulatization == 'L1' or regulatization == 'L2', 'You entered wrong regularization'
+        self.reg = regulatization
+        self.alpha = alpha
+        self.coef_ = None
+        self.intercept_ = None
+        self.n = n_iter
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +36,49 @@ class LogisticRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+
+        def grad_f_withoutL(self, X_train, y_train, coef_last):
+            return 1 / \
+                len(X_train) * X_train.transpose() @ (self.sigmoid(X_train @ coef_last) - y_train)
+
+        def grad_f_L1(self, X_train, y_train, coef_last):
+            return 1 / len(X_train) * X_train.transpose() @ (self.sigmoid(
+                X_train @ coef_last) - y_train) + self.alpha * 2 * coef_last
+
+        def grad_f_L2(self, X_train, y_train, coef_last):
+            return 1 / len(X_train) * X_train.transpose() @ (self.sigmoid(X_train @
+                                                                          coef_last) - y_train) + self.alpha * np.ones(X_train.shape[1])
+
+        def normalize(X_train):
+            mean = np.mean(X_train, axis=0)
+            std = np.std(X_train, axis=0)
+            return (X_train - mean) / std
+
+        assert X_train.shape[0] == y_train.shape[0], 'Invalid dimensions'
+
+        X_train = normalize(X_train)
+        step = self.lam
+        X_train = np.c_[np.ones(X_train.shape[0]), X_train]
+        self.coef_ = np.random.randn(X_train.shape[1])
+        for i in range(self.n):
+            coef_last = np.copy(self.coef_)
+            if self.reg is None:
+                self.coef_ = coef_last - step * \
+                    grad_f_withoutL(self, X_train, y_train, coef_last)
+            if self.reg == 'L1':
+                self.coef_ = coef_last - step * \
+                    grad_f_L1(self, X_train, y_train, coef_last)
+            if self.reg == 'L2':
+                self.coef_ = coef_last - step * \
+                    grad_f_L2(self, X_train, y_train, coef_last)
+
+            if np.all(abs(self.coef_ - coef_last) <
+                      np.zeros(X_train.shape[1]) + 0.000001):
+                self.intercept_ = self.coef_[0]
+                self.coef_ = self.coef_[1:]
+                return
+        self.intercept_ = self.coef_[0]
+        self.coef_ = self.coef_[1:]
 
     def predict(self, X_test):
         """
@@ -30,7 +86,18 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        try:
+            pred_values = np.sign(
+                np.sum(
+                    self.intercept_ +
+                    self.coef_ *
+                    X_test,
+                    axis=1))
+            pred_values.astype(int)
+            pred_values[pred_values == -1] = 0
+            return pred_values
+        except TypeError:
+            print('Please, train your model first')
 
     def predict_proba(self, X_test):
         """
@@ -38,11 +105,22 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        pass
+        try:
+            return self.sigmoid(
+                np.sum(
+                    self.intercept_ +
+                    self.coef_ *
+                    X_test,
+                    axis=1))
+        except TypeError:
+            print('Please, train your model first')
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        if np.all(self.coef_ is None):
+            print('Please, train your model first')
+            return
+        return np.hstack((self.intercept_, self.coef_))
---
+++ homeworks/homework_07_ml/logreg/metrics.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+
+
+def logloss(y_true, y_pred):
+    """
+    logloss
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated probabilities
+    :return: loss
+    """
+    sum = y_true @ np.log(y_pred) + (np.ones(len(y_pred)) - \
+                          y_true) @ np.log(np.ones(len(y_pred)) - y_pred)
+    return -sum / len(y_true)
+
+
+def accuracy(y_true, y_pred):
+    """
+    Accuracy
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated class values
+    :return: loss
+    """
+    TP = 0
+    TN = 0
+    for i in range(len(y_true)):
+        if (y_true[i] == 1 and y_pred[i] == 1):
+            TP += 1
+        if (y_true[i] == 0 and y_pred[i] == 0):
+            TN += 1
+    return (TP + TN) / len(y_pred)
+
+
+def presicion(y_true, y_pred):
+    """
+    presicion
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated class values
+    :return: loss
+    """
+    TP = 0
+    FP = 0
+    for i in range(len(y_true)):
+        if (y_true[i] == 1 and y_pred[i] == 1):
+            TP += 1
+        if (y_true[i] == 0 and y_pred[i] == 1):
+            FP += 1
+    return TP / (FP + TP)
+
+
+def recall(y_true, y_pred):
+    """
+    presicion
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated class values
+    :return: loss
+    """
+    TP = 0
+    FN = 0
+    for i in range(len(y_true)):
+        if (y_true[i] == 1 and y_pred[i] == 1):
+            TP += 1
+        if (y_true[i] == 1 and y_pred[i] == 0):
+            FN += 1
+    return TP / (TP + FN)
+
+
+def roc_auc(y_true, y_pred):
+    """
+    roc_auc
+    :param y_true: vector of truth (correct) target values
+    :param y_hat: vector of estimated probabilities
+    :return: loss
+    """
+    TPR = []
+    FPR = []
+    threshold = 0
+    for _ in range(100):
+        y_pred1 = np.copy(y_pred)
+        TP, FN, FP, TN = 0, 0, 0, 0
+        y_pred1[y_pred1 >= threshold] = 1
+        y_pred1[y_pred1 < threshold] = 0
+        y_pred1.astype(int)
+        for i in range(len(y_true)):
+            if (y_true[i] == 1 and y_pred1[i] == 1):
+                TP += 1
+            if (y_true[i] == 0 and y_pred1[i] == 1):
+                FP += 1
+            if (y_true[i] == 0 and y_pred1[i] == 0):
+                TN += 1
+            if (y_true[i] == 1 and y_pred1[i] == 0):
+                FN += 1
+        TPR = np.append(TPR, TP / (TP + FN))
+        FPR = np.append(FPR, FP / (FP + TN))
+        threshold += 0.01
+    return -np.trapz(TPR, FPR, axis=0)
