fca42c1509f2155b87cccb21ee0de992fbd5c141
bibliophagist <viktorkozlovkonst@gmail.com>
2019-04-22 23:31:39 +0300

    HW ml 7 metrics improvement (from slack)

--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -5,13 +5,15 @@
 import numpy as np
 
 
-def logloss(y_true, y_hat):
+def logloss(y_true, y_hat, eps=1e-8):
     """
     logloss
     :param y_true: vector of truth (correct) class values
     :param y_hat: vector of estimated probabilities
+    :param eps: for evading log(1) and log(0)
     :return: loss
     """
+    y_hat = np.clip(y_hat, eps, 1 - eps)
     return ((y_true * np.log2(y_hat) + (1 - y_true) * np.log2(
         1 - y_hat)).sum()) / (len(y_true))
 
@@ -60,7 +62,7 @@ def roc_auc(y_true, y_hat):
     :param y_hat: vector of estimated probabilities
     :return: loss
     1 + TPR - FPR
-    -------------   ????
+    -------------  = S  ????
          2
     """
     true_positive, false_positive, true_negative, false_negative = \
