4418184f0ab4975794dcbcfee390c855571f0627
AlexLyash <procusvorum@gmail.com>
2019-04-23 09:29:33 +0300

    hw7

--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -12,7 +12,9 @@ def logloss(y_true, y_pred):
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    assert y_true == y_pred, 'The sizes of y_true and y_pred vectors \
+    are different'
+    return np.log(1 + np.exp(- y_true*y_pred))
 
 
 def accuracy(y_true, y_pred):
@@ -22,7 +24,13 @@ def accuracy(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    assert y_true == y_pred, 'The sizes of y_true and y_pred vectors \
+    are different'
+    n_true = 0
+    for i in range(len(y_true)):
+        if y_true[i] == y_pred[i]:
+            n_true += 1
+    return n_true/len(y_true)
 
 
 def presicion(y_true, y_pred):
@@ -32,7 +40,16 @@ def presicion(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    assert y_true == y_pred, 'The sizes of y_true and y_pred vectors \
+    are different'
+    tp = 0  # True positive
+    fp = 0  # False positive
+    for i in range(len(y_true)):
+        if y_true[i] == 1 and y_true[i] == y_pred[i]:
+            tp += 1
+        if y_pred[i] == 1 and y_true[i] != y_pred[i]:
+            fp += 1
+    return tp/(tp + fp)
 
 
 def recall(y_true, y_pred):
@@ -42,7 +59,16 @@ def recall(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    assert y_true == y_pred, 'The sizes of y_true and y_pred vectors \
+    are different'
+    tp = 0  # True positive
+    fn = 0  # False negative
+    for i in range(len(y_true)):
+        if y_true == 1 and y_true[i] == y_pred[i]:
+            tp += 1
+        if y_true == 1 and y_true[i] != y_pred[i]:
+            fn += 1
+    return tp/(tp + fn)
 
 
 def roc_auc(y_true, y_pred):
@@ -52,4 +78,17 @@ def roc_auc(y_true, y_pred):
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    assert y_true == y_pred, 'The sizes of y_true and y_pred vectors \
+    are different'
+    treshold = 0.5  # If a probability given by y_pred is higher than
+    #  this value, we think that's a positive prediction
+    np.where(y_pred > treshold, 1, y_pred)
+    np.where(y_pred <= treshold, 0, y_pred)
+    tp = 0  # True positive
+    fp = 0  # False positive
+    for i in range(len(y_true)):
+        if y_true == 1 and y_true[i] == y_pred[i]:
+            tp += 1
+        if y_pred[i] == 1 and y_true[i] != y_pred[i]:
+            fp += 1
+    return np.trapz(fp, tp)
