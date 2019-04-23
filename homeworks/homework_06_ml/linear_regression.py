e386d1a0eae42a1e04461f595ef5bc1013e15942
Dima <48024372+Dima1122@users.noreply.github.com>
2019-04-23 09:32:13 +0300

    Add files via upload

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -11,10 +11,20 @@ class LinearRegression:
         :param alpha: regularizarion coefficent
         """
         self.lam = lambda_coef
+        assert regulatization is None or regulatization == 'L1' or regulatization == 'L2', 'You entered wrong reg'
         self.reg = regulatization
         self.alpha = alpha
         self.coef_ = None
         self.intercept_ = None
+
+    def grad_f(self, X_train, y_train, coef_last, regulator):
+        return -2 * (1 / X_train.shape[0]) * X_train.transpose().dot(y_train) + 2 * (
+            1 / X_train.shape[0]) * X_train.transpose().dot(X_train).dot(coef_last) + regulator
+
+    def normalize(self, X_train):
+        mean = np.mean(X_train, axis=0)
+        std = np.std(X_train, axis=0)
+        return (X_train - mean) / std
 
     def fit(self, X_train, y_train):
         """
@@ -23,51 +33,31 @@ class LinearRegression:
         :param y_train: target values for training data
         :return: None
         """
-
-        def grad_f_withoutL(self, X_train, y_train, coef_last):
-            return -2 * (1 / X_train.shape[0]) * X_train.transpose().dot(y_train) + 2 * (
-                1 / X_train.shape[0]) * X_train.transpose().dot(X_train).dot(coef_last)
-
-        def grad_f_L1(self, X_train, y_train, coef_last):
-            return -2 * (1 / X_train.shape[0]) * X_train.transpose().dot(y_train) + 2 * (
-                1 / X_train.shape[0]) * X_train.transpose().dot(X_train).dot(coef_last) + self.alpha * 2 * coef_last
-
-        def grad_f_L2(self, X_train, y_train, coef_last):
-            return -2 * (1 / X_train.shape[0]) * X_train.transpose().dot(y_train) + 2 * (1 / X_train.shape[0]) * \
-                X_train.transpose().dot(X_train).dot(coef_last) + self.alpha * np.ones(X_train.shape[1])
-
-        def normalize(X_train):
-            mean = np.mean(X_train, axis=0)
-            std = np.std(X_train, axis=0)
-            return (X_train - mean) / std
-
-        X_train = normalize(X_train)
+        assert X_train.shape[0] == y_train.shape[0], 'Invalid dimensions'
+        X_train = self.normalize(X_train)
         step = self.lam
         X_train = np.c_[np.ones(X_train.shape[0]), X_train]
         self.coef_ = np.random.randn(X_train.shape[1])
-        i = 0
-        while True:
-            i += 1
+        regulator = 0
+        for i in range(3000):
             coef_last = np.copy(self.coef_)
             if self.reg is None:
-                self.coef_ = coef_last - step * \
-                    grad_f_withoutL(self, X_train, y_train, coef_last)
+                regulator == 0
             if self.reg == 'L1':
-                self.coef_ = coef_last - step * \
-                    grad_f_L1(self, X_train, y_train, coef_last)
+                regulator = self.alpha * 2 * coef_last
             if self.reg == 'L2':
-                self.coef_ = coef_last - step * \
-                    grad_f_L2(self, X_train, y_train, coef_last)
+                regulator == self.alpha * np.ones(X_train.shape[1])
+
+            self.coef_ = coef_last - step * \
+                self.grad_f(X_train, y_train, coef_last, regulator)
 
             if np.all(abs(self.coef_ - coef_last) <
-                      np.zeros(X_train.shape[1]) + 0.000001):
+                      (np.zeros(X_train.shape[1]) + 0.000001)):
                 self.intercept_ = self.coef_[0]
                 self.coef_ = self.coef_[1:]
-                break
-            if i == 3000:
-                self.intercept_ = self.coef_[0]
-                self.coef_ = self.coef_[1:]
-                break
+                return
+        self.intercept_ = self.coef_[0]
+        self.coef_ = self.coef_[1:]
 
     def predict(self, X_test):
         """
@@ -75,11 +65,17 @@ class LinearRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        return np.sum(self.intercept_ + self.coef_ * X_test, axis=1)
+        try:
+            return np.sum(self.intercept_ + self.coef_ * X_test, axis=1)
+        except ValueError:
+            print('Please, train your model first')
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
+        if np.all(self.coef_ is None):
+            print('Please, train your model first')
+            return
         return np.hstack((self.intercept_, self.coef_))
