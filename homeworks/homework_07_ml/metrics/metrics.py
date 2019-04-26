6aed64725c1bfeb40e5e4ea422fbb8bf02515e1b
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-04-26 04:06:34 +0300

    Metrics

--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -12,7 +12,12 @@ def logloss(y_true, y_pred):
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    if len(y_true) != len(y_pred):
+        raise Exception("Size error")
+    # формула из лекции
+    coef = (-1)/len(y_true)
+    return coef*np.sum((y_true * np.log(y_pred)) +
+                       (1 - y_true) * np.log(1 - y_pred))
 
 
 def accuracy(y_true, y_pred):
@@ -22,7 +27,13 @@ def accuracy(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    if len(y_true) != len(y_pred):
+        raise Exception("Size error")
+    loss = 0
+    for i in len(y_true):
+        if y_true[i] == y_true[i]:
+            loss = loss + 1
+    return loss / len(y_pred)
 
 
 def presicion(y_true, y_pred):
@@ -32,7 +43,15 @@ def presicion(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    if len(y_true) != len(y_pred):
+        raise Exception("Size error")
+    TP = sum(y_true * y_pred)
+    FP = 0
+    for i in range(y_true):
+        if (y_true[i] == 0 and y_pred[i] == 1):
+            FP += 1
+    # формула из лекции
+    return TP / (FP + TP)
 
 
 def recall(y_true, y_pred):
@@ -42,7 +61,15 @@ def recall(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    if len(y_true) != len(y_pred):
+        raise Exception("Size error")
+    TP = sum(y_true * y_pred)
+    FN = 0
+    for i in range(y_true):
+        if (y_true[i] == 1 and y_pred[i] == 0):
+            FN += 1
+    # формула из лекции
+    return TP / (TP + FN)
 
 
 def roc_auc(y_true, y_pred):
@@ -52,4 +79,16 @@ def roc_auc(y_true, y_pred):
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    if len(y_true) != len(y_pred):
+        raise Exception("Size error")
+    TPR = recall(y_true, y_pred)
+    FP = 0
+    TN = 0
+    for i in range(y_true):
+        if (y_true[i] == 0 and y_pred[i] == 1):
+            FP += 1
+        if (y_true[i] == 0 and y_pred[i] == 0):
+            TN += 1
+    FPR = FP/(FP + TN)
+    # общий случай площади для бинарного решения
+    return (1 + TPR - FPR)/2
