36072fca9c2f9bd8e23771edca1b645595e2de6b
IgorKurylev <kurilevigor@mail.ru>
2019-04-28 23:36:07 +0300

    metrics

--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -5,51 +5,52 @@
 import numpy as np
 
 
-def logloss(y_true, y_pred):
-    """
-    logloss
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated probabilities
-    :return: loss
-    """
-    pass
+def logloss(y_true, y_pred, eps=1e-15):
+
+    y_pred = np.clip(y_pred, eps, 1 - eps)
+    return (y_true.dot(np.log(y_pred)) + (1 - y_true).dot(np.log(1 - y_pred))) / (-len(y_true))
 
 
 def accuracy(y_true, y_pred):
-    """
-    Accuracy
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
-    :return: loss
-    """
-    pass
+
+    return np.sum(y_true == y_pred) / len(y_pred)
 
 
-def presicion(y_true, y_pred):
-    """
-    presicion
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
-    :return: loss
-    """
-    pass
+def precision(y_true, y_pred):
+
+    TP = 0
+    FP = 0
+    for i in range(y_true):
+        if y_true[i] == 0 and y_pred[i] != y_true[i]:
+            FP += 1
+        if y_true[i] == 1 and y_pred[i] == 1:
+            TP += 1
+    return TP / (FP + TP)
 
 
 def recall(y_true, y_pred):
-    """
-    presicion
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
-    :return: loss
-    """
-    pass
+
+    FN = 0
+    TP = 0
+    for i in range(y_true):
+        if y_true[i] == 1 and y_pred[i] == 0:
+            FN += 1
+        if y_true[i] == 1 and y_pred[i] == 1:
+            TP += 1
+
+    return TP / (TP + FN)
 
 
 def roc_auc(y_true, y_pred):
-    """
-    roc_auc
-    :param y_true: vector of truth (correct) target values
-    :param y_hat: vector of estimated probabilities
-    :return: loss
-    """
-    pass
+
+    TPR = recall(y_true, y_pred)
+    FP = 0
+    TN = 0
+    for i in range(y_true):
+        if y_true[i] == 0 and y_pred[i] == 1:
+            FP += 1
+        if y_true[i] == 0 and y_pred[i] == 0:
+            TN += 1
+    FPR = FP / (FP + TN)
+
+    return (1 + TPR - FPR) / 2
