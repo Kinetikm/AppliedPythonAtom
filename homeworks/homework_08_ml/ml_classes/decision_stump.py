3ded9ef828ab882c3941df91881a05a3fbde8c97
IgorKurylev <kurilevigor@mail.ru>
2019-04-30 01:18:47 +0300

    decision_stump

--- homeworks/homework_08_ml/ml_classes/decision_stump.py
+++ homeworks/homework_08_ml/ml_classes/decision_stump.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 
+import numpy as np
+
+
 class DecisionStumpRegressor:
     '''
     Класс, реализующий решающий пень (дерево глубиной 1)
@@ -13,16 +16,32 @@ class DecisionStumpRegressor:
         Мы должны создать поля, чтобы сохранять наш порог th и ответы для
         x <= th и x > th
         '''
-        raise NotImplementedError
+        self.th = None
+        self.left = None
+        self.right = None
 
     def fit(self, X, y):
         '''
-        метод, на котором мы должны подбирать коэффициенты th, y1, y2
+        метод, на котором мы должны подбирать коэффициенты th, left, right
         :param X: массив размера (1, num_objects)
         :param y: целевая переменная (1, num_objects)
         :return: None
         '''
-        raise NotImplementedError
+        X, y = zip(sorted(zip(X, y), key=lambda x: x[0]))
+        X, y = np.array(X), np.array(y)
+        hall = np.average(np.square(X - X.mean()))
+        n = len(y)
+        res_th = []
+        for i in range(1, n):
+            th = ((X[i - 1] + X[i]) / 2)[0]
+            left, right = X[X < th], X[X > th]
+            hleft = np.average(np.square(left - left.mean()))
+            hright = np.average(np.square(right - right.mean()))
+            res_th.append((th, hall - (len(left) * hleft + len(right) * hright) / n))
+
+        self.th = max(res_th, key=lambda x: x[1])[0]
+        self.left = y[(X <= self.th)].mean()
+        self.right = y[(X > self.th)].mean()
 
     def predict(self, X):
         '''
@@ -30,4 +49,4 @@ class DecisionStumpRegressor:
         :param X: массив размера (1, num_objects)
         :return: массив, размера (1, num_objects)
         '''
-        raise NotImplementedError
+        return [self.right if x > self.th else self.left for x in X]
