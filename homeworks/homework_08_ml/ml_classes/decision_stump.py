157bdca247fe41655eea153095daf9224296c65e
AlexLyash <procusvorum@gmail.com>
2019-04-30 09:04:31 +0300

    hw8

--- homeworks/homework_08_ml/ml_classes/decision_stump.py
+++ homeworks/homework_08_ml/ml_classes/decision_stump.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
 
 class DecisionStumpRegressor:
     '''
@@ -13,7 +16,9 @@ class DecisionStumpRegressor:
         Мы должны создать поля, чтобы сохранять наш порог th и ответы для
         x <= th и x > th
         '''
-        raise NotImplementedError
+        self.left = None
+        self.right = None
+        self.th = None
 
     def fit(self, X, y):
         '''
@@ -22,7 +27,20 @@ class DecisionStumpRegressor:
         :param y: целевая переменная (1, num_objects)
         :return: None
         '''
-        raise NotImplementedError
+        xy = np.array(sorted(zip(X, y)))
+        X, y = xy[:, 0], xy[:, 1]
+        y2 = np.zeros(y.shape)
+        mse = mean_squared_error(y, 0)
+        for i in range(len(X) - 1):
+            left = np.average(y[:i + 1])
+            right = np.average(y[i + 1:])
+            th = np.average(X[i:i + 2])
+            y2[:i + 1] = left
+            y2[i + 1:] = right
+        if mse > mean_squared_error(y, y2):
+            self.left = left
+            self.right = right
+            self.th = th
 
     def predict(self, X):
         '''
@@ -30,4 +48,4 @@ class DecisionStumpRegressor:
         :param X: массив размера (1, num_objects)
         :return: массив, размера (1, num_objects)
         '''
-        raise NotImplementedError
+        return [self.right if x > self.th else self.left for x in X]
